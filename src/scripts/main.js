2f10b966c21ee3bb2e9f2eeaadaade227dd90a0e
Vitalii <vitaliybondarenko1982@gmail.com>
2020-01-03 13:43:53 +0200

    add header & info

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const headerMain = document.querySelector('.header');
+const headerToggle = document.querySelector('.header__toggle');
+
+headerMain.classList.remove('header--nojs');
+
+headerToggle.addEventListener('click', function() {
+  if (headerMain.classList.contains('header--closed')) {
+    headerMain.classList.remove('header--closed');
+    headerMain.classList.add('header--opened');
+  } else {
+    headerMain.classList.add('header--closed');
+    headerMain.classList.remove('header--opened');
+  }
+});
