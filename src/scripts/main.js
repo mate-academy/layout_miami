1aa7bf705f1b66af4bb3db9279823c8bd1f258bc
Dan Denysovets <alen250299@gmail.com>
2020-01-04 20:42:52 +0200

    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuSlide = () => {
+  const burger = document.querySelector('.mobile-nav');
+  const header = document.querySelector('.header');
+
+  burger.addEventListener('click', () => {
+    header.classList.toggle('header--open');
+    burger.classList.toggle('toogle');
+  });
+};
+
+menuSlide();
