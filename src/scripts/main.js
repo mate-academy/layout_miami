c4c876e1fedef22251c03087f664be30b41172b0
Senya Volkov <formobileprima@gmail.com>
2020-01-04 15:10:32 +0200

    complited addaptive version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const burgerIcon = document.getElementsByClassName('burger-icon')[0];
+const nav = document.getElementsByClassName('header__nav')[0];
+const headerLink = document.getElementsByClassName('header__link')[0];
+const headerPhone = document.getElementsByClassName('header__phone')[0];
+
+burgerIcon.onclick = function(e) {
+  if (nav.classList.contains('header__nav')) {
+    nav.classList.remove('header__nav');
+    nav.classList.add('nav-burger');
+    burgerIcon.classList.add('change');
+    headerLink.classList.add('move');
+    headerPhone.classList.add('move');
+  } else {
+    nav.classList.add('header__nav');
+    nav.classList.remove('nav-burger');
+    burgerIcon.classList.remove('change');
+    headerLink.classList.remove('move');
+    headerPhone.classList.remove('move');
+  }
+};
