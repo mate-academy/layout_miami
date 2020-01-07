1551f84f64916e1f2481ccc603f8b19f5c0e4b39
Stanislav Mochyr <delenjer@gmail.com>
2020-01-07 20:22:38 +0200

    miami

---
+++ src/scripts/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let isScrolling = false;
+
+window.addEventListener('scroll', throttleScroll, false);
+
+function throttleScroll(e) {
+  if (isScrolling === false) {
+    window.requestAnimationFrame(function() {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+}
+
+document.addEventListener('DOMContentLoaded', scrolling, false);
+
+const firstBox = document.querySelector('#animateRight');
+const secondBox = document.querySelector('#animateLeft');
+const itemRight = document.querySelector('#item-right');
+const itemLeft = document.querySelector('#item-left');
+const itemTop = document.querySelector('#item-top');
+
+function scrolling(e) {
+  if (isPartiallyVisible(firstBox && secondBox)) {
+    firstBox.classList.add('wowRight');
+    secondBox.classList.add('wowLeft');
+  }
+
+  if (isPartiallyVisible(itemRight && itemLeft && itemTop)) {
+    itemRight.classList.add('wowLeft');
+    itemLeft.classList.add('wowRight');
+    itemTop.classList.add('wowTop');
+  }
+}
+
+function isPartiallyVisible(el) {
+  const elementBoundary = el.getBoundingClientRect();
+  const topTo = elementBoundary.top;
+  const bottom = elementBoundary.bottom;
+  const height = elementBoundary.height;
+
+  return ((topTo + height >= 0) && (height + window.innerHeight >= bottom));
+}
