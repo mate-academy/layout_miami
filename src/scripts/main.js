2058aec1a33a55fd59a0eb4f38e0fcadb5a4fdb2
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-07 17:04:23 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,4 +16,19 @@ window.onload = function() {
     headerTitle.classList.toggle('opacity');
     navMobile.classList.toggle('block');
   });
+
+  /* global jQuery */
+  /* eslint no-undef: "error" */
+
+  jQuery('.nav__link, .nav_mobile__link').on('click', function() {
+    const navLink = jQuery(this).attr('href');
+
+    if (navLink.length > 1) {
+      if (jQuery('div').is(navLink)) {
+        const blockTop = jQuery(navLink).offset().top;
+
+        jQuery('html, body').animate({ scrollTop: blockTop }, 400);
+      }
+    }
+  });
 };
