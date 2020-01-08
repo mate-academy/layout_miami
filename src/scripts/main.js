a43fc3efb7b8eb89360983459964b789c392c8b5
Actireon <eheieiei37@gmail.com>
2020-01-08 23:14:25 +0200

    improved layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const header = document.querySelector('.header');
-const phonee = document.querySelector('.header-contact__phone');
+const phoneIcon = document.querySelector('.header-contact__phone-icon');
+const phoneNum = document.querySelector('.header-contact__phone');
 const bars = document.querySelector('.header-contact__bars-icon');
 const closing = document.querySelector('.header-contact__close-icon');
-const phone = document.querySelector('.header-contact__phone-icon');
 const order = document.querySelector('.header-contact__order');
 const nav = document.querySelector('.nav');
 const title = document.querySelector('.header-title');
 
 bars.addEventListener('click', function() {
   header.classList.add('header--mobile');
-  phone.classList.toggle('header-contact__phone-icon');
-  phone.classList.toggle('header-contact__phone-icon--off');
-  phonee.classList.toggle('header-contact__phone');
-  phonee.classList.toggle('header-contact__phone--on');
+  phoneIcon.classList.toggle('header-contact__phone-icon');
+  phoneIcon.classList.toggle('header-contact__phone-icon--off');
+  phoneNum.classList.toggle('header-contact__phone');
+  phoneNum.classList.toggle('header-contact__phone--on');
   bars.classList.toggle('header-contact__bars-icon');
   bars.classList.toggle('header-contact__bars-icon--off');
   closing.classList.toggle('header-contact__close-icon');
@@ -29,10 +29,10 @@ bars.addEventListener('click', function() {
 
 closing.addEventListener('click', function() {
   header.classList.remove('header--mobile');
-  phone.classList.toggle('header-contact__phone-icon--off');
-  phone.classList.toggle('header-contact__phone-icon');
-  phonee.classList.toggle('header-contact__phone--on');
-  phonee.classList.toggle('header-contact__phone');
+  phoneIcon.classList.toggle('header-contact__phone-icon--off');
+  phoneIcon.classList.toggle('header-contact__phone-icon');
+  phoneNum.classList.toggle('header-contact__phone--on');
+  phoneNum.classList.toggle('header-contact__phone');
   bars.classList.toggle('header-contact__bars-icon--off');
   bars.classList.toggle('header-contact__bars-icon');
   closing.classList.toggle('header-contact__close-icon--on');
@@ -44,3 +44,14 @@ closing.addEventListener('click', function() {
   order.classList.toggle('header-contact__order--on');
   order.classList.toggle('header-contact__order');
 });
+
+const navItems = document.querySelectorAll('.nav__item');
+
+for (const i in navItems) {
+  navItems[i].addEventListener('click', function() {
+    const navActive = document.querySelector('.nav__item--active');
+
+    navActive.classList.remove('nav__item--active');
+    navItems[i].classList.add('nav__item--active');
+  });
+}
