dafc2c16d908f382d37b3bfd43febec9a33d6e87
Actireon <eheieiei37@gmail.com>
2020-01-08 02:00:39 +0200

    40% adaptive added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const bars = document.querySelector('.header-contact__bars--icon');
+const closing = document.querySelector('.header-contact__close--icon');
+const phone = document.querySelector('.header-contact__phone--icon');
+const nav = document.querySelector('.nav');
+const title = document.querySelector('.header-title');
+
+bars.addEventListener('click', function() {
+  header.classList.add('header--mobile');
+  phone.style.opacity = '0';
+  bars.style.display = 'none';
+  closing.style.cssText = 'display: block; cursor:pointer';
+  nav.style.display = 'block';
+  title.style.display = 'none';
+});
+
+closing.addEventListener('click', function() {
+  header.classList.remove('header--mobile');
+  phone.style.opacity = '1';
+  bars.style.display = 'block';
+  closing.style.display = 'none';
+  nav.style.display = 'none';
+  title.style.display = 'block';
+});
