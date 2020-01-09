8fbd7fb7ec88d2f14ed43aafca1bd78df708e21d
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-09 07:17:07 +0200

    miami landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const humburger = document.querySelector('.header__burger');
+const mobileMenu = document.querySelector('.header__mobile-nav');
+const cross = document.querySelector('.mobile-nav__cross');
+
+humburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('header__mobile-nav--active');
+});
+
+cross.addEventListener('click', () => {
+  mobileMenu.classList.remove('header__mobile-nav--active');
+});
