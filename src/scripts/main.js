2ae40063d1ee9d9e72de6162d3ee09069b96142d
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-13 16:55:52 +0200

    js deleted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 'use strict';
-
-function initMenu() {
-  const burger = document.querySelector('.header__menu-icon');
-
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('header__menu-icon--is-opened');
-  });
-}
-initMenu();
