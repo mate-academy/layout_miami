6c2a532520f927ab378d527a4c74d156486ba84b
Mikhail <mikhail.sapuzhak@gmail.com>
2020-02-25 20:10:39 +0200

    add carusel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,24 @@
 'use strict';
 
+const buttonTop = document.getElementById('button__top');
+
 window.onscroll = function() {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById('button__top').style.display = 'block';
+  if (document.documentElement.scrollTop > 650) {
+    buttonTop.style.display = 'block';
   } else {
-    document.getElementById('button__top').style.display = 'none';
+    buttonTop.style.display = 'none';
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-topFunction();
+buttonTop.addEventListener('click', topFunction);
 
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('.nav');
@@ -29,3 +30,36 @@ window.addEventListener('scroll', () => {
     link.classList.toggle('nav__link-sticky', window.scrollY > 770);
   });
 });
+
+const carousel = document.querySelector('.gallery__wrapper');
+const btnPrev = document.querySelector('.gallery__btn_prev');
+const btnNext = document.querySelector('.gallery__btn_next');
+const dots = document.querySelectorAll('.gallery__dot');
+let count = 0;
+
+function movePrevDot() {
+  if (count > 0) {
+    count--;
+    dots[count].classList.add('gallery__dot_active');
+    dots[count + 1].classList.remove('gallery__dot_active');
+  }
+}
+
+function moveNextDot() {
+  if (count < dots.length - 1) {
+    count++;
+    dots[count].classList.add('gallery__dot_active');
+    dots[count - 1].classList.remove('gallery__dot_active');
+  }
+}
+
+function moveCarousel(width) {
+  return function() {
+    carousel.scrollLeft += width;
+  };
+}
+
+btnPrev.addEventListener('click', moveCarousel(-800));
+btnNext.addEventListener('click', moveCarousel(800));
+btnPrev.addEventListener('click', movePrevDot);
+btnNext.addEventListener('click', moveNextDot);
