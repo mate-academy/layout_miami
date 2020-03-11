1d30c8436288f0b13bd75f49a56ef6d9c0565b65
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-11 16:08:09 +0200

    Heade and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const navBtnOpen = document.querySelector('#header__nav-open');
+const navBtnClose = document.querySelector('#header__nav-close');
+const navList = document.querySelector('#header__nav');
+
+navBtnOpen.addEventListener('click', () => {
+  navList.style.display = 'block';
+  navBtnClose.style.display = 'block';
+  navBtnOpen.style.display = 'none';
+});
+
+navBtnClose.addEventListener('click', () => {
+  navList.style.display = 'none';
+  navBtnClose.style.display = 'none';
+  navBtnOpen.style.display = 'block';
+});
