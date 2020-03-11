dc7d6534de9e8d548ed601213c067ff49698fdae
crash2bash <sachagurov@gmail.com>
2020-03-11 15:49:26 +0200

    done mobile header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const sandwich = document.querySelector('.header__sandwich');
+const navigation = document.querySelector('.navigation');
+const closeIcon = document.querySelector('.navigation__close');
+
+sandwich.addEventListener('click', function() {
+  navigation.classList.remove('showout');
+  navigation.classList.add('showin');
+});
+
+closeIcon.addEventListener('click', function() {
+  navigation.classList.remove('showin');
+  navigation.classList.add('showout');
+});
