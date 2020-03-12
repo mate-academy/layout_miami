8126937a4cd82c740456110bfd75512f5eff5b20
WincherZ <wincher2016@gmail.com>
2020-03-12 16:45:17 +0200

    miami4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const func = () => {
+  const header = document.querySelector('.header');
+  const navbar = document.querySelector('.header-navbar');
+  const mainTitle = document.querySelector('.header-lower__title');
+  const phone = document.querySelector('.header-upper__telephone');
+
+  header.addEventListener('click', e => {
+    const spsMaxLineLength = navbar.style.transform;
+
+    if (e.target.classList.contains('header-upper__burger')) {
+      if (spsMaxLineLength === 'translateY(-100%)' || spsMaxLineLength === '') {
+        navbar.style.display = 'block';
+        navbar.style.transform = 'translateY(0)';
+        mainTitle.style.display = 'none';
+        phone.style.opacity = '0';
+      } else {
+        navbar.style.transform = 'translateY(-100%)';
+        mainTitle.style.display = 'block';
+        phone.style.opacity = '1';
+      };
+    };
+  });
+};
+
+func();
