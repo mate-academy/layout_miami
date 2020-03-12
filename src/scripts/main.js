7fd233373afbdbfe060f7744cf16f54e54da000e
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-12 08:52:49 +0200

    Fixed nav. Added gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,15 @@ const navBtnClose = document.querySelector('#header__nav-close');
 const navList = document.querySelector('#header__nav');
 
 navBtnOpen.addEventListener('click', () => {
-  navList.style.display = 'block';
+  navList.classList.remove('nav__hide');
+  navList.classList.add('nav__show');
   navBtnClose.style.display = 'block';
   navBtnOpen.style.display = 'none';
 });
 
 navBtnClose.addEventListener('click', () => {
-  navList.style.display = 'none';
+  navList.classList.remove('nav__show');
+  navList.classList.add('nav__hide');
   navBtnClose.style.display = 'none';
   navBtnOpen.style.display = 'block';
 });
