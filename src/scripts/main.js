ec894bf8cfcafa03db4cd97f6b0cdb2e17f5b89f
Vitalii Shytikov <v.shytikov@gmail.com>
2020-03-12 14:38:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+const body = document.body;
+
+burger.addEventListener('click', mobileMenuToggle);
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', mobileMenuToggle);
+}
+
+function mobileMenuToggle() {
+  burger.classList.toggle('burger--clicked');
+  mobileMenu.classList.toggle('nav--visisble');
+  body.classList.toggle('has-modal');
+}
