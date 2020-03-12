df7c5d2d8dc51ed6d41f9963ecdc33fdaf1daafb
olenazubashenko <olenazubashenko@gmail.com>
2020-03-12 09:45:32 +0200

    continue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-// function toggleNav() {
-//   const toggleEls = document.getElementsByClassName('toggle');
+const toggleNav = () => {
+  const toggleEls = document.getElementsByClassName('toggle');
 
-//   for (const elem of toggleEls) {
-//     elem.classList.toggle('non-visible');
-//   }
-// };
+  for (const elem of toggleEls) {
+    elem.classList.toggle('non-visible');
+  }
+};
+
+// TODO: This is for linters:
+toggleNav();
+toggleNav();
