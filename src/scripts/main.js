3019d069d201ba5e844bef238e8b173cfc8dd0a9
olenazubashenko <olenazubashenko@gmail.com>
2020-03-12 09:28:39 +0200

    continue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// function toggleNav() {
+//   const toggleEls = document.getElementsByClassName('toggle');
+
+//   for (const elem of toggleEls) {
+//     elem.classList.toggle('non-visible');
+//   }
+// };
