3c2fe49d08daff3af24d4306e04b7ca8ebafc955
morfeii <morfeii88@gmail.com>
2020-03-16 20:42:38 +0200

    fix form sending action

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const formConsultation = document.querySelector('.consultation__form');
+const nameConsultation = document.querySelector('.form__input_name');
+const phoneConsultation = document.querySelector('.form__input_phone');
+const emailConsultation = document.querySelector('.form__input_email');
+
+formConsultation.addEventListener('submit', function(e) {
+  e.preventDefault();
+  nameConsultation.value = '';
+  phoneConsultation.value = '';
+  emailConsultation.value = '';
+});
+
+const formContact = document.querySelector('.contact__form');
+const nameContact = document.querySelector('.input_name');
+const emailContact = document.querySelector('.input_email');
+const messageContact = document.querySelector('.input_textarea');
+
+formContact.addEventListener('submit', function(e) {
+  e.preventDefault();
+  nameContact.value = '';
+  emailContact.value = '';
+  messageContact.value = '';
+});
