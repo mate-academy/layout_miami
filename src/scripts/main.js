c149f888e728fd1efab348524ef041e7cd33846d
Yuliya312 <yu312liya@gmail.com>
2020-03-16 16:32:42 +0200

    fix form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const consultationForm = document.querySelector('.consultation__form');
+
+consultationForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+});
+
+const contactForm = document.querySelector('.contact__form');
+
+contactForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+});
