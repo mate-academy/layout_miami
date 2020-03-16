89b2863784edb703f3903893b07bc4e434a9aeb2
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-03-16 19:39:05 +0200

    form action

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const formConsult = document.querySelector('.consultation__form');
+
+formConsult.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.location.reload(false);
+});
+
+const formContact = document.querySelector('.contact__form');
+
+formContact.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.location.reload(false);
+});
