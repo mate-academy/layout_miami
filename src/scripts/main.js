8daf29b557d1e824966a7ae1c8ff49ac599cdf03
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-03-20 20:35:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const burgerMenu = document.getElementById('menu__toggle');
+const links = document.getElementsByClassName('menu__item');
+let toggle = false;
+
+const closeMenu = () => {
+  document.body.style.overflow = 'visible';
+  toggle = false;
+};
+
+burgerMenu.addEventListener('click', () => {
+  if (toggle) {
+    closeMenu();
+  } else {
+    document.body.style.overflow = 'visible';
+    toggle = false;
+  }
+});
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', () => {
+    burgerMenu.checked = false;
+    closeMenu();
+  });
+}
