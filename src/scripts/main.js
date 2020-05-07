ab54b7121b3d342123f07dc4f2760d59bdbd21ee
aleksey-10 <alexcare1337@gmail.com>
2020-05-07 21:44:34 +0300

    fixed nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav-mobile');
 const burger = document.querySelector('.header__burger-btn');
-const times = nav.querySelector('.nav__times');
+const times = nav.querySelector('.nav-mobile__times');
 
 burger.onclick = function() {
   translateNav();
 };
 
 nav.addEventListener('click', function(e) {
-  if ((e.target === times || e.target.classList.contains('nav__link'))
-    && document.documentElement.clientWidth < 1025) {
+  if (e.target === times || e.target.classList.contains('nav-mobile__link')) {
     translateNav(100);
   }
 });
