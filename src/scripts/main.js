06dea7dbd132b6d64e07e3c24b8f2ed6f4f454f0
aleksey-10 <alexcare1337@gmail.com>
2020-05-07 18:59:26 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const burger = document.querySelector('.header__burger-btn');
+const times = nav.querySelector('.nav__times');
+
+burger.onclick = function() {
+  translateNav();
+};
+
+nav.addEventListener('click', function(e) {
+  if ((e.target === times || e.target.classList.contains('nav__link'))
+    && document.documentElement.clientWidth < 1025) {
+    translateNav(100);
+  }
+});
+
+function translateNav(val = 0) {
+  nav.style.transform = `translateX(${val}%)`;
+}
