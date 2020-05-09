122bfe082453259a19adba479846bfebe113a03a
Valentyn Krushynin <krushunin89@gmail.com>
2020-05-09 23:08:32 +0300

    add normal side menu, add JS, close-open works well

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const burger = document.querySelector('.nav-top__burger--toggler');
+const closeMenu = document.querySelector('.nav-toggle__close');
+const menu = document.querySelector('.nav-toggle');
+const clickListener = document.querySelector('.click-listener');
+
+function classToggle() {
+  clickListener.classList.toggle('click-listener--active');
+  menu.classList.toggle('nav-toggle--active');
+};
+
+burger.onclick = classToggle;
+
+menu.addEventListener('click', function(e) {
+  if (e.target === closeMenu
+    || e.target.classList.contains('nav-toggle__link')) {
+    classToggle();
+  }
+});
+
+clickListener.addEventListener('click', function(e) {
+  if (e.target === clickListener) {
+    classToggle();
+  }
+});
