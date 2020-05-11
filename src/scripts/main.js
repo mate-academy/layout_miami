3d044573e4ca60dd001aa860d22da289ec06a831
Nazarii <nazik100378@gmail.com>
2020-05-11 14:03:31 +0300

    mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,17 @@
 document.querySelector('.menu-icon').onclick = function() {
   document.querySelector('.menu-icon-line')
     .classList.toggle('menu-icon-active');
+
+  document.querySelector('.mob__nav')
+    .classList.toggle('mob__slide');
 };
+
+/* document.body.querySelector('.mob__nav').onclick = function() {
+  document.classList.toggle('mob__slide');
+}; */
+
+/* const menuToggle = document.getElementById('menu-togle');
+
+menuToggle.onclick = function() {
+  menuToggle.classList.toggle('menu-icon-active');
+}; */
