41e59400bdf51b7c5100337395bec4c02f391d76
Vovasef <vovasef@gmail.com>
2020-05-12 09:50:21 +0300

    v. 1.3 burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+var $nav = $("#nav__container");
+var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    $nav.toggleClass("nav__container nav__container--mobile-show");
+    $(".mobile-bg").fadeToggle();
+    $(".nav__item--link").toggleClass("nav__item--link-mobile");
+    $(".header__call-to-order").toggleClass("header__call-to-order--mobile");
+    $(".header__phone").toggleClass("header__phone--mobile");
+  });
