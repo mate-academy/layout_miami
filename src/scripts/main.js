1a3a75eaf2b05bc04d546d1db4fb40990ec57661
bran4u <s.oselskiy@gmail.com>
2020-05-12 14:19:07 +0300

    broked git with last commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const nav = document.querySelector('.nav-mobile');
+const mobile = document.querySelector('.header__mobile-btn');
+const times = nav.querySelector('.nav-mobile__times');
+
+mobile.onclick = function() {
+  translateNav();
+};
+
+nav.addEventListener('click', function(e) {
+  if (e.target === times || e.target.classList.contains('nav-mobile__link')) {
+    translateNav(100);
+  }
+});
+
+function translateNav(val = 0) {
+  nav.style.transform = `translateX(${val}%)`;
+}
