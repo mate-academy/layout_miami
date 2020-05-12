1a9d5107d57625955bdd150b4b6481a939d83baa
Vovasef <vovasef@gmail.com>
2020-05-12 17:38:33 +0300

    v. 1.3.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
-var $nav = $("#nav__container");
-var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-    $nav.toggleClass("nav__container nav__container--mobile-show");
+var nav = $("#nav__container");
+var hamburger = $(".hamburger");
+  hamburger.on("click", function(e) {
+    hamburger.toggleClass("is-active");
+    nav.toggleClass("nav__container nav__container--mobile-show");
     $(".mobile-bg").fadeToggle();
     $(".nav__item--link").toggleClass("nav__item--link-mobile");
     $(".header__call-to-order").toggleClass("header__call-to-order--mobile");
@@ -12,3 +12,7 @@ var $hamburger = $(".hamburger");
 
   $("textarea").attr("placeholder", "Message");
   $(textarea).attr("required");
+
+  $(".form").trigger("reset");
+  
+  
