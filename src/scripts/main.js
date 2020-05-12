9b4a5cea7bcb758dae5896315984f1dee9154261
solaryasha <solaryasha@gmail.com>
2020-05-12 17:20:19 +0300

    Mobile first version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const navMenu = document.querySelector('.navbar-mobile');
+const burger = document.querySelector('.promo__toggler-label');
+
+burger.onclick = function() {
+  toggler();
+};
+
+navMenu.addEventListener('click', e => {
+  toggler(100);
+});
+
+function toggler(count = 0) {
+  navMenu.style.transform = `translateX(${count}%)`;
+}
