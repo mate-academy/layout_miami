36de21a75cd42a2bfbdea165c365b5019e389585
Nazarii <nazik100378@gmail.com>
2020-05-12 18:31:07 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,3 @@ document.querySelector('.menu-icon').onclick = function() {
   document.querySelector('.mob__nav')
     .classList.toggle('mob__slide');
 };
-
-/* document.body.querySelector('.mob__nav').onclick = function() {
-  document.classList.toggle('mob__slide');
-}; */
-
-/* const menuToggle = document.getElementById('menu-togle');
-
-menuToggle.onclick = function() {
-  menuToggle.classList.toggle('menu-icon-active');
-}; */
