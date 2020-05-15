2dae682ce0dd7268de14f86b9cccc4a969b39c5b
Yura <wa7er14@gmail.com>
2020-05-15 23:21:11 +0300

    form clean & scale add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,21 @@
 'use strict';
 /*eslint-disable */ //error *'burger' is defined but never used  no-unused-vars*, but it's used in button's onClick
-function burger() { 
+function burger() {
   const elem = document.getElementById('header');
 
   elem.classList.toggle('header--active');
 }
 
-function onSubmit() {
-  console.log('hello');
+function onSubmitConsult() {
   event.preventDefault();
-  console.log('submit');
+  document.querySelector('input[name=consultation-name]').value = '';
+  document.querySelector('input[name=consultation-tel]').value = '';
+  document.querySelector('input[name=consultation-email]').value = '';
+}
+
+function onSubmitContact() {
+  event.preventDefault();
+  document.querySelector('input[name=contact-name]').value = '';
+  document.querySelector('input[name=contact-tel]').value = '';
+  document.querySelector('textarea[name=contact-message]').value = '';
 }
