69b48333ac9e8e0263f5b0ffe7e32898367b5f41
olenazubashenko <olenazubashenko@gmail.com>
2020-05-20 23:52:29 +0300

    refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,10 @@ const toggleNav = () => {
   }
 };
 
-// TODO: This is for linters:
-toggleNav();
-toggleNav();
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 700, 'linear');
+});
