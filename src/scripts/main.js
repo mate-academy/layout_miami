93b7182f22a18c1d64ceb5ad16581c37b73d3906
Denys <gryshko_denys@gmail.com>
2020-06-20 10:47:13 +0300

    optimized photoes and changed bg of header mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const burger = document.querySelector('.header__toggler');
+const header = document.querySelector('header');
+
+burger.onclick = () => {
+  header.classList.toggle('header--mobile-bg');
+};
