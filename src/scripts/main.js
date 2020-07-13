cf9ef65b64ed7f2c6118ebd2eb1a704cf4e86b0a
nazmeln <nazik100378@gmail.com>
2020-07-13 18:06:30 +0300

    fixed forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,27 @@ document.querySelector('.menu-icon').onclick = function() {
   document.querySelector('.mob__nav')
     .classList.toggle('mob__slide');
 };
+
+// form
+
+function onSubmitConsultation() {
+  event.preventDefault();
+  document.querySelector('.input__consultation-text').value = '';
+  document.querySelector('.input__consultation-email').value = '';
+  document.querySelector('.input__consultation-phone').value = '';
+}
+
+function onSubmitContact() {
+  event.preventDefault();
+  document.querySelector('.input__contact-textarea').value = '';
+  document.querySelector('.input__contact-name').value = '';
+  document.querySelector('.input__contact-phone').value = '';
+}
+
+const submitButtonConsultation = document.querySelector('.consultation__form');
+const submitButtonContact = document.querySelector('.contact__form');
+
+submitButtonConsultation.addEventListener('submit', onSubmitConsultation);
+submitButtonContact.addEventListener('submit', onSubmitContact);
+
+// .addEventListener('submit', onSubmit);
