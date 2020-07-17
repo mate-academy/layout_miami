d63979fd456da22a3901c09e18dbfa0384865c16
mpone <m.parphenjuk@gmail.com>
2020-07-18 00:06:09 +0300

    Landing Miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (const anchor of anchors) {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
