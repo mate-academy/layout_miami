2496ce38fc6fe8a5064a74934a25ce7f16eba821
Roman <romikkus@gmail.com>
2020-07-18 21:33:54 +0300

    Added mobile solution and fixed some errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const screenSize = window.matchMedia('(min-width: 641px)');
+
+screenSize.addEventListener('change', (e) => {
+  const navState = document.querySelector('.mobile-header');
+
+  if (e.matches) {
+    navState.style.display = 'none';
+  }
+});
