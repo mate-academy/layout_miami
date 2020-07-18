a2b87792a11add45194a9d64f57af9b61b9005b9
movchko <lemberg.my.city@gmail.com>
2020-07-18 18:26:29 +0300

    v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+/* global $ */
+
+$(document).ready(function() {
+  $('.top__burger').click(function(event) {
+    $('.top__burger,.nav,.header__blue,.top__call,.top__number')
+      .toggleClass('active');
+  });
+});
