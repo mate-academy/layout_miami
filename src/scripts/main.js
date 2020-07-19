625246a1e139d9eb07f01af5f29708f24792ccf1
artsashko <artyomsashko@gmail.com>
2020-07-19 20:57:31 +0300

    Miami header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+document.querySelector('.burger-menu').onclick = function() {
+  document.querySelector('.burger-menu__icon').classList.toggle(
+    'burger-menu__icon--active');
+
+  document.querySelector('.burger-menu').classList.toggle(
+    'burger-menu--active');
+
+  document.querySelector('.header__container').classList.toggle(
+    'header__container--burger-menu');
+
+  document.querySelector('.nav').classList.toggle(
+    'nav--burger-menu');
+
+  document.querySelector('.nav__link').classList.remove(
+    'nav__link--active');
+
+  document.querySelector('.header__logo').classList.toggle(
+    'header__logo--burger-menu');
+
+  document.querySelector('.header__phone').classList.toggle(
+    'header__phone--burger-menu');
+
+  document.querySelector('.header__link-call').classList.toggle(
+    'header__link-call--burger-menu');
+};
