ac246049800016023fd223508f32499520ebfe35
Roman <romikkus@gmail.com>
2020-07-20 10:50:02 +0300

    Added menu toggler

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
+const navState = document.querySelector('.mobile-header');
 const screenSize = window.matchMedia('(min-width: 641px)');
+const toggler = document.querySelectorAll('.toggler');
 
 screenSize.addEventListener('change', (e) => {
-  const navState = document.querySelector('.mobile-header');
-
   if (e.matches) {
     navState.style.display = 'none';
   }
+});
+
+toggler.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    if (navState.style.display === 'block') {
+      navState.style.display = 'none';
+    } else {
+      navState.style.display = 'block';
+    }
+  });
 });
