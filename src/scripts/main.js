bef41dd03cd69733366b3accbad6a90a9905685c
artsashko <artyomsashko@gmail.com>
2020-07-21 17:27:47 +0300

    Done with last checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,3 +25,26 @@ document.querySelector('.burger-menu').onclick = function() {
   document.querySelector('.header__link-call').classList.toggle(
     'header__link-call--burger-menu');
 };
+
+document.querySelector('.nav__link').onclick = function() {
+  document.querySelector('.burger-menu__icon').classList.remove(
+    'burger-menu__icon--active');
+
+  document.querySelector('.burger-menu').classList.remove(
+    'burger-menu--active');
+
+  document.querySelector('.header__container').classList.remove(
+    'header__container--burger-menu');
+
+  document.querySelector('.nav').classList.remove(
+    'nav--burger-menu');
+
+  document.querySelector('.header__logo').classList.remove(
+    'header__logo--burger-menu');
+
+  document.querySelector('.header__phone').classList.remove(
+    'header__phone--burger-menu');
+
+  document.querySelector('.header__link-call').classList.remove(
+    'header__link-call--burger-menu');
+};
