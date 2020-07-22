2945801ca7fc3143070078656b1921c04c88a815
Roman <romikkus@gmail.com>
2020-07-22 12:24:49 +0300

    Improved mobile menu  animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,10 @@
 'use strict';
 
 const navState = document.querySelector('.mobile-header');
-const screenSize = window.matchMedia('(min-width: 769px)');
 const toggler = document.querySelectorAll('.toggler');
-
-screenSize.addEventListener('change', (e) => {
-  if (e.matches) {
-    navState.style.display = 'none';
-  }
-});
 
 toggler.forEach((item) => {
   item.addEventListener('click', (e) => {
-    if (navState.style.display === 'block') {
-      navState.style.display = 'none';
-    } else {
-      navState.style.display = 'block';
-    }
+    navState.classList.toggle('mobile-header-active');
   });
 });
