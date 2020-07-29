86f9450c10246352d044c1e860bd965197319d69
Oleg <mvp.westbrook4k@gmail.com>
2020-07-29 11:53:12 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const navList = document.querySelector('.nav__list');
+const burgerList = document.querySelector('.burger__list');
+const burgerCheckbox = document.querySelector('.burger__checkbox');
+
+navList.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const anchor = event.target;
+  const sectionId = anchor.getAttribute('href');
+
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+);
+
+burgerList.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  burgerCheckbox.checked = false;
+
+  const anchor = event.target;
+  const sectionId = anchor.getAttribute('href');
+
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+);
