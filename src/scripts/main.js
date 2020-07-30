46a03969ad3297d3f6e636882c80e55a7fdd07fc
Dennis Priadka <dennis.priadka@gmail.com>
2020-07-30 13:14:37 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function cardSlider(sliderSection) {
+  const wrapper = document.querySelector(`${sliderSection} 
+  .cards-slider__slides-wrapper`);
+
+  wrapper.style.marginLeft = '0';
+
+  document.querySelector(`${sliderSection} 
+  .slider-section__button--left`).onclick = () => {
+    if (parseInt(wrapper.style.marginLeft) === 0) {
+      wrapper.style.marginLeft = '-200%';
+    } else {
+      wrapper.style.marginLeft = parseInt(wrapper.style.marginLeft) + 100 + '%';
+    };
+  };
+
+  document.querySelector(`${sliderSection} 
+  .slider-section__button--right`).onclick = () => {
+    if (parseInt(wrapper.style.marginLeft) === -200) {
+      wrapper.style.marginLeft = '0';
+    } else {
+      wrapper.style.marginLeft = parseInt(wrapper.style.marginLeft) - 100 + '%';
+    };
+  };
+};
+
+cardSlider('.hot-prices__slider-section');
+cardSlider('.brand-new__slider-section');
