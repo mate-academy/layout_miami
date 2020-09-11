a3348f79121c9d36e8c7fd0ae7dcd23485a2f18c
Petro <petdavy@1991gmail.com>
2020-09-11 15:18:22 +0300

    add burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menuIcon = document.querySelector('.menu-icon');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuList = document.querySelector('.burger-menu__list');
+
+menuIcon.addEventListener('click', openMenu);
+burgerMenuList.addEventListener('click', openMenu);
+
+function openMenu() {
+  document.body.classList.toggle('no-scroll');
+  burgerMenu.classList.toggle('burger-menu--open');
+
+  menuIcon.classList.toggle('menu-icon--open');
+}
