8c1406a1dd67365d57fffa7639719abc13c6984e
Alona <aionovaionova@gmail.com>
2020-09-11 20:11:18 +0300

    fixed mobile v

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burger = document.querySelector('.header__checkbox');
+const list = document.querySelector('.nav__list');
+
+burger.addEventListener('click', toActivate(list));
+
+function toActivate(elem) {
+  elem.classlist.add('active');
+}
