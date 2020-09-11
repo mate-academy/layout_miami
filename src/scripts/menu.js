9098bf927ac4d34e54da0cb2a5bda9fa162c4430
Nikolay Filiuk <Filiuk98@gmail.com>
2020-09-11 17:29:55 +0300

    Modify menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const toggler = document.getElementById('toggler');
+const nav = document.getElementById('nav');
+const logo = document.getElementById('logo');
+const links = Array.from(document.getElementsByClassName('nav__link'));
 
 toggler.onclick = function toggleClass() {
-  const nav = document.getElementById('nav');
-
+  logo.classList.toggle('logo--fixed');
   nav.classList.toggle('nav--active');
+  toggler.classList.toggle('nav-toggler--opened');
 };
+
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = function toggleClass() {
+    logo.classList.toggle('logo--fixed');
+    nav.classList.toggle('nav--active');
+    toggler.classList.toggle('nav-toggler--opened');
+  };
+}
