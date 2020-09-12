da33fb625eb14ab17dbb37ffa36d8983e18876ec
Alona <aionovaionova@gmail.com>
2020-09-12 12:49:38 +0300

    fixed menu width

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,10 @@
 
 const burger = document.querySelector('.header__burger');
 const mobileNav = document.querySelector('#mobile-nav');
+const firstScreen = document.querySelector('.page__first-screen');
 
 burger.addEventListener('click', function() {
   mobileNav.classList.toggle('mobile-nav--active');
   burger.classList.toggle('header__burger--active');
+  firstScreen.classList.toggle('page__first-screen--active');
 });
