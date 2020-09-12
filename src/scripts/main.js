1a97330d64533defde6b4aa6fff57592ab57f3cb
KhrystynaYelyseyeva <play1409@gmail.com>
2020-09-12 11:50:13 +0300

    burger 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+(function() {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('.header__nav--active');
+  });
+}());
+
+(function() {
+  const burgerItem = document.querySelector('.burger');
+  const closeItem = document.querySelector('.nav__close');
+  const menu = document.querySelector('.header__nav');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('.header__nav--active');
+  });
+  closeItem.addEventListener('click', () => {
+    menu.classList.remove('.header__nav--active');
+  });
+}());
