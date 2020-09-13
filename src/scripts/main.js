a31736e219cd884774d555b6d79f27adb0887b68
Evgenii <voznukzhenia@gmail.com>
2020-09-13 12:35:28 +0300

    relize

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const nav = document.querySelector('.header__nav');
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', openMenu);
+
+function openMenu() {
+  nav.classList.toggle('active');
+}
