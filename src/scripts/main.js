605a37eeb362bf7f0af18ab583be38fad6225db3
Evgenii <voznukzhenia@gmail.com>
2020-09-14 11:26:45 +0300

    fix after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,18 @@
 
 const nav = document.querySelector('.header__nav');
 const burger = document.querySelector('.burger');
+const logo = document.querySelector('.logo');
+const navItem = document.querySelectorAll('.nav__item');
 
 burger.addEventListener('click', openMenu);
 
 function openMenu() {
   nav.classList.toggle('active');
+  burger.classList.toggle('active');
+  logo.classList.toggle('active');
 }
+
+navItem.forEach(element => 
+  { 
+    element.addEventListener('click',openMenu)
+  })
