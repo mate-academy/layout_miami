5da6cf190cd6817cf3c8b9744fba172ddb0f9f82
Dmitry Morozov <10mdv11@gmail.com>
2020-09-14 13:27:18 +0300

    add burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,73 @@
 'use strict';
+
+const burger = document.querySelector('.burger'); // element
+const burgerItem = document.querySelectorAll('.burger__item');
+const phone = document.querySelectorAll('.header__phone'); // massive
+const link = document.querySelectorAll('.header__link');
+const logo = document.querySelector('.header__logo'); // element
+const header = document.querySelector('.header'); // element
+const list = document.querySelector('.nav__list'); // element
+const navItem = document.querySelectorAll('.nav__item'); // massive
+const promo = document.querySelector('.promo'); // element
+const title = document.querySelector('.header__title');
+const nav = document.querySelector('.header__nav');
+const headerIcon = document.querySelector('.header__phone--icon');
+const active = document.querySelector('.nav__item_active');
+
+burger.addEventListener('click', () => {
+  burgerItem.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  phone.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  link.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  logo.classList.toggle('menu');
+  header.classList.toggle('menu');
+  list.classList.toggle('menu');
+
+  navItem.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  active.classList.toggle('nav__item_active');
+
+  promo.classList.toggle('menu');
+
+  title.classList.toggle('menu');
+  nav.classList.toggle('menu');
+  headerIcon.classList.toggle('menu');
+});
+
+navItem.forEach(element => {
+  element.addEventListener('click', () => {
+    burgerItem.forEach(item => {
+      item.classList.toggle('menu');
+    });
+
+    phone.forEach(item => {
+      item.classList.toggle('menu');
+    });
+
+    logo.classList.toggle('menu');
+    header.classList.toggle('menu');
+    list.classList.toggle('menu');
+
+    navItem.forEach(item => {
+      item.classList.toggle('menu');
+    });
+
+    active.classList.toggle('nav__item_active');
+
+    promo.classList.toggle('menu');
+
+    title.classList.toggle('menu');
+    nav.classList.toggle('menu');
+    headerIcon.classList.toggle('menu');
+  });
+});
