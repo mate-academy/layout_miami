61469813c5d3297586ae84db89c69ab827813f23
Your Name <mogolgogol.90@gmail.com>
2020-09-16 12:36:58 +0300

    fixed 4k gallery and hover links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict'
+function mobileNavOn() {
+  document.querySelector('.navbar__adapt').style.display = 'block';
+}
+function mobileNavOff() {
+  document.querySelector('.navbar__adapt').style.display = 'none';
+  document.querySelector('.navbar__adapt').classList.add("closed");
+}
