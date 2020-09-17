25b86db9b8b8e32a265d6c29feb59a72b90923f0
KhrystynaYelyseyeva <play1409@gmail.com>
2020-09-17 20:20:52 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,16 @@
 'use strict';
 
-(function() {
-  const burgerItem = document.querySelector('.burger');
-  const menu = document.querySelector('.header__nav');
+const body = document.querySelector('.page');
+const burgerItem = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const closeItem = document.querySelector('.nav__close-burger');
 
-  burgerItem.addEventListener('click', () => {
-    menu.classList.add('header__nav--active');
-  });
-}());
+burgerItem.addEventListener('click', () => {
+  menu.classList.add('header__nav--active');
+  body.style.overflow = 'hidden';
+});
 
-(function() {
-  const burgerItem = document.querySelector('.burger');
-  const closeItem = document.querySelector('.nav__close');
-  const menu = document.querySelector('.header__nav');
-
-  burgerItem.addEventListener('click', () => {
-    menu.classList.add('header__nav--active');
-  });
-
-  closeItem.addEventListener('click', () => {
-    menu.classList.remove('header__nav--active');
-  });
-}());
+closeItem.addEventListener('click', () => {
+  menu.classList.remove('header__nav--active');
+  body.style.overflow = 'scroll';
+});
