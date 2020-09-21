2ba7d5da2ccc0a688e7a5a95355b3557a2116424
KhrystynaYelyseyeva <play1409@gmail.com>
2020-09-21 14:50:48 +0300

    add function to nav__item in burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const body = document.querySelector('.page');
 const burgerItem = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
+const menuItems = document.querySelectorAll('.nav__item');
 const closeItem = document.querySelector('.nav__close-burger');
 
 burgerItem.addEventListener('click', () => {
@@ -14,3 +15,10 @@ closeItem.addEventListener('click', () => {
   menu.classList.remove('header__nav--active');
   body.style.overflow = 'scroll';
 });
+
+for (const menuItem of menuItems) {
+  menuItem.addEventListener('click', () => {
+    menu.classList.remove('header__nav--active');
+    body.style.overflow = 'scroll';
+  });
+}
