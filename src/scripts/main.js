73ef0a63b696368b9553a750d16a7260cf9da0b4
KirillLutsenko <mnrovski68@gmail.com>
2020-09-22 17:27:22 +0300

    fixes error 405 & lint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var form = document.querySelector('.form');
+
+var contactForm = document.querySelector('.contact__form');
+
+form.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
+
+contactForm.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
