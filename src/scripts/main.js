2261cedb828f2f546937ee9f050c017eb157b8e4
Viktor Morhun <victor.morgun16@gmail.com>
2020-12-03 17:12:18 +0200

    anchor links were added and error 405 fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menu = document.querySelector('.mobile-menu');
+const toggler = document.querySelector('.toggler');
+
+toggler.onclick = function toggleSidebar() {
+  menu.classList.toggle('mobile-menu--open');
+  toggler.classList.toggle('toggler--active');
+};
