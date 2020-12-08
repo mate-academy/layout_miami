37266ffb6fab6e19111c62bfc256b98a4d16ffa0
Viktor Morhun <victor.morgun16@gmail.com>
2020-12-08 20:29:48 +0200

    adaptive for all blocks and burger were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,10 @@
 
 const menu = document.querySelector('.mobile-menu');
 const toggler = document.querySelector('.toggler');
+const page = document.querySelector('.page');
 
 toggler.onclick = function toggleSidebar() {
   menu.classList.toggle('mobile-menu--open');
-  toggler.classList.toggle('toggler--active');
+  toggler.classList.toggle('toggler--open');
+  page.classList.toggle('page--fixed');
 };
