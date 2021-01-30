e929191d6c4c495e5c3d7d971311f2e6baa1759f
chri911 <delightsome@bigmir.net>
2021-01-30 17:57:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.nav');
+const toggler = document.querySelector('.burger__line');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('nav--desktop');
+  menu.classList.toggle('nav--mobile');
+  toggler.classList.toggle('burger__line--open');
+});
