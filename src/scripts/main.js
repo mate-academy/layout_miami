07eeb367bbeae0294106272e7bc47382dd69b1ec
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-02-02 00:41:56 +0200

    almost finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const burger = document.querySelector('.burger__menu');
+
+// eslint-disable-next-line no-unused-vars
+const header = document.querySelector('.header');
+
+burger.addEventListener('click', function(e) {
+  e.preventDefault();
+  this.classList.toggle('is-active');
+  this.classList.toggle('background');
+});
+
+burger.addEventListener('click', function(e) {
+  e.preventDefault();
+  header.classList.toggle('background');
+});
