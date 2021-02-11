a8fcca1226ffb11a443372be6a03e0ac00edad24
Artem Maksimikhin <uanov86@gmail.com>
2021-02-11 20:49:05 +0200

    finish header, mobile-nav and adaptive this

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burgerMenuWrap = document.querySelector('.burger-menu-wrapper');
+const burgerMenu = document.querySelector('.burger-menu');
+const headerLogo = document.querySelector('.header__logo');
+
+burgerMenuWrap.onclick = function() {
+  burgerMenu.classList.toggle('burger-menu--active');
+  burgerMenuWrap.classList.toggle('burger-menu-wrapper--active');
+  document.querySelector('.mobile-nav').classList.toggle('mobile-nav--active');
+  headerLogo.classList.toggle('header__logo--active');
+  document.body.classList.toggle('body--disabled');
+  document.querySelector('.nav').classList.toggle('nav--active');
+};
