bd1ee81e6a7aba0a6aa28dc3d3e300168c45ad69
Artem Maksimikhin <uanov86@gmail.com>
2021-02-15 18:37:07 +0200

    Changed mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,19 @@
 
 const burgerMenuWrap = document.querySelector('.burger-menu-wrapper');
 const burgerMenu = document.querySelector('.burger-menu');
-const headerLogo = document.querySelector('.header__logo');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileNav = document.querySelector('.mobile-nav__list');
 
 burgerMenuWrap.onclick = function() {
   burgerMenu.classList.toggle('burger-menu--active');
   burgerMenuWrap.classList.toggle('burger-menu-wrapper--active');
-  document.querySelector('.mobile-nav').classList.toggle('mobile-nav--active');
-  headerLogo.classList.toggle('header__logo--active');
+  mobileMenu.classList.toggle('mobile-menu--active');
   document.body.classList.toggle('body--disabled');
-  document.querySelector('.nav').classList.toggle('nav--active');
 };
 
-const nav = document.querySelector('.nav__list');
-
-nav.onclick = function() {
+mobileNav.onclick = function() {
   burgerMenu.classList.remove('burger-menu--active');
   burgerMenuWrap.classList.remove('burger-menu-wrapper--active');
-  headerLogo.classList.remove('header__logo--active');
   document.body.classList.remove('body--disabled');
-  document.querySelector('.nav').classList.remove('nav--active');
-  document.querySelector('.mobile-nav').classList.remove('mobile-nav--active');
+  mobileMenu.classList.remove('mobile-menu--active');
 };
