319a3acf859d05cacbd510c72a0ed7a58045b909
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-02-18 13:49:12 +0200

    add burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const mobileBurger = document.querySelector('.header__burger');
+const mobileNav = document.querySelector('.mobile');
+
+mobileBurger.onclick = function() {
+  if (mobileBurger.style.position === 'fixed') {
+    mobileBurger.style.position = 'relative';
+    mobileBurger.style.right = '0px';
+  } else {
+    mobileBurger.style.position = 'fixed';
+    mobileBurger.style.right = '20px';
+  }
+
+  if (mobileNav.style.visibility === 'visible') {
+    mobileNav.style.visibility = 'hidden';
+  } else {
+    mobileNav.style.visibility = 'visible';
+  };
+
+  mobileBurger.classList.toggle('transformation');
+};
