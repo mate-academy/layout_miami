303b1607231ab57a1d633d83f97f0ef5839a5ebf
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-02-18 11:31:10 +0200

    first_version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burger = document.getElementById('burger');
+const nav = document.getElementById('mobile__nav');
+
+burger.onclick = function() {
+  this.classList.toggle('change');
+
+  if (nav.style.visibility === 'visible') {
+    nav.style.visibility = 'hidden';
+  } else {
+    nav.style.visibility = 'visible';
+  };
+};
