35a8d9328b1f0effb1f23680370b2747071e33f4
Iryna Haiduk <$ git config --global user.name Iryna>
2021-02-18 18:02:20 +0200

    Finished creating page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuMb = document.querySelector('.header__mobile');
+const menuOpen = document.querySelector('.action__icon');
+const menuClose = document.querySelector('.mobile__icon');
+
+menuOpen.addEventListener('click', function() {
+  menuMb.style.display = 'block';
+});
+
+menuClose.addEventListener('click', function() {
+  menuMb.style.display = 'none';
+});
