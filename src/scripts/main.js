c4a590011a3aee3bbac7306bbd4561def0b6cbe9
Ihor Drul <druleger@gmail.com>
2021-02-19 12:15:15 +0200

    Created burger menu, made header responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const burger = document.getElementById('burger');
+const nav = document.getElementById('mobile-nav');
+const links = document.getElementsByClassName('mobile-nav__link');
+const phone = document.getElementById('mobile-phone');
+const menuOpen = document.getElementById('open-menu');
+const menuClose = document.getElementById('close-menu');
+
+const visible = () => {
+  if (nav.style.display === 'block') {
+    nav.style.display = 'none';
+    phone.style.visibility = 'visible';
+    menuOpen.style.display = 'inline-block';
+    menuClose.style.display = 'none';
+  } else {
+    phone.style.visibility = 'hidden';
+    nav.style.display = 'block';
+    menuOpen.style.display = 'none';
+    menuClose.style.display = 'inline-block';
+  }
+};
+
+[...links].forEach(el => el.addEventListener('click', visible));
+burger.addEventListener('click', visible);
