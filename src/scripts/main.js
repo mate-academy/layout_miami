c9272bcfc9679daf0207fc346a7075df814e9783
olena <melolena33@gmail.com>
2021-02-20 11:12:37 +0200

    Added mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const $ = window.$;
+$(document).ready(function() {  
+  $('.header__button').click(function() {
+    $('.burger').slideToggle();
+    $('.burger__nav').slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $('.burger__button').click(function() {
+    $('.burger').slideToggle();
+    $('.burger__nav').slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $('.mobile-nav__item').click(function() {
+    $('.burger').slideToggle();
+    $('.burger__nav').slideToggle();
+  });
+});
