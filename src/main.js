4c030a77e6aec809bdef3fd124e76df01259544b
Onishchuk <onishchuk.danil@gmail.com>
2021-02-22 14:51:47 +0200

    fixed

--- src/main.js
+++ src/main.js
@@ -5,15 +5,11 @@ const $ = window.$;
 $(document).ready(function() {
   $('.header__burger-on').click(function() {
     $('.burger').slideToggle();
-    $('.header__title').slideToggle();
-    $('.header__row').slideToggle();
   });
 });
 
 $(document).ready(function() {
   $('.burger__off').click(function() {
     $('.burger').slideToggle();
-    $('.header__title').slideToggle();
-    $('.header__row').slideToggle();
   });
 });
