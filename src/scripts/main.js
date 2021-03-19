e847d086f4a42fbc5a97c525eaa4ca64bd0fe198
morozoleh <olegmoroz93@gmail.com>
2021-03-19 19:43:01 +0200

    fixed-bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const callForm = document.querySelector('.call__form');
+const callSubmit = document.querySelector('.call__button');
+
+callSubmit.addEventListener('click', () => {
+  setTimeout(() => (
+    [...callForm.children].forEach(child => (child.value = ''), 1000
+    )));
+});
+
+const consultationForm = document.querySelector('.consultation__form');
+const consultationSubmit = document.querySelector('.consultation__button');
+
+consultationSubmit.addEventListener('click', () => {
+  setTimeout(() => (
+    [...consultationForm.children].forEach(child => (child.value = ''), 1000
+    )));
+});
