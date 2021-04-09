0422df0a4dab4ab1ee3f2440c2b18494a40fa669
ViPiven <vitalijjpiven777@gmail.com>
2021-04-09 09:40:17 +0300

    'header'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__burger-icon').classList.toggle('active');
+  document.querySelector('.header__nav').classList.toggle('animate');
+});
