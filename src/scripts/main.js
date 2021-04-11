45865bfd4ce15bebdc77d1ca8fb959193645b309
Igor Lisogor <illisogori@gmail.com>
2021-04-12 00:14:12 +0300

    First page.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const menuPage = document.getElementById('menu-page');
+const menuLink = document.getElementById('header__menu-link');
+const menuCloseLink = document.getElementById('menu-page__close-link');
+
+menuLink.addEventListener('click', onClickEventHandler);
+menuCloseLink.addEventListener('click', onClickEventHandler);
+
+function onClickEventHandler(event) {
+  if (menuPage.style.display === 'block') {
+    menuPage.style.display = 'none';
+  } else {
+    menuPage.style.display = 'block';
+  }
+};
