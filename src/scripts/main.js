47a7039a2d25ac174943a83eea80ad41eff0783d
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-04-12 20:33:11 +0300

    changes in header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const title = document.querySelector('navigation__bar');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  // title.classList.add('navigation__bar--active');
+  // title.classList.remove('navigation__bar--active');
+  title.classList.toggle('title--active');
+});
