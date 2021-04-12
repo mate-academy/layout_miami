857fa6c5f199cea6f74f66e5aa3c0529a13489f0
Nazar Kudatsky <divipi1996@icloud.com>
2021-04-12 23:12:10 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+(function() {
+  const burger = document.querySelector('.burger');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger--active');
+  });
+}());
