93e9cb6034ea6122d8a59fcc98b0ec6f2d543cca
Vadym Liubych <v.liubych@ukr.net>
2021-04-12 18:58:48 +0300

    add layout header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const burger = document.querySelector('.header-top__btn');
+const hidden = document.querySelector('.nav--hidden');
+const modal = document.querySelector('.header');
+const title = document.querySelector('.header-baner__title');
+const phone = document.querySelector('.header-top__btn--phone');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('header-top__btn--active');
+  hidden.classList.toggle('nav--hidden');
+  modal.classList.toggle('header--active');
+  title.classList.toggle('header-baner__title--none');
+  phone.classList.toggle('header-top__btn--hidden');
+});
