4de0dfa05c93212bc44a4c5b960c79d457c9a9a5
Taras Konovalov <taras.konovalov2014@gmail.com>
2021-04-12 10:00:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelector('.header__burger').addEventListener('click', function() {
+  document.querySelector('.header__burger-icon').classList.toggle('active');
+  document.querySelector('.header__burger').classList.toggle('active');
+  document.querySelector('.header__menu').classList.toggle('animate');
+});
