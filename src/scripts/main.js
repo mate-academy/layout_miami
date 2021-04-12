0a99e3b78e2a4402031c886958c6495dc3fe55c2
avgusta92 <avgusta92@gmail.com>
2021-04-13 01:17:03 +0300

    not completely solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const navigation = document.querySelector('#navigation');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  navigation.classList.toggle('nav--active');
+});
