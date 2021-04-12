4aee448089f3c906de57b1a2d21a4e1beec3caf6
Dmytro Abram <dmytro.study@gmail.com>
2021-04-12 19:42:36 +0300

    header and nav blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
+
 'use strict';
+
+const menu = document.querySelector('.menu');
+const openButton = document.querySelector('.header__burger-menu-toggler');
+const closeButton = document.querySelector('.nav__close');
+
+openButton.addEventListener('click', () => {
+  menu.classList.add('menu--active');
+}
+);
+
+closeButton.addEventListener('click', () => {
+  menu.classList.remove('menu--active');
+}
+);
