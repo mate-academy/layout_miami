26acd8e0f1c22b69e015dddf56c46f9c240389ec
Leonid Vegera <leonid.vegera@gmail.com>
2021-04-12 15:13:08 +0300

    Improved structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// const title = document.querySelector('.title');
+// const toggler = document.querySelector('#toggler');
+// toggler.addEventListener('click', () => {
+//   // title.classList.add('title--active');
+//   // title.classList.remove('title--active');
+//   title.classList.toggle('title--active');
+// });
