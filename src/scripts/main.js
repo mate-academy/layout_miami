ef9ddc347e5860447d9cddee2da22cd26da8577c
Nikolay Butylkov <kokos.ibo.58@gmail.com>
2021-04-13 15:39:19 +0300

    lost burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const menu = document.querySelector('.menu__togler');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  menu.classList.toggle('menu__main--active');
+});
