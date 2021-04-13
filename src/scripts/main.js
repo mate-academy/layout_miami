e7778c50a8f26ecc4334c3c90342d24db1f78f18
Volodymyr Myronenko <v.mironenko01@gmail.com>
2021-04-13 13:54:41 +0300

    Miami Header 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
-const header = document.querySelector('.header');
-
 const menu = document.querySelector('.header__menu');
+
 const navigation = document.querySelector('.nav');
 
 const toggler = document.querySelector('#toggler');
-
-toggler.addEventListener('click', () => {
-  header.classList.toggle('header--active');
-});
 
 toggler.addEventListener('click', () => {
   menu.classList.toggle('header__menu--active');
