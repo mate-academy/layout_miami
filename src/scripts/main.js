52f71913f1b3a36215fe75160560cc00418435a3
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-04-13 17:27:48 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,10 @@
 'use strict';
 
-const navIcon = document.querySelector('.nav__icon');
-const menu = document.querySelector('.nav__menu');
+const toggler = document.querySelector('#toggler');
+const nav = document.querySelector('.nav');
 
-navIcon.addEventListener('click', show);
-
-function show() {
-  menu.style.display = 'flex';
-
-  navIcon.removeEventListener('click', show);
-  navIcon.addEventListener('click', hide);
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+  toggler.classList.toggle('header__menu--active');
 }
-
-function hide() {
-  menu.style.display = 'none';
-
-  navIcon.removeEventListener('click', hide);
-  navIcon.addEventListener('click', show);
-}
-
-const headerIcon = document.querySelector('.phone__icon');
-const headerNumber = document.querySelector('.phone__number');
-
-headerIcon.addEventListener('click', opasityOn);
-
-function opasityOn() {
-  headerNumber.style.opacity = '100';
-
-  headerIcon.removeEventListener('click', opasityOn);
-  headerIcon.addEventListener('click', opasityOff);
-}
-
-function opasityOff() {
-  headerNumber.style.opacity = '0';
-  headerIcon.removeEventListener('click', opasityOff);
-  headerIcon.addEventListener('click', opasityOn);
-}
+);
