4cf8d2d14790577ea8fd12413ea0a25cbf9a9754
Grygoriy Shytikov <gregorysgm@gmail.com>
2021-04-13 15:22:29 +0300

    headre and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const title = document.querySelector('.header__title');
+const toggler = document.querySelector('#nav-toggler');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav__active');
+  toggler.classList.toggle('toggler--active');
+  title.classList.toggle('title--active');
+});
