373a25a07856bb50f174f9bb363c04f7a2aa195c
avgusta92 <avgusta92@gmail.com>
2021-04-13 21:35:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,16 @@
 
 const navigation = document.querySelector('#navigation');
 const toggler = document.querySelector('#toggler');
+const navLinks = document.querySelectorAll('.nav__link');
 
 toggler.addEventListener('click', () => {
   navigation.classList.toggle('nav--active');
+  toggler.classList.toggle('header__nav-icon--close');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navigation.classList.remove('nav--active');
+    toggler.classList.toggle('header__nav-icon--close');
+  });
 });
