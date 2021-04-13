164ea422913cb36e177eaa8f00f8945919706a87
Eva <eva@MacBook-Air-Eva.local>
2021-04-13 19:25:01 +0300

    some changes for bem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const burger = document.querySelector('.header-top__menu--burger');
+const burger = document.querySelector('.header-top__menu-burger');
 const menu = document.querySelector('.header__menu--nav');
 
 burger.addEventListener('click', () => {
-  burger.classList.toggle('header-top__menu--burger--active');
+  burger.classList.toggle('header-top__menu-burger--active');
   menu.classList.toggle('nav--active');
 });
