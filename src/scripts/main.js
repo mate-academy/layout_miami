f2363d93acb322b618232aa1790fe289f03384a3
Maria Yasevina <maria.yasevina@gmail.com>
2021-04-13 16:16:11 +0300

    fix for 1-3 and 4-6 added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,17 @@
 window.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('.nav');
   const hamburger = document.querySelector('.nav__hamburger');
+  const navItem = document.querySelectorAll('.nav__link');
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('nav__nav__hamburger_active');
     nav.classList.toggle('nav_active');
+  });
+
+  navItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('nav__nav__hamburger_active');
+      nav.classList.toggle('nav_active');
+    });
   });
 });
