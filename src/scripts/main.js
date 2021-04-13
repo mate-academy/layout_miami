32c783092314938fc5e9be6235a51f5921ea652c
Volodymyr Myronenko <v.mironenko01@gmail.com>
2021-04-13 09:40:40 +0300

    Miami Header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const header = document.querySelector('.header');
+
+const menu = document.querySelector('.header__menu');
+const navigation = document.querySelector('.nav');
+
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  header.classList.toggle('header--active');
+});
+
+toggler.addEventListener('click', () => {
+  menu.classList.toggle('header__menu--active');
+});
+
+toggler.addEventListener('click', () => {
+  // title.classList.add('title--active');
+  // title.classList.remove('title--active');
+  navigation.classList.toggle('header__nav--active');
+});
+
+
+
+
