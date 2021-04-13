7ab861252b796ed3eb03ae3f64431595d2eba9bc
Stas Dukach <stasdukach@gmail.com>
2021-04-13 13:36:10 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 document.querySelector('.header__burger').addEventListener('click', function() {
   document.querySelector('.header__burger-icon').classList.toggle('active');
   document.querySelector('.header__burger').classList.toggle('active');
