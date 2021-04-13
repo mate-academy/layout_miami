661351f6c90995adc9bbb669e7f040512f8b25d1
Serhii Karapota <s.karapota@gmail.com>
2021-04-13 08:43:24 +0300

    solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+(function() {
+  const burger = document.querySelector('.nav__icon');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('nav__icon--active');
+  });
+}());
