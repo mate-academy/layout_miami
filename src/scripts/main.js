bb06effb726f7b926ff8ad2fcb7bac4d87164031
Nazar Kudatsky <divipi1996@icloud.com>
2021-04-13 12:28:45 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,10 @@
 
 (function() {
   const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav');
 
   burger.addEventListener('click', () => {
     burger.classList.toggle('burger--active');
+    nav.classList.toggle('nav--active');
   });
 }());
