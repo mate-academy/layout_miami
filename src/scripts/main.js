7e1aed535157302f7db67d739699a2ba6a32baea
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-04-14 12:21:59 +0300

    solved issue with header content centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const title = document.querySelector('navigation__bar');
-const toggler = document.querySelector('#toggler');
+const sidebar = document.querySelector('.sidebar');
+const toggler = document.querySelector('#nav-toggler');
 
 toggler.addEventListener('click', () => {
-  // title.classList.add('navigation__bar--active');
-  // title.classList.remove('navigation__bar--active');
-  title.classList.toggle('title--active');
+  sidebar.classList.toggle('sidebar--active');
 });
