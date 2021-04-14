5573860bfd0a1697600b70f833e110a295a736f7
ViPiven <vitalijjpiven777@gmail.com>
2021-04-14 14:52:15 +0300

    'miami'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__burger-icon').classList.toggle('active');
+  document.querySelector('.header__top').classList.toggle('active');
+  document.querySelector('.header__nav').classList.toggle('animate');
+});
