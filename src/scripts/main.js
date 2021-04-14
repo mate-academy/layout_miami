94aa58e05502a3862e63ee939cc5f05fbc9525f2
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-04-14 12:37:33 +0300

    layout_miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const sidebar = document.querySelector('.sidebar');
+const toggler = document.querySelector('#nav-toggler');
+
+toggler.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar--active');
+});
