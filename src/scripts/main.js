676ddc4e41ac46c5964b876a50e32789201b36cb
amithos <alex.theutos@gmail.com>
2021-04-14 11:09:07 +0300

    desktop version was finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const $ = window.$;
+
+$(document).ready(function() {
+  const menu = $('.header__menu');
+
+  const toggleMenuButton = () => {
+    if (menu.hasClass('header__menu--active')) {
+      menu.addClass('header__menu--static');
+      menu.removeClass('header__menu--active');
+    } else {
+      menu.addClass('header__menu--active');
+      menu.removeClass('header__menu--static');
+    }
+  };
+
+  menu.click(function() {
+    $('.navigation').toggleClass('navigation--down');
+    toggleMenuButton();
+  });
+
+  const phone = $('.header__phone-box');
+  const phoneNumber = $('.header__number');
+
+  const togglePhone = () => {
+    phoneNumber.toggleClass('header__number--toggler');
+  };
+
+  phone.mouseover(togglePhone);
+  phone.mouseout(togglePhone);
+});
