6ade06761481f77c7e64c448528fc39c70e78b20
Nikolay Butylkov <kokos.ibo.58@gmail.com>
2021-04-14 18:22:54 +0300

    header 100%, main 100%, gallery 50%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const menu = document.querySelector('.menu__togler');
-const toggler = document.querySelector('#toggler');
+const menu = document.querySelector('.menu__list');
+const toggler = document.querySelector('.menu__img');
 
 toggler.addEventListener('click', () => {
-  menu.classList.toggle('menu__main--active');
+  menu.classList.toggle('menu__list--active');
 });
