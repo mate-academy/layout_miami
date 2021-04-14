852eb592e7c14e3647197b664a7b30273d5819e2
Sasha Chornyi <sasha-202@inbox.ru>
2021-04-14 16:10:52 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const menu = document.querySelector('.nav__menu-full');
+const toggler = document.querySelector('#nav-toggler');
+
+toggler.addEventListener('click', () => {
+  menu.classList.toggle('nav__menu-full--active');
+});
