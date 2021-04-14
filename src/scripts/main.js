293ef570228f7c9b32b852923fd8bcd78fff23f3
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-04-14 13:27:54 +0300

    added close-button to burger-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,9 @@
 
 const sidebar = document.querySelector('.sidebar');
 const toggler = document.querySelector('#nav-toggler');
+const label = document.querySelector('.navigation__toggler-label');
 
 toggler.addEventListener('click', () => {
   sidebar.classList.toggle('sidebar--active');
+  label.classList.toggle('navigation__toggler--close');
 });
