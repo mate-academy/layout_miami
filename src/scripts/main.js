fa22aa422df04e3986ae707f7a0f66bfd56cc9da
Olga Tsarova <alita.jet@gmail.com>
2021-04-15 10:46:19 +0300

    all done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const toggler = document.querySelector('.header__nav-toggler');
+const menu = document.querySelector('.nav');
+
+const toggleMenu = () => {
+  toggler.classList.toggle('header__nav-toggler--active');
+  menu.classList.toggle('nav--active');
+  page.classList.toggle('page--fixed');
+};
+
+toggler.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
