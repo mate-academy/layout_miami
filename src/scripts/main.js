ac65244dd4ad2ee79a7f70969a753e18ea0949eb
Dmytro Yurchyk <yurchik.d.work@gmail.com>
2021-06-10 18:47:04 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
+/* eslint-disable no-undef */
 'use strict';
+
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.burger-menu').toggleClass('active');
+  });
+});
+
+$(document).ready(function() {
+  $('.burger-menu__item').click(function(event) {
+    $('.header__burger.active,.burger-menu.active').toggleClass('active');
+  });
+});
