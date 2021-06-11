9d9bf5e1e333a22f406a0f9d02010154c9bcd79a
Kandiak Oleg <kandio1604@gmail.com>
2021-06-11 09:29:46 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+document.querySelector('.header__menu').onclick = function() {
+  document.querySelector('.nav').classList.toggle('hide');
+  document.querySelector('.header__menu').classList.toggle('close');
+};
