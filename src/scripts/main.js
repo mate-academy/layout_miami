4cd9ccebba36e8c93265dba697ecb926040a240f
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-06-14 12:21:16 +0300

    Miami v1 + burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const hamburger = document.querySelector('.header__menu');
+const menu = document.querySelector('.header__nav-container');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('header__menu--active');
+  hamburger.classList.toggle('header__menu--passive');
+  menu.classList.toggle('header__nav-container--active');
+});
