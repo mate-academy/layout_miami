7855e2afbd25b793cff27db47f5de89d477340a9
Taras <hevko.tars@gmail.com>
2021-06-14 18:43:46 +0300

    implement header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burger = document.querySelector('.header__burger');
+const links = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+  links.classList.toggle('header__nav--active');
+  burger.classList.toggle('header__burger--active');
+});
