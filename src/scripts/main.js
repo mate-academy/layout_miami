3465fc96792dd3a25331b2cf873736a2b9963c98
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-06-14 16:18:55 +0300

    all blocks are added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const toggler = document.querySelector('.header__toggler');
+const nav = document.querySelector('.header__nav');
+const pageContent = document.querySelector('.page__content');
+const navList = document.querySelector('.nav__list');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('header__nav--active');
+  toggler.classList.toggle('header__toggler--active');
+  pageContent.classList.toggle('page__content--hidden');
+});
+
+navList.addEventListener('click', () => {
+  nav.classList.remove('header__nav--active');
+  pageContent.classList.remove('page__content--hidden');
+  toggler.classList.toggle('header__toggler--active');
+});
