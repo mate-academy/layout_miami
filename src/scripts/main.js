d0421eadd565e7e53de163a782bb3501dead45d3
vlepekha <vlepekha@student.ucode.world>
2021-06-15 20:01:06 +0300

    added gallery section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const label = document.querySelector('.header__label');
+const callSection = document.querySelector('.header__call');
+
+label.addEventListener('click', () => {
+  callSection.classList.toggle('header__call--hidden');
+});
