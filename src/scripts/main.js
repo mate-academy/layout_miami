ac24a9768f6514e7bd01a856c520fb9bb55c5f65
Михаил <mihay0113@gmail.com>
2021-06-15 17:37:04 +0300

    autocomplete and form fixed + UI fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+const inputs = document.querySelectorAll('.form__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '""';
+  }
+});
