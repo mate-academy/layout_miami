5ccc148d56e6881b7c2792b03e7bd328cfefd638
David Dohru <tronsmartmy@gmail.com>
2021-06-15 13:34:32 +0300

    1/6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const buttonOpenClose = document.querySelector('.main__header-menu');
+const nav = document.querySelector('.main__header-nav');
+
+buttonOpenClose.addEventListener('click', () => {
+  buttonOpenClose.classList.toggle('main__header-menu--active');
+  nav.classList.toggle('main__header-nav--active');
+});
