b4b5f0732bd6c5809b6d6bcee00a2ad9ae42d3c8
Sergei Trofimov <feast.st@gmail.com>
2021-06-15 15:56:59 +0300

    Added header section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menuOpen = document.querySelector('.header-menu');
+const nav = document.querySelector('.page__nav');
+const menuClose = document.querySelector('.nav__close-btn');
+const navLink = document.querySelector('.nav__list');
+
+menuOpen.addEventListener('click', () => {
+  nav.classList.add('page__nav-is-active');
+});
+
+menuClose.addEventListener('click', () => {
+  nav.classList.remove('page__nav-is-active');
+});
+
+navLink.addEventListener('click', () => {
+  nav.classList.remove('page__nav-is-active');
+});
