198d654cf110d6c6930aa6cb85096cdc2d32932f
Dmytro Yurchyk <yurchik.d.work@gmail.com>
2021-06-15 21:14:07 +0300

    added some suggestions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,27 @@
 
 $(document).ready(function() {
   $('.header__burger').click(function(event) {
-    $('.header__burger,.burger-menu,.header__logo').toggleClass('active');
+    $('.header__burger,.burger-menu,.header__logo,html').toggleClass('active');
   });
 });
 
 $(document).ready(function() {
   $('.burger-menu__item,.burger-menu__logo-img').click(function(event) {
-    $('.header__burger.active,.burger-menu.active,.header__logo.active')
+    $(
+      // eslint-disable-next-line max-len
+      '.header__burger.active,.burger-menu.active,.header__logo.active,html.active')
       .toggleClass('active');
   });
+});
+
+const form = document.querySelector('.input');
+const inputs
+  = document.querySelectorAll('.input__name,.input__email,.input__message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
 });
