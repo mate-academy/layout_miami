1e1e16174bc65b6e8ddefa95ba2683e2f03d9058
NYaremchuk <nazarmails@gmail.com>
2021-06-15 16:41:05 +0300

    gallery bug fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+});
