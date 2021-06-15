11c5df9adb51f3beb6823177774618960ba29c16
vlepekha <vlepekha@student.ucode.world>
2021-06-15 21:22:23 +0300

    added testimonials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,14 @@
 
 const label = document.querySelector('.header__label');
 const callSection = document.querySelector('.header__call');
+const navLinks = document.querySelectorAll('.header__link');
 
 label.addEventListener('click', () => {
   callSection.classList.toggle('header__call--hidden');
 });
+
+for (const link of navLinks) {
+  link.addEventListener('click', () => {
+    document.querySelector('.header__toggler').checked = false;
+  });
+}
