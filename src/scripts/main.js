9ffdaae3784763521923df763145b71f67e8abcd
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-06-15 18:52:58 +0300

    Miami all 2d review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const form = document.querySelector('.consult');
+
+const inputs = document.querySelectorAll('.consult__name');
+
+// const inputs = form.elements
+
+const messages = document.querySelectorAll('.consult__message');
+
+for (const message of messages) {
+  message.value = '';
+}
+
+// const message = form.elements
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  for (const message of messages) {
+    message.value = '';
+  }
+});
