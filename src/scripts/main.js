c46de8dad2a21d7370b0be20c39a3aaaf0403009
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-06-15 11:23:07 +0300

    "cursor: pointer" was added to menu button, forms method changet to POST

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ const toggler = document.querySelector('.header__toggler');
 const nav = document.querySelector('.header__nav');
 const pageContent = document.querySelector('.page__content');
 const navList = document.querySelector('.nav__list');
+const button = document.querySelector('.contacts__button');
+const inputs = document.querySelectorAll('.contacts__input');
 
 toggler.addEventListener('click', () => {
   nav.classList.toggle('header__nav--active');
@@ -15,4 +17,10 @@ navList.addEventListener('click', () => {
   nav.classList.remove('header__nav--active');
   pageContent.classList.remove('page__content--hidden');
   toggler.classList.toggle('header__toggler--active');
+});
+
+button.addEventListener('click', () => {
+  inputs.forEach((input) => {
+    input.value = '';
+  });
 });
