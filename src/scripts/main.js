7e6e9aa3344ca44d527dbbdab69514e9572525de
Kandiak Oleg <kandio1604@gmail.com>
2021-06-15 10:58:30 +0300

    add reset form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const menu = document.querySelector('.header__menu');
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.value');
 
 document.querySelector('.header__menu').onclick = function() {
   document.querySelector('.nav').classList.toggle('nav-hide');
   menu.classList.toggle('header__menu-close');
 };
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
