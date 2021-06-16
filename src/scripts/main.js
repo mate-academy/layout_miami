94bdda27de5dd193533dbc9b4427079631a24ad0
NYaremchuk <nazarmails@gmail.com>
2021-06-16 18:36:08 +0300

    add cursor pointer and indentations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const form = document.querySelector('.form');
-const inputs = document.querySelectorAll('.form__input');
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__input');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
