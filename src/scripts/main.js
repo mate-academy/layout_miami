8f3396df998ef8a717ce9474bd5e15aa328c3620
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-06-16 12:51:58 +0300

    Miami all. Updates: menu on mobile landscape, other comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,7 @@ const form = document.querySelector('.consult');
 
 const inputs = document.querySelectorAll('.consult__name');
 
-// const inputs = form.elements
-
 const messages = document.querySelectorAll('.consult__message');
-
-for (const message of messages) {
-  message.value = '';
-}
-
-// const message = form.elements
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
