fa2df1d7efedb702ae594eaa92efe7a03e1aeb03
vlepekha <vlepekha@student.ucode.world>
2021-06-16 13:21:13 +0300

    added contacts section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const label = document.querySelector('.header__label');
 const callSection = document.querySelector('.header__call');
 const navLinks = document.querySelectorAll('.header__link');
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.contacts__input');
 
 label.addEventListener('click', () => {
   callSection.classList.toggle('header__call--hidden');
@@ -11,5 +13,14 @@ label.addEventListener('click', () => {
 for (const link of navLinks) {
   link.addEventListener('click', () => {
     document.querySelector('.header__toggler').checked = false;
+    callSection.classList.toggle('header__call--hidden');
   });
-}
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
