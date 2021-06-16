78d8297f06a19aca7b2573af10abc0fd39e07806
VadymMakevytss <vadymlat@mail.ru>
2021-06-16 17:16:38 +0300

    code refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const toggler = document.querySelector('.header__toggler');
+const nav = document.querySelector('.nav');
+
+toggler.addEventListener('click', (events) => {
+  nav.classListadd('red');
+});
