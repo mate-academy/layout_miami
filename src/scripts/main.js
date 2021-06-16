d369481b0c82de837949c23981952a8aca1d053a
stasianich <spechenuy@gmail.com>
2021-06-16 21:39:32 +0300

    Header, home and gallary sections were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const navContainer = document.querySelector('.nav-container');
+
+hamburger.addEventListener('click', () => {
+  navContainer.classList.toggle('nav-container--active');
+  hamburger.classList.toggle('hamburger--active');
+});
+
+const headerNav = document.querySelector('.nav');
+const burgerButton = document.querySelector('.hamburger');
+
+headerNav.addEventListener('click', (event) => {
+  burgerButton.click();
+});
