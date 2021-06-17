f305c401abf535b8eb9eebc73ad81bf109b489ed
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-06-17 14:50:19 +0300

    small fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,17 @@
 
 const burger = document.querySelector('.burger');
 const burgerIcon = document.querySelector('.header__burger');
+const menuList = document.querySelectorAll('.burger__link');
 
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('header__burger--active');
   burger.classList.toggle('burger--active');
 });
+
+//
+
+menuList.addEventListener('click', () => {
+  burger.classList.remove('burger--active');
+});
+
+//  чого не працює?
