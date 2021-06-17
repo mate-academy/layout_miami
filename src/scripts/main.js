6fe36be9d7bb42cb77d3c59960e53fdedeb93a3e
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-06-17 17:38:41 +0300

    done? v1.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,19 @@
 
 const burger = document.querySelector('.burger');
 const burgerIcon = document.querySelector('.header__burger');
-// const menuList = document.querySelectorAll('.burger__link');
+const menuList = document.querySelector('.burger__list');
 const form = document.querySelector('.form');
 const inputs = document.querySelectorAll('.form__input');
 
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('header__burger--active');
   burger.classList.toggle('burger--active');
+  menuList.classList.toggle('burger__link--active');
 });
 
-//
-
-// menuList.addEventListener('click', () => {
-//   burger.classList.remove('burger--active');
-// });
-
-//  чого не працює?
+menuList.addEventListener('click', () => {
+  burger.classList.remove('burger--active');
+});
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
