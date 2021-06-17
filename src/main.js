5abd967ee8e9b73d79156b40e57f13dfe11d6279
NazarYanchyshyn <yanchishin12@ukr.net>
2021-06-17 15:02:54 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@
 
 const toggler = document.querySelector('#toggler');
 const nav = document.querySelector('.nav');
+const link = document.querySelectorAll('.nav__link');
 
 toggler.addEventListener('click', () => {
   nav.classList.toggle('nav--active');
   toggler.classList.toggle('header__menu--active');
 }
 );
+
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', () => {
+    nav.classList.toggle('nav--active');
+    toggler.classList.toggle('header__menu--active');
+  }
+  );
+}
