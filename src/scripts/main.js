682bdf12d3f992b6c7c98651468e20e4e003f7df
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-06-17 14:22:31 +0300

    99.9% rdy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const burgerIcon = document.querySelector('.header__burger');
+
+burgerIcon.addEventListener('click', () => {
+  burgerIcon.classList.toggle('header__burger--active');
+  burger.classList.toggle('burger--active');
+});
