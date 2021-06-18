ae8af3ae51fbc44de3eab425977a2abc10efa4a4
John <jozaman96@gmail.com>
2021-06-18 11:04:50 +0300

    improved interactivity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const form = document.querySelector('.contacts-form');
+const burger = document.querySelector('.header-content__checkbox');
+const navList = document.querySelector('.nav__list');
 
 form.addEventListener('submit', event => {
   document.location.reload();
   event.preventDefault();
+});
+
+navList.addEventListener('click', event => {
+  burger.checked = false;
 });
