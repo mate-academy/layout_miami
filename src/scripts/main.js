a2bcc80ff4791ad174f46199778c3288d35871ed
Liudmyla Yevchuck <lol22lil22@gmail.com>
2021-06-18 13:30:19 +0300

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const checkbox = document.querySelector('.header__nav-toggler');
+const navigation = document.querySelectorAll('.nav__link');
+
+navigation.forEach(element => {
+  element.addEventListener('click', () => {
+    checkbox.checked = !checkbox.checked;
+  });
+});
