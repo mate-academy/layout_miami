ec95a525418dff14c738c246182b56267b6b6cc5
Leonid <manu31051996@gmail.com>
2021-06-29 11:24:01 +0300

    form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.contact__form');
+const inputs = document.querySelectorAll('.contact__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = ' ';
+  }
+});
