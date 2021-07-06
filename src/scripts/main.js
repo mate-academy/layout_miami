fb948af2c5c89b7edfcf44ca1792d7f8e55f33a4
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-07-06 10:02:56 +0300

    "Contact us" section on table screen fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const feedbackForm = document.querySelector('.form');
+
+feedbackForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of feedbackForm.elements) {
+    input.value = '';
+  }
+});
