10c3db4496129d0a669529a19d1f29e134b0cb27
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-21 22:07:14 +0300

    added favicon, big images compressed, fixed textarea clearing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,12 @@
 
 const form = document.querySelector('.contact-us__form');
 const inputs = document.querySelectorAll('.contact-us__input');
+const textarea = document.querySelector('.contact-us__textarea');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
+  textarea.value = '';
 
   for (const input of inputs) {
     input.value = '';
