404b0f25b1834ee582eb03090ee15d30ba2551fd
Dana Pryshelets <tanyarud53@gmail.com>
2021-07-25 17:55:39 +0300

    fixed-contact-us

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const form = document.querySelector('.contuct-us__form');
-
-form.addEventListener('submit', event => {
-  document.location.reload();
-  event.preventDefault();
-});
