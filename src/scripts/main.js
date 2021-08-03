ce2bd96dc12e105ea35e642704c3b0d127cfc1ce
tkachukione <tkachukione@gmail.com>
2021-08-04 01:34:46 +0300

    changed readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
-const form = document.querySelector(".contact__form");
-const inputs = document.querySelectorAll(".contact__input");
-const textarea = document.querySelectorAll(".contact__textarea");
+'use strict';
 
-form.addEventListener("submit", (event) => {
+const form = document.querySelector('.contact__form');
+const inputs = document.querySelectorAll('.contact__input');
+const textarea = document.querySelectorAll('.contact__textarea');
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
+
   for (const input of inputs) {
-    input.value = "";
+    input.value = '';
   }
+
   for (const text of textarea) {
-    text.value = "";
+    text.value = '';
   }
 });
-
