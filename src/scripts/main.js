2674a1ee767972e3ad3190fc5284cbf9ec3d4fd5
Loza Marusa <marusa.loza@gmail.com>
2021-09-07 13:46:47 +0300

    changed version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const body = document.querySelector('.page-body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menuCloser = document.querySelector('.menu-toggler--closer');
+
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page-body--with-menu');
+});
+
+menuCloser.addEventListener('click', () => {
+  body.classList.remove('page-body--with-menu');
+});
