1da47b7c55564c981425d8b2a5b51993a6fa04c3
Oksana Onopa <onopa.kseni@gmail.com>
2021-09-07 10:44:42 +0300

    gallery links fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
-const body = document.querySelector(".page__body");
-const menuOpener = document.querySelector(".icon--menu");
-const menuCloser = document.querySelector(".icon--cross");
-menuOpener.addEventListener("click", function() {
-  body.classList.add("page__body--with-menu")
-});
-menuCloser.addEventListener("click", () => {
-  body.classList.remove("page__body--with-menu")
-});
+// const body = document.querySelector(".page__body");
+// const menuOpener = document.querySelector(".icon--menu");
+// const menuCloser = document.querySelector(".icon--cross");
+// menuOpener.addEventListener("click", function() {
+//   body.classList.add("page__body--with-menu")
+// });
+// menuCloser.addEventListener("click", () => {
+//   body.classList.remove("page__body--with-menu")
+// });
