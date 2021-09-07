dcfdb9bd1014d5f082d1e499fcd3c9b39db6842b
Oksana Onopa <onopa.kseni@gmail.com>
2021-09-07 14:16:58 +0300

    scroll fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,10 @@
+"use strict";
 const body = document.querySelector(".page__body");
 const menuOpener = document.querySelector(".icon--menu");
-const menuCloser = document.querySelector(".icon--cross");
+const menu = document.querySelector(".menu");
 menuOpener.addEventListener("click", function() {
-  body.classList.add("page__body--with-menu")
+  body.classList.add("page__body--with-menu");
 });
-menuCloser.addEventListener("click", () => {
-  body.classList.remove("page__body--with-menu")
+menu.addEventListener("click", () => {
+  body.classList.remove("page__body--with-menu");
 });
