21d8d708e8bec5b8b98a3e88d3d14809f7040bbf
Vitalii Strohush <vitalikstrog2003v@gmail.com>
2021-09-07 15:13:12 +0300

    js edits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const body = document.querySelector('.page__body');
 const menuOpener = document.querySelector('.menu-toggler--opener');
-const menuCloser = document.querySelector('.menu-toggler--closer');
+const menuCloser = document.querySelector('.menu');
 
 menuOpener.addEventListener('click', function() {
   body.classList.add('page__body--with-menu');
