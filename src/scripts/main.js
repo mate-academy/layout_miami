9cc798ce60684cc0751bf23d50bae73e713e05c1
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-09-07 14:54:39 +0300

    second try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menuCloser = document.querySelector('.menu-toggler--closer');
+
+menuOpener.addEventListener('click', function(){
+  body.classList.add('page__body--with-menu');
+});
+
+menuCloser.addEventListener('click', function(){
+  body.classList.remove('page__body--with-menu');
+});
