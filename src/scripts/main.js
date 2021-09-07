5a5b22b37e47e5e4e9e41271ddca4b3c03ccf05d
Mariia Torkaienko <mary_dob@hotmail.com>
2021-09-07 14:10:30 +0300

    Miami with hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menuCloser = document.querySelector('.menu-toggler--closer');
+const menu = document.querySelector('.menu__list');
+
+menuOpener.addEventListener('click', () => {
+  body.classList.add('page__body--with-menu');
+});
+
+menuCloser.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
+
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
