31918aec9f263ec0060cd14e0bb8a3a06c40063b
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-09-07 13:11:31 +0300

    Done Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.top-menu__opener');
+const menuCloser = document.querySelector('.menu-close');
+const menu = document.querySelector('.menu__list');
+
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page__body--menu-open');
+});
+
+menuCloser.addEventListener('click', () => {
+  body.classList.remove('page__body--menu-open');
+});
+
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--menu-open');
+});
