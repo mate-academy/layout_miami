0abd1b1b365521cba59284d6724386ea5940227b
Tati <tnch313@gmail.com>
2021-09-08 13:11:50 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.icon--menu');
+const menu = document.querySelector('.menu');
+
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page__body--with-menu');
+});
+
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
