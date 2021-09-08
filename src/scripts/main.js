5b63d43179e6b8292275b8f8c4ef8e1617152c18
Jane Verbitskaya <jvzns17@gmail.com>
2021-09-08 17:31:28 +0300

    menu scroll was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menuCloser = document.querySelector('.menu-toggler--closer');
+
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page__body--with-menu');
+});
+
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
