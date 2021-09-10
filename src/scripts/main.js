a0395e377112ac9a7f3344158fbe6f1b661e42f3
darynakravchuk <darinakravchuk@gmail.com>
2021-09-10 15:34:38 +0300

    second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menu = document.querySelector('.menu');
+
+// We disable page scroll when openning the menu
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page__body--with-menu');
+});
+
+// We enable page scrolling after closing the menu or pressing a nav link
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
