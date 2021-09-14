7e4713a358de9770fc03a523bbd3bfa74a44fe28
Taras Ulko <internalpeace10@outlook.com>
2021-09-14 22:54:11 +0300

    fixed links and default margin for title header and fixed scrolling page with opened menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.icon--menu');
+const menuCloser = document.querySelector('.icon--cross');
+const menuLink = document.querySelectorAll('.menu__link');
+
+menuOpener.addEventListener('click', () => {
+  body.classList.add('page__body--with-menu');
+});
+
+menuCloser.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
+
+menuLink.forEach(link => {
+  link.addEventListener('click', () => {
+    body.classList.remove('page__body--with-menu');
+  });
+});
