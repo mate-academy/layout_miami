39875ad4af8142fbac18da5cd0f6ecb060fe1c3a
Dima Krasnoboky <81876541+krasnobokydima@users.noreply.github.com>
2021-09-14 10:28:33 +0300

    miami v2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menu = document.querySelector('.menu');
+
+menuOpener.addEventListener('click', function() {
+  body.classList.add('page__body--with-menu');
+});
+
+menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
