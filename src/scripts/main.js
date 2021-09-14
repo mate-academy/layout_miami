25dfce1e8b687d546aee59cba107b9aef9b10f66
caelum29 <caelum29@gmail.com>
2021-09-14 15:12:23 +0300

    overflow fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const opener = document.querySelector('.header__menu-opener');
+const closer = document.querySelector('.icon--cross');
+const body = document.querySelector('.page__body');
+
+opener.addEventListener('click', () => {
+  body.classList.add('page__body--with-menu');
+});
+
+closer.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
