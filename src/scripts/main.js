210c214489c4c19b252bd059f952c39a8140dbd2
Bogdan <bogdanemails@gmail.com>
2021-09-15 13:21:53 +0300

    bugs fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,16 @@ const body = document.querySelector('.page__body');
 const menuOpener = document.querySelector('.icon--menu');
 const menu = document.querySelector('.menu__list');
 
+const closeMenuButton = document.getElementById('close-menu-button');
+
 menuOpener.addEventListener('click', function() {
   body.classList.add('page__body--with-menu');
 });
 
 menu.addEventListener('click', () => {
+  body.classList.remove('page__body--with-menu');
+});
+
+closeMenuButton.addEventListener('click', () => {
   body.classList.remove('page__body--with-menu');
 });
