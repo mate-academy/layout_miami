787ab9c5c6c5419d60032fd3da990c17adaf3484
Olena Kopyl <olenatriasii@gmail.com>
2021-09-27 12:59:56 +0300

    changes to js the file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(event) {
+  event.preventDefault();
+  form.reset();
+}
