2b71343851b9b8f3f3b602ff60ffb5d9700a86e0
Alina Kostiuk <alina.hariaha@gmail.com>
2021-09-27 11:37:19 +0300

    Add JS file.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('hashchange', () => {
+  if (location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleEvent);
+
+function handleEvent(event) {
+  event.preventDefault();
+  form.reset();
+}
