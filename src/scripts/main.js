7d33f51a60d8d3f00a26dc858ec6f4e62ac92465
Olexandr Yatsykovych <sashayatsikovic@gmail.com>
2021-10-04 15:59:46 +0300

    fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const form = document.querySelector('.contact-us__form');
+
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(event) {
+  event.preventDefault();
+  form.reset();
+}
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
