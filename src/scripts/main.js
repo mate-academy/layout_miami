2f88d115c51217976cfa7962c73c5d459e1db7b2
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-10-09 23:10:33 +0300

    Miami solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
+
 const body = document.querySelector('.page__body');
 const menuOpener = document.querySelector('.icon--menu');
-const menuCloser = document.querySelector('.icon--cross');
+const menuCloser = document.querySelector('.menu');
 
 menuOpener.addEventListener('click', function() {
-  body.classList.add('page__body--with-menu')
+  body.classList.add('page__body--with-menu');
 });
 
 menuCloser.addEventListener('click', () => {
-  body.classList.remove('page__body--with-menu')
+  body.classList.remove('page__body--with-menu');
 });
