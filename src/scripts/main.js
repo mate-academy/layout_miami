79b78719d6bdda405c44458c5e8fef9f2417f11e
Vitaliy Voytovich <voitovichatque@gmail.com>
2021-10-28 20:31:58 +0300

    Tabindex, required fields, menu adaptation were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,9 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    document.querySelector('main').style.visibility = 'hidden';
   } else {
     document.body.classList.remove('page__body--with-menu');
+    document.querySelector('main').style.visibility = 'visible';
   }
 });
