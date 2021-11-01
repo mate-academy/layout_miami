8119c0c3db9cc040088bda22661e6e45131f2b04
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-11-01 17:25:03 +0200

    changes according to review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,26 @@
 'use strict';
 
+const disableFocusFor = document.querySelectorAll('[data-focus="disabled"]');
+const enableFocusFor = document.querySelectorAll('[data-focus="enabled"]');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+
+    disableFocusFor.forEach(disableFocus);
+    enableFocusFor.forEach(enableFocus);
   } else {
     document.body.classList.remove('page__body--with-menu');
+
+    disableFocusFor.forEach(enableFocus);
+    enableFocusFor.forEach(disableFocus);
   }
 });
+
+function disableFocus(el) {
+  el.setAttribute('tabindex', '-1');
+}
+
+function enableFocus(el) {
+  el.removeAttribute('tabindex');
+}
