dd78f1f3ef7398d47e659f9e2003c998abec029f
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-11-21 14:14:25 +0200

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+menu.addEventListener('keydown', (event) => {
+  if (event.keyCode === 9) {
+    event.preventDefault();
+  }
+});
