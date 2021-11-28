8b59c5ed24991905f146e21bc0f58b63790336c6
KHTRE <KHTREE@gmail.com>
2021-11-28 17:07:14 +0300

    All works.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
+  document.onkeydown = function(t) {
+    if (t.which === 9) {
+      return false;
+    }
+  };
+});
