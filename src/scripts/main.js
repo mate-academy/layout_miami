7df98986a63f9d8ec71e25a4c182fa2a349eb1f0
SavelovSerhii <savelov12111@gmail.com>
2021-12-01 15:13:47 +0200

    After checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const main = document.getElementById('main');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+
+    main.setAttribute('hidden', 'true');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+
+    main.removeAttribute('hidden');
+  }
+});
