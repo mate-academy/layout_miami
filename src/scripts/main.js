9bf26a7b2a8137beb6475b76352c567bb6d27ebe
KHTRE <KHTREE@gmail.com>
2021-12-01 17:10:17 +0200

    good tab js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,21 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+
+    document.onkeydown = function(key) {
+      if (key.which === 9) {
+        return false;
+      }
+    };
+
   } else {
     document.body.classList.remove('page__body--with-menu');
+
+    document.onkeydown = function(key) {
+      if (key.which === 9) {
+        return true;
+      }
+    };
   }
 
-  document.onkeydown = function(t) {
-    if (t.which === 9) {
-      return false;
-    }
-  };
 });
