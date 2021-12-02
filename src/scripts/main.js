2fef4b65b92e792d34f78d507d46db77ebf4902b
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2021-12-02 13:16:42 +0200

    Miami v1.00

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('submit').addEventListener(
+  'click', stopDefAction, false
+);
