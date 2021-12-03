b770c32ea4161777392986cad4e94962fc092073
Nataliia Hrynyk <nataliiahrynyk2001@gmail.com>
2021-12-03 02:08:59 +0200

    disabled reload on submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,8 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function handleForm(event) {
+  event.preventDefault();
+}
+document.getElementById('contacts-form').addEventListener('submit', handleForm);
