77ee10aba17ffca48174aef6bf2a9ff31191281f
Pavlo Melnyk <pashamelnyk95@gmail.com>
2021-12-03 12:19:23 +0200

    beta version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,19 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+
+    document.onkeydown = function(key) {
+      if (key.which === 9) {
+        return false;
+      }
+    };
   } else {
     document.body.classList.remove('page__body--with-menu');
+
+    document.onkeydown = function(key) {
+      if (key.which === 9) {
+        return true;
+      }
+    };
   }
 });
