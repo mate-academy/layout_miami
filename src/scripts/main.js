76fb6f5f41af66b05b5e413aa33e0c18b4373b8b
Andrii Koptiev <koptev.andrey87@gmail.com>
2021-12-06 13:01:17 +0200

    final_fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-function stopDefAction(evt) {
-  evt.preventDefault();
-}
-
-document.getElementById('button-act').addEventListener(
-  'click', stopDefAction, false
-);
