20c466a48261830f400b53e8d4c88529f298ab59
Mikedacheka <mikedacheka@gmail.com>
2021-12-07 11:53:55 +0200

    sm fixes with form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,16 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.form-field');
+const textarea = document.querySelector('.form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+  textarea.value = '';
+});
