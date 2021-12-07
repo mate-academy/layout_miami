dd6d63149f9d4883cb8f59a024f024e4c2f2719c
izzilowe <izzilowe7@gmail.com>
2021-12-07 10:19:48 +0200

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,16 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__form-input');
+const textarea = document.querySelector('.form-field--textarea');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // eslint-disable-next-line no-return-assign
+  inputs.forEach(elem => elem.value = '');
+
+  textarea.value = '';
+});
