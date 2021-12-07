da329f64233d9483e7e2798c089d57fb1da6325b
Nick Datsiuk <perronogrande@gmail.com>
2021-12-07 14:52:37 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,18 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-var form = document.getElementById("form");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+// var form = document.getElementById("form");
+// function handleForm(event) { event.preventDefault(); }
+// form.addEventListener('submit', handleForm);
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+const textarea = document.querySelector('.contact-us__field--last');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => elem.value = '');
+
+  textarea.value = '';
+});
