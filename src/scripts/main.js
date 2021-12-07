3664ad94e07f7beebabddd4111b9f1f8b0d789b9
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2021-12-07 10:28:01 +0200

    miami form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
 
-    document.onkeydown = function(key) {
+    document.onkeydown = function (key) {
       if (key.which === 9) {
         return false;
       }
@@ -12,10 +12,21 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__body--with-menu');
 
-    document.onkeydown = function(key) {
+    document.onkeydown = function (key) {
       if (key.which === 9) {
         return true;
       }
     };
+  }
+});
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (let input of inputs) {
+    input.value = ''
   }
 });
