9bde7ec4ce53443c6416cf59d84eae2ef51b8244
Oleksii Yatsyna <cogitoergoviveret@gmail.com>
2021-12-07 11:53:56 +0200

    miami v5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector('.contact-us__field');
+const inputs = document.querySelectorAll('.contact-us__field');
+const textarea = document.querySelector('.contact-us__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  textarea.value = '';
+});
