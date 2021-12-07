8e1f7118c8e30f2b8caad71dfdf9ff7aae60a253
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2021-12-07 13:27:23 +0200

    fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach((elem) => {
+    elem.value = '';
+  });
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+
+    document.onkeydown = (e) => {
+      if (e.which === 9) {
+        return false;
+      }
+    };
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+
+    document.onkeydown = (e) => {
+      if (e.which === 9) {
+        return true;
+      }
+    };
+  }
+});
