9213f974a0b6d5fa1818d4c572e37e1595c4756b
Orest Mykhailyshyn <orest.mp@gmail.com>
2021-12-07 13:53:00 +0200

    with preventDefault

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+// const textarea = document.querySelector('.form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
