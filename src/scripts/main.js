518d435cd6e9810fb905e6870bf754a22bd02161
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-12-29 14:56:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  const gallery = document.querySelector('.gallery');
+
+  if (window.location.hash === '#menu') {
+    gallery.style.display = 'none'; // push the 'Tab' and .gallery display: none
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+    gallery.style.display = 'block'; // .gallery demonstrate
+  }
+});
