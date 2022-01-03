fa9bb94a2f9a2681cc6f7b142e6f74c7cfdfec65
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-03 14:43:56 +0200

    seems close, scripts not working(((, need q&a...

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.getElementById('formButton')
+  .addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.getElementById('page').classList.add('page--with-menu');
+    document.querySelector('#menu').focus();
+  } else {
+    document.getElementById('page').classList.remove('page--with-menu');
+  }
+});
