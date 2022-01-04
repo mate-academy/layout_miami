8dd64dbed78b0e588f249037eceaf91134f83a1a
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-04 12:29:39 +0200

    fixed(no) phone icon, logo hrefs, still cant fix overflow and tab(((

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ document.getElementById('formButton')
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.getElementById('page').classList.add('page--with-menu');
-    document.querySelector('#menu').focus();
+    document.querySelectorAll('body > div:not(.menu)').tabIndex = -1;
   } else {
     document.getElementById('page').classList.remove('page--with-menu');
   }
