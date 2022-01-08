88720ae41629d93b53f30813054a2e2e2f4328d1
Kostya Kovbel <dovkerdota@gmail.com>
2022-01-08 20:04:18 +0200

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const elem = document.getElementById('gallery');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    elem.style.display = 'none';
   } else {
     document.body.classList.remove('page__body--with-menu');
+    elem.style.display = 'block';
   }
 });
+
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault();
+}, false);
