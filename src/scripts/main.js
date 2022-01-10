2afddff361eca25a9e1f8fec00f3e3a9c1f49155
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-01-10 13:51:14 +0200

    miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.getElementById('contactForm')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    document.getElementsByName('name')[0].value = '';
+    document.getElementsByName('email')[0].value = '';
+    document.getElementsByName('message')[0].value = '';
+  });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
