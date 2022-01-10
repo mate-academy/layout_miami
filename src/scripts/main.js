67bc0413a6d2a31ae1fd0ffd45e0aa1220881ace
Kostya Kovbel <dovkerdota@gmail.com>
2022-01-10 12:16:51 +0200

    T_T

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,10 @@
 'use strict';
 
-const elem = document.getElementById('gallery');
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
-    elem.style.display = 'none';
   } else {
     document.body.classList.remove('page__body--with-menu');
-    elem.style.display = 'block';
   }
 });
 
