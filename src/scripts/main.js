d873c039a68a243e5a483b821551dcb6b3af16c3
Roman Kozhukhovskyi <4kroman@gmail.com>
2022-01-11 17:11:59 +0200

    fix errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,13 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('form').addEventListener('submit',
+  function(e) {
+    e.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+  }
+);
