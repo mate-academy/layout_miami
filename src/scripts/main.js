330bda8e2cca5f6d938b5ecca063ed80fa449d14
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-01-11 16:11:57 +0200

    miami1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelector('#form').addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash !== '#menu') {
+    document.body.classList.remove('page__body--withMenu');
+  } else if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--withMenu');
+  }
+});
