b00a3a2e982024ed87b9dde62c3b3dcb77f9e5a7
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-12 22:05:36 +0200

    miami save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.getElementById('sent').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('text').value = '';
+  }
+);
