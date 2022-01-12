152d9cbc3fcfccd345c4893ad01cb110a23bcf1d
Lena Utkina <littleplushka@gmail.com>
2022-01-12 10:42:25 +0200

    first draft #3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('submitForm');
+
+function handleForm(event) {
+  event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
