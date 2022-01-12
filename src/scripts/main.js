595d8d07792367f6f2ac39a3969fcc1bec9026c6
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-13 00:51:37 +0200

    miami save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+document.getElementById('nav-menu').addEventListener('click', function() {
+  document.getElementById('html-gen').style.overflow = 'hidden';
+});
+
+document.getElementById('nav-click').addEventListener('click', function() {
+  document.getElementById('html-gen').style.overflow = 'visible';
+});
+
 document.getElementById('sent').addEventListener('submit',
   function(event) {
     event.preventDefault();
