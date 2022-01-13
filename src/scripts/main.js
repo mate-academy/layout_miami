7d56be3ebf43c46db780faac6bbbe74fc805403f
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-01-13 09:01:44 +0200

    save after fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+document.querySelector('#consult').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+  }
+);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
