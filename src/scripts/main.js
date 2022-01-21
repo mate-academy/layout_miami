8020995865876474db83d781f1f8add7a5ef06fa
KatrukEvgeny <zaiaz_@i.ua>
2022-01-21 16:03:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,28 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const emailForm = document.getElementById('get-user-info');
+const nameInput = document.getElementById('user-name');
+const emailInput = document.getElementById('user-mail');
+const messageInput = document.getElementById('user-message');
+
+const userMessage = {};
+
+emailForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  userMessage[nameInput.name] = nameInput.value;
+  userMessage[emailInput.name] = emailInput.value;
+
+  if (messageInput.value) {
+    userMessage[messageInput.name] = messageInput.value;
+  } else {
+    userMessage[messageInput.name]
+    = nameInput.value + ' didn\'t left message for us';
+  }
+
+  emailInput.value = '';
+  nameInput.value = '';
+  messageInput.value = '';
+});
