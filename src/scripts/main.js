b01f007b0c713352574568b782a955c7b0bc5e1c
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-01-29 11:20:18 +0200

    Fix bugs and add form listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('#contact-form')
+  .addEventListener('submit', function(event) {
+    document.querySelector('#submit-button').animate([
+      { background: '#fff' },
+      { background: '#62ff6f' },
+      { background: '#fff' },
+    ],
+    {
+      delay: 600,
+      duration: 600,
+    }
+    );
+
+    event.preventDefault();
+  }, false);
