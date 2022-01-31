f241c1253e37ae24750e34bd104a9c375fc8f507
Nikita Tkach <dev.tkach@gmail.com>
2022-02-01 00:21:24 +0200

    Created Miami landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Disabling page scrolling
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__menu--with-menu');
+  } else {
+    document.body.classList.remove('page__menu--with-menu');
+  }
+});
+
+// Prevent page refrech on form
+
+document.querySelector('#submit-button').addEventListener(
+  'click',
+  function(event) {
+    event.preventDefault();
+  },
+  false
+);
