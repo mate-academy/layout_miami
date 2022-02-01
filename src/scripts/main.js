cb724080a47f42f351a0f110334e290776c1c820
Mary <anokhinamasha27@gmail.com>
2022-02-01 16:56:38 +0200

    header done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuBtnOpen = document.querySelector('.burger-menu');
+const menuBtnClose = document.querySelector('.menu__closer');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+function toggleMenu() {
+  document.body.classList.toggle('page--with-menu');
+  menu.classList.toggle('page--menu-open');
+}
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
