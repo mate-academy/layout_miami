1967658aa20b209f57929a8e64907c5a4fce6f90
Dmytro Pelovych <falcon.patron@gmail.com>
2022-02-02 17:59:30 +0200

    but animation added, also page hover effects improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const button = document.querySelector('.button--submit');
+const form = document.querySelector('.contacts__form');
+
+form.addEventListener('submit', (event) => {
+  button.animate(
+    [
+      {
+        background: '#fff',
+        transform: 'scale(1)',
+      },
+      {
+        background: '#9d9d9d',
+        transform: 'scale(0.95)',
+      },
+      {
+        background: '#fff',
+        transform: 'scale(1)',
+      },
+    ],
+    {
+      duration: 300,
+    });
+
+  event.preventDefault();
+  form.reset();
+});
+
+/*
+const menuLink = document.querySelector('.menu__link');
+const menuLinkContent = menuLink.textContent;
+console.log(menuLinkContent);
+
+menuLink.onclick = () => {
+  document.title = menuLinkContent;
+}
+*/
