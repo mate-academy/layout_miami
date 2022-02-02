8613ec18e16beb12258d2c81baca001db216271f
Nazar Ostafiichuk <ostafii4uk@gmail.com>
2022-02-02 14:45:06 +0200

    Header not scroling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ form.addEventListener('submit', (event) => {
 
   window.location.href = 'page';
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
