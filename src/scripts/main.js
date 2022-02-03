7f6d3555e951cb1ee1d142b0158cada36b3bc52f
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-03 07:18:57 +0200

    header hover & form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  form.reset();
+  e.preventDefault();
+  window.location.href = '#';
+});
