f1e2465c4ff1a85b483d27fd14acb697e7119122
Alexxs <irreligious86@gmail.com>
2022-02-04 00:19:01 +0200

    set all img sizes without grid for 1024+ screen

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const menuButton = document.querySelector('.header__menu-opener');
-const closeMenuButton = document.querySelector('.popup__close');
-const menu = document.querySelector('.popup');
-
-const showMenu = () => menu.classList.add('popup--active');
-const hideMenu = () => menu.classList.remove('popup--active');
-
-menuButton.addEventListener('click', () => showMenu());
-closeMenuButton.addEventListener('click', () => hideMenu());
+// const menuButton = document.querySelector('.header__menu-opener');
+// const closeMenuButton = document.querySelector('.popup__close');
+// const menu = document.querySelector('.popup');
+//
+// const showMenu = () => menu.classList.add('popup--active');
+// const hideMenu = () => menu.classList.remove('popup--active');
+//
+// menuButton.addEventListener('click', () => showMenu());
+// closeMenuButton.addEventListener('click', () => hideMenu());
