f8707e297b4875182f13c55a11bc984b4cd7b137
Anton Hanetskyi <antonganetskiy@gmail.com>
2022-02-03 20:41:08 +0200

    script from menu was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ form.addEventListener('submit', (event) => {
 
   window.location.href = '#';
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
