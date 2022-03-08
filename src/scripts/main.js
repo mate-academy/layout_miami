0be368414b32f60259d960793778902ecdd5e0f6
yurii837 <gorogotskiy@gmail.com>
2022-03-08 19:07:18 +0200

    done1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// disable menu scroll
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// disable reloding when submit form
+
+const form = document.getElementById('form');
+
+function handleForm(event) {
+  event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
