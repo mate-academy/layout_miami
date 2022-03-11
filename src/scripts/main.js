3fa4ccdb598726d42a2ce6caf95f32fa7fad93b5
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-03-11 17:57:17 +0200

    changes were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function stopDefAction(event) {
+  document.getElementById('consultationForm').reset();
+  event.preventDefault();
+}
+
+document.getElementById('consultationForm').addEventListener(
+  'submit', stopDefAction, false
+);
