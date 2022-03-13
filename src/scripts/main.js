d442602787dc82403ad6977b5e0a3064fd97c2af
Helen Petrovska <paznachka@gmail.com>
2022-03-13 11:54:21 +0200

    landing was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const formButton = document.querySelector('.button--primary');
+const form = document.querySelector('form');
+
+formButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  form.reset();
+});
