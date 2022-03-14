ef96678bed9fb731bc5d65048e1d94eca40d79aa
Kirill Polozenko <kir.polozenko@icloud.com>
2022-03-14 14:38:44 +0200

    miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefAction(event) {
+  document.getElementById('consultationForm').reset();
+  event.preventDefault();
+}
+
+document.getElementById('consultationForm').addEventListener(
+  'submit', stopDefAction, false
+);
