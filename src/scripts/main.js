2181560f1b333d9e64d0eb9103f547194af1cb7f
Misha Kravtsov <aniahucm@gmail.com>
2022-03-19 17:52:52 +0200

    fix links, reset event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('contact-us-form').addEventListener(
+  'submit',
+  (event) => {
+    document.getElementById('contact-us-form').reset();
+    event.preventDefault();
+  },
+  false
+);
