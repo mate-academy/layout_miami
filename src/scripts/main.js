aecd478275f51b1cd69e1e15fa9c33efbf9ce8f6
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-03-23 12:57:09 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function stopDefAction(event) {
+  document.getElementById('form').reset();
+  event.preventDefault();
+}
+
+document.getElementById('form').addEventListener(
+  'submit', stopDefAction, false
+);
