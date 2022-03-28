0145c98b5d2bfb79f74821a9c9c0cb9d0859878e
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-03-28 21:36:15 +0300

    small fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ function stopDefAction(event) {
 document.getElementById('form').addEventListener(
   'submit', stopDefAction, false
 );
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
