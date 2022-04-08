6682e321b8e87068dd69ecf5b2b8d4fbbb76d86b
Hubert Wolak <29hihub@gmail.com>
2022-04-08 06:50:37 +0200

    Mobile v develop

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const nav = document.querySelector('.nav');
-const bgc = document.querySelector('.header');
+const bgc = document.querySelector('.header__wrapper');
 const toggler = document.querySelector('.header__burger');
 const cross = document.querySelector('.header__cross');
 const title = document.querySelector('.header__title');
 
 toggler.addEventListener('click', function() {
   nav.style.display = 'block';
-  bgc.style.backgroundColor = '#e8eff9';
+  bgc.style.background = '#e8eff9';
   cross.style.display = 'inline-block';
   toggler.style.display = 'none';
   title.style.display = 'none';
@@ -16,7 +16,8 @@ toggler.addEventListener('click', function() {
 
 cross.addEventListener('click', function() {
   nav.style.display = 'none';
-  bgc.style.backgroundColor = 'transparent';
+  bgc.style.background = 'none';
+  bgc.style.backgroundImg = 'url("/images/photos/background.png")';
   cross.style.display = 'none';
   toggler.style.display = 'inline-block';
   title.style.display = 'block';
