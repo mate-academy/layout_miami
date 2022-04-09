ecaa62c57887966aeabcf32c33bb922442b29a86
Dima Cherednyk <dicherednyk@gmail.com>
2022-04-09 14:51:46 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const button = document.querySelector('.button--primary');
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
