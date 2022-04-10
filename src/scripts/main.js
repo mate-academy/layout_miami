28c8392af632d8fa874fb0d7aea2a05f19b171be
Mfanomutsha Ncube <mfanomutsha@gmail.com>
2022-04-10 21:10:00 +0200

    Completed the overlay menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-// 'use strict';
+'use strict';
+
+const menu = document.querySelector('#menuOverlay');
+const navLink = document.querySelectorAll('.nav__link');
+const openMenuIcon = document.querySelector('.header__menu-toggler');
+const closeMenuIcon = document.querySelector('.header__menu-hide');
+
+function openMenu(e) {
+  menu.classList.add('header__menu--is-active');
+  e.preventDefault();
+}
+
+function closeMenu(e) {
+  menu.classList.remove('header__menu--is-active');
+  e.preventDefault();
+}
+
+openMenuIcon.addEventListener('click', openMenu, true);
+closeMenuIcon.addEventListener('click', closeMenu, true);
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu, true));
