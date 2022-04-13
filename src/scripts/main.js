40614f5a5c904c376e6a241baca4c05f81275531
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-13 14:00:43 +0200

    made further changes but couldnt fix form issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+});
