7b1ba0fc8a204f6d04886bf43466417a9ef1e144
Karina Prozner <prozner.karina@gmail.com>
2022-04-13 20:21:07 +0200

    added non working slide menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
