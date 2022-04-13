1545507fb1adc8a5fa5b3625eca803fbb1bfb522
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2022-04-14 01:19:23 +0200

    added functional navigation panel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
-document.getElementById('#button')
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+document.getElementById('button')
   .addEventListener('click', function(element) {
     element.preventDefault();
   });
