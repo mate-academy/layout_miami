896fdae1defe4732aa96ed51533f0c678e7ea3b1
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-04-14 11:02:46 +0200

    js fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+  window.scrollTo(0, 0);
+});
