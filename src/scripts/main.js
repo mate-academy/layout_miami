195f1918a7b460217f46cdef249b0b9a0ad6e8e4
Wojtek <wojciech.cz.bor@gmail.com>
2022-04-14 22:06:03 +0200

    added working menu with slight jitter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
