40a33640d753cf8f785d8290c03a8156c09bf0b6
Hubert Wolak <29hihub@gmail.com>
2022-04-14 14:59:39 +0200

    Adjustments

--- src/script.js
+++ src/script.js
@@ -6,15 +6,18 @@ const toggler = document.querySelector('.header__burger');
 const cross = document.querySelector('.header__cross');
 const title = document.querySelector('.header__title');
 const phone = document.querySelector('.header__phone');
+const body = document.querySelector('body');
 
 toggler.addEventListener('click', function() {
   nav.style.display = 'block';
   bgc.style.background = '#e8eff9';
-  bgc.style.height = 'fit-content';
+  bgc.style.height = '100vh';
+  bgc.style.justifyContent = 'start';
   cross.style.display = 'inline-block';
   toggler.style.display = 'none';
   title.style.display = 'none';
   phone.style.display = 'none';
+  body.style.overflow = 'hidden';
 });
 
 cross.addEventListener('click', function() {
@@ -22,15 +25,20 @@ cross.addEventListener('click', function() {
   bgc.style.background = 'none';
   bgc.style.height = '100%';
   bgc.style.backgroundImg = 'url("/images/photos/background.png")';
+  bgc.style.justifyContent = 'space-between';
   cross.style.display = 'none';
   toggler.style.display = 'inline-block';
   title.style.display = 'block';
   phone.style.display = 'inline-block';
+  body.style.overflow = 'scroll';
 });
 
+/*
 const button = document.querySelector('#submitButton');
 
-button.addEventListener('click', function(event) {
+button.addEventListener('submit', function(event) {
   window.alert('preventDeafault', 1);
   event.preventDefault();
 }, false);
+
+*/
