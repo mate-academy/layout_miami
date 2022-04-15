adb6d84a96cdcb7dd13b98c81b64e06bc1d097af
DominoSl <artemenko.mik@gmail.com>
2022-04-15 17:41:11 +0300

    create Bose landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', ev => ev.preventDefault());
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
