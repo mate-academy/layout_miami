4a58a3efb3c4ef3eddf7bbe88ba348033735901b
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-18 17:54:55 +0300

    add favicon | remove images above menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,13 @@
 const contactForm = document.querySelector('#contact-us__form');
 
 contactForm.addEventListener('submit', ev => ev.preventDefault());
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+    document.querySelector('.content').hidden = true;
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+    document.querySelector('.content').hidden = false;
+  }
+});
