ed69f772c9cdf3e126bd3045cd2fdfb9eb22440c
Daria Liubymova <dellachaise@gmail.com>
2022-04-19 01:58:13 +0300

    Added changes according to checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.querySelector('#form-button').addEventListener(
+  'click', function(event) {
+    event.preventDefault();
+  }, false);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
