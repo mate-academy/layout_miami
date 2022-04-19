127aac51cd9b26ce227d692f0e3a6b9039d9f131
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-19 18:38:12 +0300

    fixed menu links activity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,7 @@ contactForm.addEventListener('submit', ev => ev.preventDefault());
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
-    document.querySelector('.content').hidden = true;
   } else {
     document.body.classList.remove('page__body--with-menu');
-    document.querySelector('.content').hidden = false;
   }
 });
