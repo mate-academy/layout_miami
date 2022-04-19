67ef3d752bfbcf685f879c2471300af74a69ff9d
Lebogang Mogane <mogane420@gmail.com>
2022-04-19 10:59:26 +0200

    added hovers on images and preventDefault in the form

--- src/scripts/preventDefault.js
+++ src/scripts/preventDefault.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const sendButton = document.querySelector('.form__button');
-
-sendButton.onclick = function(e) {
+function preventFunction(e) {
   e.preventDefault();
-};
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function() {
+  preventFunction();
+});
