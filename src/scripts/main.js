16aed1de38dacc946a7ab9cee67511d017bccdcd
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-04-21 21:10:20 +0300

    clear the form, prevent the default action on submitting the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__item');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
