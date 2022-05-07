6cdd23f361cd8d56a77fa5bcf0425d56bac3793d
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-05-07 16:40:14 +0300

    BOSE landing page was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.getElementById('submit_form').addEventListener('click', (event) => {
+  event.preventDefault();
+});
