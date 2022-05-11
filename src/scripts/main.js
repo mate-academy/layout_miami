2e6ee7eb1419e2afa2fe7c36d051ba3d6138461f
Bohdan Dubravin <dubravin111@gmail.com>
2022-05-11 12:38:12 +0300

    Bike website

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menuOpener = document.querySelector('.icon--menu');
+const menuCloser = document.querySelector('.icon--cross');
+const slideMenu = document.querySelector('.page__menu');
+const listItems = document.querySelector('.menu__list');
+
+menuOpener.addEventListener('click', () => {
+  slideMenu.classList.add('page__menu--open');
+});
+
+menuCloser.addEventListener('click', () => {
+  slideMenu.classList.remove('page__menu--open');
+});
+
+listItems.addEventListener('click', () => {
+  slideMenu.classList.remove('page__menu--open');
+});
