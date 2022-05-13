f2a53fa3dcb7f4ed064a749006e11751f1be8978
Bohdan Dubravin <dubravin111@gmail.com>
2022-05-13 19:17:59 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,9 @@ const menuOpener = document.querySelector('.icon--menu');
 const menuCloser = document.querySelector('.icon--cross');
 const slideMenu = document.querySelector('.page__menu');
 const listItems = document.querySelector('.menu__list');
+const submitButton = document.querySelector('.button--submit');
+const popupMessage = document.querySelector('.sended-form');
+const bookTestRide = document.querySelector('.menu__call--action');
 
 menuOpener.addEventListener('click', () => {
   slideMenu.classList.add('page__menu--open');
@@ -15,4 +18,27 @@ menuCloser.addEventListener('click', () => {
 
 listItems.addEventListener('click', () => {
   slideMenu.classList.remove('page__menu--open');
+});
+
+bookTestRide.addEventListener('click', () => {
+  slideMenu.classList.remove('page__menu--open');
+});
+
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+
+  const emailForm = document.querySelector('.input__field--email').value;
+  const nameForm = document.querySelector('.input__field--name').value;
+
+  if (emailForm === '' || nameForm === '') {
+    return;
+  }
+
+  document.querySelector('.contact-us__user-input').reset();
+
+  popupMessage.hidden = false;
+
+  setTimeout(function() {
+    popupMessage.hidden = true;
+  }, 1000);
 });
