3983fb7571a06ccc5f999286f0d1b137104fec21
Illia Vodonis <illavodonis@gmail.com>
2022-05-25 16:47:52 +0300

    MyBike

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function preventDef(event) {
+  event.preventDefault();
+}
+
+function removeHandler() {
+  document.getElementById('formBtn').removeEventListener('click',
+    preventDef, false);
+}
+
+removeHandler();
