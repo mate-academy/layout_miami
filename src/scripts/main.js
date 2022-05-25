ce064ce3b624921d60b29eba399a0370f830b45f
Ihor Kuchin <vedunigor@gmail.com>
2022-05-25 16:29:06 +0300

    Added task Miami (MyBike)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+(function addHeaderToPageWhenScroll() {
+  const header = document.querySelector('#header');
+  const sectionHome = document.querySelector('#home');
+  const headerHeight = header.clientHeight;
+  const sectionHomeHeight = sectionHome.clientHeight;
+  const headerModifier = 'page__header--scroll';
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY >= (sectionHomeHeight - headerHeight)) {
+      header.classList.add(headerModifier);
+    } else {
+      header.classList.remove(headerModifier);
+    }
+  });
+})();
