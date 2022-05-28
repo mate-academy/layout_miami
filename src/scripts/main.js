19d97713fa76f40f3a572147354b00e7cd5462e3
Luyda Andrikevych <l.andrikevych@gmail.com>
2022-05-28 15:29:21 +0200

    were added animation for images + fixed grid layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const form = document.getElementById('form');
+
+form.onsubmit = function() {
+  document.getElementById('form').reset();
+
+  return false;
+};
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
