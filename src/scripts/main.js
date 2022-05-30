8cb69cecf2fd153e3f128e20d1ef389db631e58d
Kate Khon <katerynakhon@gmail.com>
2022-05-30 13:34:14 +0300

    fixes applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  form.reset();
+  event.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
