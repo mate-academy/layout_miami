c5a132327575b87a7dede77ce31f0b928ec438f9
DanaKhromenko <khromenkodana@gmail.com>
2022-05-31 23:01:53 +0300

    Initial layout_miami commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefaultAction(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('call-me-back-button').addEventListener('click',
+  stopDefaultAction, false);
