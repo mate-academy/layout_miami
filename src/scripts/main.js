621e3b8fb047951c915bfceab044e3e55a22646e
Illia Bielichenko <ibelich12@gmail.com>
2022-06-02 18:34:48 +0300

    fixes was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu');
+  } else {
+    document.body.classList.remove('menu');
+  }
+});
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
