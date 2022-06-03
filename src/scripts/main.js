ed5395e13f4a2fad37ada82e8b5ebf507a059a26
DanaKhromenko <khromenkodana@gmail.com>
2022-06-03 15:47:47 +0300

    Added a form clean after submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const submitButton = document.getElementById('call-me-back-button');
+const formsToClean = document.querySelectorAll('.form-field');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -10,7 +13,10 @@ window.addEventListener('hashchange', () => {
 
 function stopDefaultAction(evt) {
   evt.preventDefault();
+
+  for (const formToClean of formsToClean) {
+    formToClean.value = '';
+  }
 }
 
-document.getElementById('call-me-back-button').addEventListener('click',
-  stopDefaultAction, false);
+submitButton.addEventListener('click', stopDefaultAction);
