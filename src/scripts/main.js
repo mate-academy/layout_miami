c86c2600e66a5dfd5b7964ff7a274af2eb3b5ebe
Kostiantyn <havryshkostya@gmail.com>
2022-06-08 17:43:34 +0400

    some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const submitButton = document.querySelector('.button--submit');
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+});
