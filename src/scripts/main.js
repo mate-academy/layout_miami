422cfe8b4451fc96eb0aa402e0ec62b0ac3e6d9c
Anton Chetveryk <chetverykantonma@gmail.com>
2022-06-11 17:20:29 +0300

    miami page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const menuBtn = document.querySelector('.icon--menu');
+const menuBtns = document.querySelectorAll('.menu__item');
+const closeBtn = document.querySelector('.icon--croos');
+const menu = document.querySelector('.menu');
+
+function onClick() {
+  menu.classList.add('open');
+}
+
+function closeMenu() {
+  menu.classList.remove('open');
+}
+
+[...menuBtns].forEach(e => {
+  e.addEventListener('click', closeMenu);
+});
+
+menuBtn.addEventListener('click', onClick);
+closeBtn.addEventListener('click', closeMenu);
