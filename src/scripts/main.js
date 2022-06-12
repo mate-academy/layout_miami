97eb1c1e92445c56b096ac8b0c4504bbad03a77e
Oleg Vetrov <oleg94038@gmail.com>
2022-06-12 11:22:18 +0300

    Header worked, slider-adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const btnDown = document.getElementsByClassName('nav__icon');
+const btnUp = document.getElementsByClassName('nav__icon-cross');
+const par = document.getElementsByClassName('nav__menu');
+
+btnDown[0].onclick = function() {
+  par[0].classList.add('nav__menu--active');
+};
+
+btnUp[0].onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
