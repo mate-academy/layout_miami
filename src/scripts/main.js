b8a471dae5e054c50835b5d47766ff934fe49248
Oleg Vetrov <oleg94038@gmail.com>
2022-06-13 20:57:51 +0300

    Aligned with the checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global $ */
 
 const btnDown = document.getElementsByClassName('nav__icon');
 const btnUp = document.getElementsByClassName('nav__icon-cross');
@@ -11,3 +12,14 @@ btnDown[0].onclick = function() {
 btnUp[0].onclick = function() {
   par[0].classList.remove('nav__menu--active');
 };
+
+$('#form').submit(function(e) {
+  e.preventDefault();
+
+  $.ajax({
+    success: function() {
+      document.getElementById('form').reset();
+      document.getElementById('send-mail').innerHTML = 'Message send!';
+    },
+  });
+});
