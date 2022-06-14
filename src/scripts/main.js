a07be143aa6676223ba8260c9ee158b6cc8e263d
Anton Chetveryk <chetverykantonma@gmail.com>
2022-06-14 10:57:05 +0300

    fixed bugs according to checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,29 @@ const menuBtn = document.querySelector('.icon--menu');
 const menuBtns = document.querySelectorAll('.menu__item');
 const closeBtn = document.querySelector('.icon--croos');
 const menu = document.querySelector('.menu');
+const submit = document.getElementById('submit');
+const body = document.querySelector('.page__body');
 
-function onClick() {
+function openMenu() {
   menu.classList.add('open');
+  body.classList.add('page__body--with-menu');
 }
 
 function closeMenu() {
   menu.classList.remove('open');
+  body.classList.remove('page__body--with-menu');
+}
+
+function onSubmit(e) {
+  if (document.querySelector('form').checkValidity()) {
+    e.preventDefault();
+  }
 }
 
 [...menuBtns].forEach(e => {
   e.addEventListener('click', closeMenu);
 });
 
-menuBtn.addEventListener('click', onClick);
+menuBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
+submit.addEventListener('click', onSubmit);
