2ff4094fb8f8f73fdda2dfb1d409a36e6ff81041
Oleg Vetrov <oleg94038@gmail.com>
2022-06-15 14:48:43 +0300

    Fixes navigation The menu now closes when navigating the page. When the menu is open, scrolling is disabled.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,39 @@
 /* global $ */
 
 const btnDown = document.getElementsByClassName('nav__icon');
-const btnUp = document.getElementsByClassName('nav__icon-cross');
 const par = document.getElementsByClassName('nav__menu');
+const closeMenuCross = document.getElementById('close-m-cross');
+const closeMenuHome = document.getElementById('close-m-home');
+const closeMenuAbout = document.getElementById('close-m-about');
+const closeMenuGallery = document.getElementById('close-m-gallery');
+const closeMenuTestimonials = document.getElementById('close-m-testimonials');
+const closeMenuContacts = document.getElementById('close-m-contacts');
 
 btnDown[0].onclick = function() {
   par[0].classList.add('nav__menu--active');
 };
 
-btnUp[0].onclick = function() {
+closeMenuCross.onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
+
+closeMenuAbout.onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
+
+closeMenuGallery.onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
+
+closeMenuTestimonials.onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
+
+closeMenuContacts.onclick = function() {
+  par[0].classList.remove('nav__menu--active');
+};
+
+closeMenuHome.onclick = function() {
   par[0].classList.remove('nav__menu--active');
 };
 
@@ -22,4 +47,12 @@ $('#form').submit(function(e) {
       document.getElementById('send-mail').innerHTML = 'Message send!';
     },
   });
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
