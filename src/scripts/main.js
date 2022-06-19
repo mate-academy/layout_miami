1c2acf295e6f4ba7c1c24d3f6f9ce43c4ed2244e
Claudia Kalnitskaya <kalnitskii.family@gmail.com>
2022-06-19 15:01:21 +0300

    version to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.getElementById('form').addEventListener('submit',
+  function(e) {
+    e.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+  }
+);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
