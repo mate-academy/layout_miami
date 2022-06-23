95d58945c8fe2892370aadfbee8ad8698069a4b2
dasha <dashasv76@gmail.com>
2022-06-23 16:01:37 +0300

    contact us is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const buttons = document.querySelectorAll('.menu-opener');
-const menu = document.querySelector('.menu');
-const header = document.querySelector('.header__content');
+// const buttons = document.querySelectorAll('.menu-opener');
+// const menu = document.querySelector('.menu');
+// const header = document.querySelector('.header__content');
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    menu.classList.toggle('show');
-    header.classList.toggle('close');
-  });
-});
+// buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     menu.classList.toggle('show');
+//     header.classList.toggle('close');
+//   });
+// });
