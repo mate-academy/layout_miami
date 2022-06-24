761b451e1977a41a885cb81c3a2319d8cf431d13
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-06-24 13:47:03 +0300

    last-child added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+const form = document.querySelector(".contact-us__form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  form.reset();
+    window.location.href = "#";
+});
