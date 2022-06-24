86c7a617de3cb21c53f6fc2d71dc723d7c5d60cc
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-06-24 12:07:02 +0300

    added js script to clear fields after submit, requered fields attribute restored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,20 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__field');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  form.reset();
+  for (const input of inputs) {
+    input.value = '';
+  }
 });
