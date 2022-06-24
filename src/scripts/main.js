fdceb96939e9da1bc580d731c61fa23b4b23e0a3
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-06-24 11:32:41 +0300

    fixed form and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+  window.location.href = '#header';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
