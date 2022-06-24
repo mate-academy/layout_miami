0f79d6c37e6f61d3c742b85b603bed4b760bb65b
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-06-24 13:47:53 +0300

    lint errors fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 'use strict';
-const form = document.querySelector(".contact-us__form");
 
-form.addEventListener("submit", function (event) {
+const form = document.querySelector('.contact-us__form');
+
+form.addEventListener('submit', function(event) {
   event.preventDefault();
   form.reset();
-    window.location.href = "#";
+  window.location.href = '#';
 });
