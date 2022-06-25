6e9bfb5488c6acb8482ccbce72ca13091cfa1654
Kolya Movchan <kmovchan261@gmail.com>
2022-06-25 14:14:59 +0300

    JS corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu')
+  } else {
+    document.body.classList.remove('page__body--with-menu')
+  }
+});
+
+document.querySelector("#submit").addEventListener("click", function(event) {
+  document.getElementById("output-box").innerHTML += "Sorry! Currently the form may not be submitted!<br>";
+  event.preventDefault();
+  }, false);
+  
