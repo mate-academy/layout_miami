4b1876458eb6b790eef2af5ed12e581581ee741a
Volodya <balyshev.volodya@gmail.com>
2022-06-25 13:55:59 +0300

    contact-us section added, cosmetic fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+
+  window.location.href = '#navigation';
+});
