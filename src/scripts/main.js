1e763384296dc3afc48eb7840ae7ba8aadf03a27
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-06-25 22:34:07 +0300

    Hover-effects were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+// eslint-disable-next-line
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  form.reset();
+});
