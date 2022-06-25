9056f9f5b2e321ffb61c3a806a06a9f7cb47d1d6
Viktoria Neimesh <valencia88vika@gmail.com>
2022-06-25 16:47:49 +0300

    Reload of page was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const button = document.querySelector('#submit-btn');
+const form = document.querySelector('#contacts__form');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const result = form.reportValidity();
+
+  if (result === true) {
+    form.reset();
+  }
+}, false);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
