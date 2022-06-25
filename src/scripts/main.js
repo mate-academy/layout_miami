598f977a53f8dd2d1a5401912b660fe83c39ba16
Grebenkina <grebenkina.2490@gmail.com>
2022-06-26 01:15:34 +0700

    fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const form = document.querySelector('form');
+
+function submit(event) {
+  event.preventDefault();
+
+  for (const field of document.querySelectorAll('.form-field')) {
+    field.value = '';
+  }
+}
+
+form.addEventListener('submit', submit);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
