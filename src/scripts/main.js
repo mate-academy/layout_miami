344333dba480d9f0db76ee2cab2a52f8ce24ef98
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-27 01:27:54 +0300

    fixed bug scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,12 @@ const topElement = document.querySelector('.page__body');
 const footer = document.querySelector('.footer');
 const menu = document.querySelector('.icon--menu');
 const cross = document.querySelector('.icon--cross');
+const menuList = document.querySelector('.menu__list');
 
 footer.addEventListener('click', () => topElement.scrollIntoView());
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-
   form.reset();
   topElement.scrollIntoView();
 });
@@ -23,5 +23,14 @@ function handleRemoveClass() {
   topElement.classList.remove('page__scrolle-none');
 };
 
+function handlerRemoveBlocScroll(event) {
+  const { tagName } = event.target;
+
+  if (tagName === 'A') {
+    topElement.classList.remove('page__scrolle-none');
+  }
+}
+
 menu.addEventListener('click', handleAddedClass);
 cross.addEventListener('click', handleRemoveClass);
+menuList.addEventListener('click', handlerRemoveBlocScroll);
