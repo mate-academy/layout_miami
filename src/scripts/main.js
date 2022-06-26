03d83c718053637f35da4f1dba37c9efc56a62ea
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-26 18:43:32 +0300

    fixed footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,9 @@
 
 const form = document.querySelector('.form');
 const topElement = document.querySelector('.page__body');
+const footer = document.querySelector('.footer');
+
+footer.addEventListener('click', event => topElement.scrollIntoView());
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
