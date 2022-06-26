7963327c882518330c97f2227bbbe283b8cb683b
Kate <krasnova2912@gmail.com>
2022-06-26 21:45:40 +0300

    contact us section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+  window.location.href = '#';
+});
+
+// // const form = document.querySelector('.form');
+// // const inputs = document.querySelectorAll('.input');
+
+// // form.addEventListener('submit', function(event) {
+// //   event.preventDefault();
+
+// //   for (const input of inputs) {
+// //     input.value = '';
+// //   }
+// // });
