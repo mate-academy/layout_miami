6026da4b695de030a78c3a77e67078fccc0dc2f7
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-26 18:36:14 +0300

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const topElement = document.querySelector('.page__body');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+  topElement.scrollIntoView();
+});
