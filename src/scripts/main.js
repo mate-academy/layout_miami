a666e28c42c9ee585f5f1ebd51d70a2e90091bc8
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-26 19:20:25 +0300

    fixed bug with scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@
 const form = document.querySelector('.form');
 const topElement = document.querySelector('.page__body');
 const footer = document.querySelector('.footer');
+const menu = document.querySelector('.icon--menu');
+const cross = document.querySelector('.icon--cross');
 
-footer.addEventListener('click', event => topElement.scrollIntoView());
+footer.addEventListener('click', () => topElement.scrollIntoView());
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -12,3 +14,14 @@ form.addEventListener('submit', function(event) {
   form.reset();
   topElement.scrollIntoView();
 });
+
+function handleAddedClass() {
+  topElement.classList.add('page__scrolle-none');
+};
+
+function handleRemoveClass() {
+  topElement.classList.remove('page__scrolle-none');
+};
+
+menu.addEventListener('click', handleAddedClass);
+cross.addEventListener('click', handleRemoveClass);
