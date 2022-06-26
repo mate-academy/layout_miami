6c129453e1961eeec6417c9c5898ab18f24cb8cd
Vitalii Labunskyi <vitold85@gmail.com>
2022-06-26 10:12:28 +0300

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 'use strict';
 
+// menu
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+// form
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
 });
