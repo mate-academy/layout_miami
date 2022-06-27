71025f485783e2bf75b67033fe5fda71fec78b2b
Yaroslav Lizogub <lizogub.yar@gmail.com>
2022-06-27 15:46:33 +0300

    disabled scroll when menu is open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,11 @@ form.addEventListener('submit', (event) => {
 
   window.location.href = '#';
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
