c5475ae7b6bb846bd2ee30fcd80226cff235aaf2
Volodya <balyshev.volodya@gmail.com>
2022-06-27 13:18:53 +0300

    scroll bug fixed, paddings for menu added, changed icon size for tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,11 @@ form.addEventListener('submit', function(event) {
 
   window.location.href = '#navigation';
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
