860558a03a63fecc0a4ffa7e9d0771b85fa6f09a
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-06-27 17:37:01 +0300

    removed scroll bar when menu is open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#navMenu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
