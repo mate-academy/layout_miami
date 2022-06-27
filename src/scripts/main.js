f914d4f5726f8c84bebb01396dbf5cf793c7287c
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-06-27 12:48:44 +0300

    Mistakes were corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ form.addEventListener('submit', function (event) {
 
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
