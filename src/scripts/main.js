48f673c4b3807b8027d7d0a7cd429e5dc6f2b08b
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-06-27 19:00:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.getElementById('form').addEventListener('submit',
+  function(e) {
+    e.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+  }
+);
