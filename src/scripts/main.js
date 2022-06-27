37aea5eb4decaf9f1b65f7cefe3c01141a272b52
dasha <dashasv76@gmail.com>
2022-06-27 17:56:19 +0300

    corrected bugs that Vova found

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,26 @@
 //     header.classList.toggle('close');
 //   });
 // });
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.reset();
+
+  window.location.href = '#header';
+});
+
+const footer = document.querySelector('.footer');
+
+footer.addEventListener('click', () => {
+  window.location.href = '#header';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
