e38b673e078f13bff1d22204876d0a49316727a7
Kate <krasnova2912@gmail.com>
2022-06-27 15:53:25 +0300

    fixed menu scrol and small mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ form.addEventListener('submit', function(event) {
   form.reset();
   window.location.href = '#';
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
