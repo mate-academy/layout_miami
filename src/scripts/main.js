6c77515100f59a8c621c6c8a37cf7b9d29ca435c
Yaroslav Lizogub <lizogub.yar@gmail.com>
2022-06-27 09:40:39 +0300

    added form basic listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.querySelector('.contacts__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+
+  window.location.href = '#';
+});
