4298199da18fc713d1f54db477faf5e2f4d665a7
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-06-28 13:54:20 +0300

    Added script for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
