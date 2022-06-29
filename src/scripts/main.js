9fd2d822c6e5aefb3afd5f3565ffa988e8114820
Leoned Pustovit <lrerednawl@gmail.com>
2022-06-29 18:21:09 +0300

    fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+const form = document.querySelector(".contact-us__form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  form.reset();
+  window.scrollTo(0, 0);
+});
