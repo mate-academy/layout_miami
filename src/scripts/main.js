3934ff7ca77f374a5ad330ef56a827de2e9c0a20
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-06-29 03:36:39 +0300

    Added grid, fixed form, fixed text, added menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const form = document.querySelector('.form');
-const inputs = document.querySelectorAll('.input');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  for (const input of inputs) {
-    input.value = '';
-  }
+  form.reset();
 });
