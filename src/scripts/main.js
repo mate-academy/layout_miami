0ef3f11b155c7ae5920affcc662be4926d1c81a8
Aleksei Evtushenko <hlauhalash@gmail.com>
2022-07-03 15:32:40 +0300

    added hovers to links and fixed the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
