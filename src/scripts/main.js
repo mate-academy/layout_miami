2c4d8a5795ec7c7ce0539f90789d7635acbc95c4
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-07-04 01:48:42 +0300

    Adapted all sections and added animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,14 @@
 
 const form = document.querySelector('.form');
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
