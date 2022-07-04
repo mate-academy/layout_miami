7ee6f37b4a82152fc1e865105f35a781caa5f2bf
Anastasiya <105457299+Anastasiya145@users.noreply.github.com>
2022-07-04 13:40:17 +0200

    the mistakes were changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const contactUsForm = document.querySelector('#form__contact-us');
+
+contactUsForm.addEventListener('submit', ev => {
+  ev.preventDefault();
+  contactUsForm.reset();
+});
