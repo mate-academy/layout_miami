1356f9bbaac801d26e78c364cc078b677f23060a
Bohdan Tron <ikwiczzts@gmail.com>
2022-07-06 16:25:20 +0300

    Miami with checklist features

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,8 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// checklist 15, preventDefault didnt check empty form rows, I dont know js :c
+document.getElementById('cons').addEventListener('click', function(event) {
+  event.preventDefault();
+});
