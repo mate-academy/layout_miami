089a42703defb419cb9f7264c4b4e777d506e28d
Dmytro Kretov <dmytro.kretov@gmail.com>
2022-07-11 12:30:53 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
-'use strict';
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
+  }
+});
+ 
+
+const form = document.querySelector('form-field');
+const inputs = document.querySelectorAll('contact-us__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
   }
 });
