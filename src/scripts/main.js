ae84b8b02f5f37e5f41a6dcb96c70b995533775f
Hennadii Turovskyi <tur.gtv@gmail.com>
2022-07-13 18:53:47 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
