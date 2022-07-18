6a4f48440567d6c690f47ee9f188af5c479fea4f
Misha Kostov <kostov.mihail321@gmail.com>
2022-07-18 22:04:03 +0300

    Fixed npm lint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu')
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu')
+    document.body.classList.remove('page__body--with-menu');
   }
-})
+});
