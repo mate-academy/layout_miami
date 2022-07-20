f5c95794d8134f7eee74dcb97b80b39663f833cc
sergii-nosachenko <s.nosachenko@gmail.com>
2022-07-20 17:33:25 +0300

    Fixed grid, added intro section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.getElementById('menu-toggler').addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
