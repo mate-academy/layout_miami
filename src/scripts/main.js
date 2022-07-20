094965ffeb68942f1b3674ebd1382cfca14b43a4
Kamil Witek <kamilprogramuje@gmail.com>
2022-07-21 00:49:03 +0200

    added changes from removed repo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bar = document.querySelector('.nav__icon--bar');
+const barX = document.querySelector('.menu__x-wrapper');
+const menu = document.querySelector('.menu');
+
+bar.addEventListener('click', () => {
+  menu.classList.toggle('display-flex');
+});
+
+barX.addEventListener('click', () => {
+  menu.classList.toggle('display-flex');
+});
