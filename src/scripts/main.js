1b643ddb7d2313e55e1438e06cc24c43edfda9a1
Dima Kobzar <kobzar7dima@gmail.com>
2022-07-23 15:26:09 +0300

    renaming for reuse nav-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let hiddenText = document.querySelector(".header__hide-text");
+let phoneIcon = document.querySelector(".header__call");
+
+// console.log(hiddenText, phoneIcon);
+
+// оно на всех экранах работает а мне надо только на десктопе
+// phoneIcon.addEventListener('mouseover', (event) => {
+//   console.log('hover');
+//   hiddenText.style. opacity = "1";
+// });
+
+// phoneIcon.addEventListener('mouseout', (event) => {
+//   console.log('out');
+//   hiddenText.style.opacity = "0";
+// });
+
+phoneIcon.addEventListener('mouseover', (event) => {
+  console.log('hover');
+  hiddenText.classList.add("hover")
+});
+
+phoneIcon.addEventListener('mouseout', (event) => {
+  console.log('out');
+  hiddenText.classList.remove("hover")
+});
