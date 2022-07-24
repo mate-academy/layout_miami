22c3661643e7090e76c7929fea27728671095d6c
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-07-24 19:58:19 +0300

    full with menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const scrollUp = document.querySelector('.scroll-up');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20
+    || document.documentElement.scrollTop > 20) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+}
