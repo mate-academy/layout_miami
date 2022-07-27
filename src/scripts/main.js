75de2c3d33d9a43403afc5adb138414fe4191700
Dima Kobzar <kobzar7dima@gmail.com>
2022-07-27 20:42:15 +0300

    Draft v.4 scroll fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,9 @@
 'use strict';
 
-// const hiddenText = document.querySelector('.nav-menu__contacts');
-// const phoneIcon = document.querySelector('.icon--phone');
-
-// console.log(hiddenText, phoneIcon);
-
-// оно на всех экранах работает а мне надо только на десктопе
-// phoneIcon.addEventListener('mouseover', (event) => {
-//   console.log('hover');
-//   hiddenText.style. opacity = "1";
-// });
-
-// phoneIcon.addEventListener('mouseout', (event) => {
-//   console.log('out');
-//   hiddenText.style.opacity = "0";
-// });
-
-// phoneIcon.addEventListener('mouseover', (event) => {
-//   // console.log('hover');
-//   hiddenText.classList.add('hover');
-// });
-
-// phoneIcon.addEventListener('mouseout', (event) => {
-//   // console.log('out');
-//   hiddenText.classList.remove('hover');
-// });
-
-// for (let i = 0; i < 12; i++) {
-//   for (let k = i; k < 12; k++) {
-//     console.log(i, k + 1);
-
-//   }
-// }
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-menu') {
+    document.body.classList.add('page__body--scroll-remove');
+  } else {
+    document.body.classList.remove('page__body--scroll-remove');
+  }
+});
