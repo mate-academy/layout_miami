ab882cdf4f76b3d3fd5d19d4e7a328774a3ad36f
Jamal Amoian <leonidagutin09@gmail.com>
2022-07-27 16:25:39 +0300

    Bose v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchanger', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
