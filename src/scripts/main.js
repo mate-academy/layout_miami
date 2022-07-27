499d50bd85a225b35c60c8119228d5b1adb039ed
Viktoriia Shakhovets <vka.vic@gmail.com>
2022-07-27 14:20:42 +0300

    Bose 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+
+form.addEventListener('click', (event) => {
+  event.preventDefault();
+});
