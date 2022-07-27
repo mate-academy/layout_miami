d8cc8c49b538af76de28d2ad897b011d90029ec8
Vlad Kurachenko <x0674016244@gmail.com>
2022-07-27 19:22:26 +0300

    upd submit-form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('page--with-menu');
+//   } else {
+//     document.body.classList.remove('page--with-menu');
+//   }
+// });
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
 });
