76c1a8390d51cc35ff72117c202312abe4f5705d
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-07-27 04:33:25 +0300

    form submit  was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const scrollUp = document.querySelector('.scroll-up');
+const form = document.querySelector('.form');
+const formField = document.querySelectorAll('.form__field');
+const button = document.querySelector('.form__button');
+const borderGreen = '2px solid #62e662';
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,8 +13,6 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const scrollUp = document.querySelector('.scroll-up');
 
 window.onscroll = function() {
   scrollFunction();
@@ -22,3 +26,13 @@ function scrollFunction() {
     scrollUp.style.display = 'none';
   }
 }
+
+formField.forEach(function(element) {
+  form.addEventListener('submit', function(event) {
+    element.style.border = borderGreen;
+    button.style.border = borderGreen;
+    button.textContent = 'SENT ✅';
+    button.style.fontsize = '2rem';
+    event.preventDefault();
+  });
+});
