3661ffc184f119d7494b98191ff2fa4103093dd3
Max Kravchenko <max.kravchenko@gmail.com>
2022-07-28 12:13:09 +0300

    Fix max-width, color, scale, placeholder, form inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-document.querySelector('#send').addEventListener('click', function(event) {
-  event.preventDefault();
-}, false);
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
