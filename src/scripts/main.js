da6011ffe82b55871d99f20c452dd4f64aa04c3f
Kate Novak <novak5683@gmail.com>
2022-07-28 15:56:03 +0200

    miami_task1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.querySelector('#submit').addEventListener('click', function(event) {
+  document.getElementById('output-box').innerHTML
+  += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+  event.preventDefault();
+}, false);
