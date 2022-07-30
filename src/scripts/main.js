db60c160ce6207811419d9e1492f55ccb1f08501
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-07-30 16:16:47 +0300

    mobile version added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page--with-nav');
+  } else {
+    document.body.classList.remove('page--with-nav');
+  }
+});
