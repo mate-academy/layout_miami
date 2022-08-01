ae0b6e2069bf1c94af5bfeea48554ef7ecc379bc
Vadls <vadim_tanasienko54541@ukr.net>
2022-08-01 16:54:38 +0300

    Scrollbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,74 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+    document.getElementById('main').setAttribute('style', 'display: none;');
+    // eslint-disable-next-line max-len
+    document.getElementById('header').setAttribute('style', 'margin-bottom: 0;');
+    document.getElementById('footer').setAttribute('style', 'display: none;');
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.getElementById('main').setAttribute('style', 'display: block;');
+    // eslint-disable-next-line max-len
+    document.getElementById('header').setAttribute('style', 'margin-bottom: 120px');
+    document.getElementById('footer').setAttribute('style', 'display: block;');
   }
 });
 
 document.querySelector('#id-subm').addEventListener('click', function(event) {
   event.preventDefault();
 }, false);
+
+const firstSector = document.getElementById('firstSector');
+const aboutUs = document.getElementById('about-us');
+
+function moveOn() {
+  setTimeout(function() {
+    aboutUs.scrollIntoView(
+      {
+        block: 'start', behavior: 'smooth',
+      });
+  },
+  10);
+}
+firstSector.addEventListener('click', moveOn);
+
+const secondSector = document.getElementById('secondSector');
+const compateBikes = document.getElementById('compate-bikes');
+
+function moveOn1() {
+  setTimeout(function() {
+    compateBikes.scrollIntoView(
+      {
+        block: 'start', behavior: 'smooth',
+      });
+  },
+  10);
+}
+secondSector.addEventListener('click', moveOn1);
+
+const thirdSector = document.getElementById('thirdSector');
+const details = document.getElementById('details');
+
+function moveOn2() {
+  setTimeout(function() {
+    details.scrollIntoView(
+      {
+        block: 'start', behavior: 'smooth',
+      });
+  },
+  10);
+}
+thirdSector.addEventListener('click', moveOn2);
+
+const fourthSector = document.getElementById('fourthSector');
+const contacts = document.getElementById('contacts');
+
+function moveOn3() {
+  setTimeout(function() {
+    contacts.scrollIntoView(
+      {
+        block: 'start', behavior: 'smooth',
+      });
+  },
+  10);
+}
+fourthSector.addEventListener('click', moveOn3);
