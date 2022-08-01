167188000b16754e1963e78fbedc61e4b5119312
Nikolay Li <koliahoy@gmail.com>
2022-08-01 12:33:43 +0300

    scrolling fix bag in js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.addEventListener('hashchanger', () => {
+window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
