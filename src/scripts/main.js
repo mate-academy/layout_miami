8fe4703835218805f4a3ff1431bc212ef4bf8cab
Cyril Prue <kir8mir>
2022-08-01 14:59:25 +0300

    All comments taken into account + checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.querySelectorAll('#form').forEach(form => {
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('scroll-hidden');
+  } else {
+    document.body.classList.remove('scroll-hidden');
+  }
+});
