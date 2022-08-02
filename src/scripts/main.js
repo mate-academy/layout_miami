0b78a86a9782e7128b9a3ee7619a83fcfdd898d0
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-02 06:35:57 +0300

    fixed errors, form cleaner, visual effects etc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', function handleClick(event) {
+  event.preventDefault();
+
+  const firstNameInput = document.getElementById('first_name');
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
+
+  firstNameInput.value = '';
+  email.value = '';
+  message.value = '';
+});
+
+document.getElementById('menu-btn').addEventListener('click', () => {
+  document.body.classList.toggle('fixed-position');
+});
+
+document.getElementById('cross').addEventListener('click', () => {
+  document.body.classList.toggle('fixed-position');
+});
