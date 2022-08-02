8895dd2017e46ad66bfbc68dd70b1ba7e26e7e93
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-08-02 11:03:50 +0300

    Fix menu issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,11 @@ document.getElementById('form').addEventListener('submit',
     event.preventDefault();
   }
 );
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
