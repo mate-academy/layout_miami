a171ffba4ad27379e31d50768dcbf9f3a590d60a
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-08-03 13:40:00 +0300

    auto submit was disabled

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('form_button').addEventListener(
+  'click', stopDefAction, false
+);
