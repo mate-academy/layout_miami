9a6ca159a5891d6ed5dfbe8e6283c0e635fc0dc2
Andrii Dunets <davbiznesman@gmail.com>
2022-08-04 16:47:20 +0300

    miami was fixed again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
-window.addEventListener ('hashchange', () => {
+window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  e.target.reset();
 });
