e1b1f86ac6516910402a3ffa83a8bcd3a5e19a66
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-08-04 21:02:03 +0300

    v13

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   form.reset();
 });
+
+const check = document.getElementById('menu');
+
+menu.addEventListener('change', e => {
+
+    if(e.target.checked){
+      document.body.classList.add('page__body--with-menu');
+    } else {
+      document.body.classList.remove('page__body--with-menu');
+    }
+
+});
+
+window.addEventListener('scroll',(event) => {
+  document.getElementById("menu").checked = false;
+  document.body.classList.remove('page__body--with-menu');
+});
