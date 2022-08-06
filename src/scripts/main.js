1b6a90c525eef03b3ed9febd60a24ba570ed75b8
Kamil Witek <kamilprogramuje@gmail.com>
2022-08-06 11:44:41 +0200

    inter font added, page not allowed scroll for small dimensions, font weight, spaces changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,11 +12,18 @@ barX.addEventListener('click', () => {
   menu.classList.toggle('display-flex');
 });
 
-// disable page scrolling under the menu
+const form = document.getElementById('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  form.reset();
+  window.scrollTo(0, 0);
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('body--with-menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('body--with-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
