ff95fae818c2bc371c27cfaa8f02682bdb007dbb
Ziatiuk Serhii <sergey.zyat.tvoy@gmail.com>
2022-08-08 00:49:01 +0300

    fixed x3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
-
 const checker = document.querySelector('.form');
 
 checker.addEventListener('submit', function(event) {
   event.preventDefault();
 
   checker.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
