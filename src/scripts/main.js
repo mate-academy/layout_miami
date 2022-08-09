8840391160794cb341bc3481c21a662387b1c7e6
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-08-09 15:42:46 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const button = document.querySelector('#submit');
+
+button.onclick = function(evt) {
+  evt.preventDefault();
+};
