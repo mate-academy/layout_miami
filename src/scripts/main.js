b67c0400525e7b888d738667eb865a3fe909351b
Kamil Witek <kamilprogramuje@gmail.com>
2022-08-09 10:37:22 +0200

    added home id, added animation after show menu, removed menu after click any menu elment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,14 @@ const bar = document.querySelector('.nav__icon--bar');
 const barX = document.querySelector('.menu__x-wrapper');
 const menu = document.querySelector('.menu');
 
+const links = document.querySelectorAll('a');
+
 bar.addEventListener('click', () => {
-  menu.classList.toggle('display-flex');
+  menu.classList.toggle('show-menu');
 });
 
 barX.addEventListener('click', () => {
-  menu.classList.toggle('display-flex');
+  menu.classList.toggle('show-menu');
 });
 
 const form = document.getElementById('form');
@@ -26,4 +28,10 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('show-menu');
+  });
 });
