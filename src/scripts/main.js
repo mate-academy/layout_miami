225eee860b707cd3f639d2b15810726842b68a9d
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-08-18 15:26:24 +0300

    Testimonial and contct-us blocks were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,15 @@ function handleForm(event) {
   event.preventDefault();
 }
 form.addEventListener('submit', handleForm);
+
+
+const forma = document.querySelector('.contact-us__form');
+ const inputs = document.querySelectorAll('.contact-us__field');
+
+ forma.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   inputs.forEach(elem => {
+     elem.value = '';
+   });
+ });
