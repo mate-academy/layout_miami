ed78e02888433f3bd812bfa48b3e68bcfee99cd4
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-08-18 14:09:21 +0300

    Favicon, pictures increasing and logo fix were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,7 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+var form = document.getElementById("form");
+function handleForm(event) {event.preventDefault();}
+form.addEventListener('submit', handleForm);
