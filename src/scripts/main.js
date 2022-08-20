30d17d1b33d972fe60eed12b16d96a28716bfc08
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-08-20 20:59:59 +0300

    Add menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('my-bike__body--with-menu');
+  } else {
+    document.body.classList.remove('my-bike__body--with-menu');
+  }
+});
