8d418f732db4f8a9187549aa23ecc94822e8923d
Yuliia Nudyk <yulyayu833@gmail.com>
2022-08-23 13:41:04 +0300

    yuliia-nudyk_miami-edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,15 +8,9 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-document.querySelector('#button_form-submit')
-  .addEventListener('click', (event) => {
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
+const form = document.getElementById('form');
 
-    if (name !== 0 && email !== 0 && message !== 0) {
-      name = '';
-      email = '';
-      message = '';
-    }
-  }, false);
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
