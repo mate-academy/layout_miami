f2d22909a499d1665b2d124b2e88eefd90f3f531
Max Bovtun <maxbovtun@gmail.com>
2022-08-24 13:12:12 +0300

    js-script implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 /* eslint-disable */
 /* eslint-enable */
+
+const checkbox = document.querySelector('input[name=checkbox]');
+
+checkbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash !== '') {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
