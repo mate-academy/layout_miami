7bcb080bd7d6527fff3ada3273f45b81c2639ff3
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-08-25 19:39:43 +0300

    code fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const form = document.getElementById('contacts-form');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +9,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
