2e5aa321093b1562c310c1b46d6614d8bf3fb01c
Max Bovtun <maxbovtun@gmail.com>
2022-08-27 22:13:53 +0300

    scripts fixed after st-review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,23 @@
 /* eslint-disable */
 /* eslint-enable */
 
+const scroll = window.innerWidth - document.body.clientWidth;
 const checkbox = document.querySelector('input[name=checkbox]');
 
 checkbox.addEventListener('change', (e) => {
   if (e.target.checked) {
     document.body.classList.add('page__body--with-menu');
+    document.body.style.paddingRight = `${scroll}px`;
   } else {
     document.body.classList.remove('page__body--with-menu');
+    document.body.style.paddingRight = null;
   }
 });
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash !== '') {
     document.body.classList.remove('page__body--with-menu');
+    document.body.style.paddingRight = null;
+    checkbox.checked = false;
   }
 });
