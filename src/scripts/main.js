030c20d1dece1f5108a52f0500b1266d711cd680
Ihor Andrianov <andrianoia@gmail.com>
2022-08-28 20:31:26 +0300

    added info tiles footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const checkbox = document.querySelector('.nav__checkbox');
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    document.body.classList.add('page__body--with-menu');
+    // eslint-disable-next-line no-console
+    console.log('checked');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+}, false);
+
+const button = document.querySelector('.contact__button');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+}, false);
