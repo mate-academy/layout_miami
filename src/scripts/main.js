5dfad3578900d021f941b735bdef172b207f7c9f
Vlad Malinin <vladmalinin1999@gmail.com>
2022-08-29 11:21:15 +0300

    Add variables and mixins

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const button = document.querySelector('.form');
+
+button.submit(function(e) {
+  e.preventDefault();
+});
