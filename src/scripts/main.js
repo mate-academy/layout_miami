98adf4b1b44e5087cbd39165b1a7fe050e5e6b19
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-08-29 14:25:29 +0300

    Bose audio project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+}, false);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
