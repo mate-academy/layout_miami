e3eafbc306393cc8aa852e97d1be3d721c97bb65
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-08-29 12:18:25 +0300

    page not reload on button press anymore

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,7 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.querySelector('.button').addEventListener('click', (event) => {
+  event.preventDefault();
+}, false);
