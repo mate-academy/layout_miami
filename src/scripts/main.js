eb15d62df27d4bd9b7bcd1146aca7eaff5e3516d
Yelisieiev Kim <rbv271@outlook.com>
2022-08-30 14:39:49 +0300

    prevent default fo form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__content--with-menu');
   }
 });
+
+const refresh = document.getElementById('contacts__form');
+
+refresh.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
