14517df1459b3b63bc20847ee18f28efc04201d8
Artem Musii <musiy.artem@gmail.com>
2022-08-30 23:56:21 +0300

    js added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('#consultationButton').addEventListener('click',
+  (event) => {
+    event.preventDefault();
+  });
