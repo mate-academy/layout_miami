01833df31480f19f59acd459b95021b602982cbf
Oleksii Pishchuhin <aliliolek@gmail.com>
2022-08-31 22:46:00 +0300

    1.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const callIcon = document.querySelector('.top-actions__call-icon');
-const phoneNumber = document.querySelector('.top-actions__phone-number');
-
-callIcon.addEventListener('click', () => {
-  phoneNumber.classList.toggle('top-actions__phone-number--hidden');
-});
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
 });
