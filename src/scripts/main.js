fbca112caf818f8464222261bdd8b406c347f323
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-09-01 19:54:15 +0300

    Page not reload on form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('#form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const inputs = document.querySelectorAll('.form-field');
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
