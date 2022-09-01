64b7f37a444fe7c46767183b599cd87463fc9a1a
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-01 11:22:35 +0300

    Fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,16 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const btn = document.querySelector('.contact-us__button');
-
-btn.addEventListener('click', function handleClick(event) {
-  event.preventDefault();
-
-  const inputs = document.querySelectorAll('.contact-us__field');
-
-  inputs.forEach(input => {
-    input.value = '';
-  });
-});
-// onsubmit="return false"
