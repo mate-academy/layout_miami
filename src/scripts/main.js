2415213d1b91e360cbb5c8a720254e206363ca54
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-02 00:06:40 +0300

    header, menu, soundWave styles and btn clikc and hover functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const phone = document.querySelector('.header__phone');
+const tooltip = document.querySelector('.header__tooltip');
+const menuLink = document.querySelector('.header__menu');
+const menu = document.querySelector('.menu');
+
+menuLink.addEventListener('click', () => {
+  menuLink.classList.toggle('header__menu--close');
+  menuLink.classList.toggle('header__menu');
+  menu.classList.toggle('menu--open');
+});
+
+phone.addEventListener('mouseenter', () => {
+  tooltip.classList.add('header__tooltip--isVisiable');
+});
+
+phone.addEventListener('mouseleave', () => {
+  tooltip.classList.remove('header__tooltip--isVisiable');
+});
