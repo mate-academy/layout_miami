f3877a88b5eae18f5adaa313770c4f045390d13f
Volodymyr Klymkiv <volodymyr.klymkiv@gmail.com>
2022-09-01 19:26:17 +0300

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--scroll');
+  } else {
+    document.body.classList.remove('page__body--scroll');
+  }
+});
