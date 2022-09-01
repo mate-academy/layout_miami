6b0a53a5dd012eac4126ee9b46e5b69985b2964c
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-09-01 15:31:13 +0300

    Created: contact-us section. Fixed: menu scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('.contact-us__form').addEventListener('submit', (e) => {
+  e.preventDefault();
+});
