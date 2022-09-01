6eb0c4931c708b31cbb2bf4b44b83efe90c73f84
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-09-01 19:59:17 +0300

    Disable page scrolling under the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
 document.querySelector('#form').addEventListener('submit', e => {
   e.preventDefault();
 
