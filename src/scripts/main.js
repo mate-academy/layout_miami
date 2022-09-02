fae6130b6ab231e76672aef96e554e1457ec6ce1
Dmytri Stepanov <wootjkeee@gmail.com>
2022-09-02 11:19:32 +0300

    added script to prevent pager reload on submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// OFF VERTICAL SCROLL WHEN OPEN MENU //
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +9,18 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// OFF PAGE RELOAD WHEN SUBMIT //
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
