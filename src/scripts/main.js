c0d8de03b3a63398525b99020f3d5c8d6f371fd2
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-09-02 21:06:14 +0300

    clear input fields in form when butoon was pressed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// disable scroll when menu is open
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +8,18 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// clear input fields in form when butoon was pressed,
+// in this case, page is not updating
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.contacts__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
