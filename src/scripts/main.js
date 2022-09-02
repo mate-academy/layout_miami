55a4718d5b5ed226165ec72a77651eef1227c76a
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-02 11:43:38 +0300

    Canceled page reloading on form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
