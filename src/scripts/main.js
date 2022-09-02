cf3d654f3fb80140445dde08213502be062754a6
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-02 08:41:21 +0300

    form field clear on using send button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const formInputs = document.querySelectorAll('.form__input');
+const textArea = document.querySelector('.form__textarea');
+
 document.querySelector('.form').addEventListener('submit', (event) => {
   event.preventDefault();
+
+  for (const input of formInputs) {
+    input.value = '';
+  }
+
+  textArea.value = '';
 }, false);
 
 window.addEventListener('hashchange', () => {
