c958aee8ed81dc87aa1e7e6dab7d7a11f4e03afc
Roma Tsvindin <tsvindin1995@gmail.com>
2022-09-04 01:30:28 +0300

    Bose was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+
+  form.reset();
+});
