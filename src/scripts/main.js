f635924d3ce42b16bb30f08d30db4b6e6025a146
Ihor Andrianov <andrianoia@gmail.com>
2022-09-03 18:03:55 +0300

    fix: add hero margin when menu open and add variables fix markup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,34 @@
 'use strict';
 
 const checkbox = document.querySelector('.nav__checkbox');
+const nav = document.querySelector('.nav');
+const form = document.querySelector('.contact__form');
+const hero = document.querySelector('.hero');
 
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    document.body.classList.add('page__body--with-menu');
-    // eslint-disable-next-line no-console
-    console.log('checked');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-}, false);
+checkbox.addEventListener(
+  'change',
+  function () {
+    if (this.checked) {
+      document.body.classList.add('page__body--with-menu');
+    } else {
+      document.body.classList.remove('page__body--with-menu');
+    }
+  },
+  false
+);
 
-const button = document.querySelector('.contact__button');
-
-button.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-}, false);
+  form.reset();
+  alert('You are beautiful');
+});
+
+hero.style.setProperty('width', '100%');
+
+checkbox.addEventListener('click', () => {
+  if (hero.style.width === '100%') {
+    hero.style.setProperty('width', 'calc(100% - 15px)');
+  } else {
+    hero.style.setProperty('width', '100%');
+  }
+});
