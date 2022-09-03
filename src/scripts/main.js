96d0a41be59f546393a17e958e894c323b21938b
stukankarina <stukankarina@gmail.com>
2022-09-03 18:09:13 +0300

    changed approach to mobile first, removed all grids, added js for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.js-input');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
