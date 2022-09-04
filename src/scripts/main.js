8a260a59b331b024ef3abc30b7545d57ec16c50e
fatihsamur <a.fatihsamur@gmail.com>
2022-09-04 03:06:07 +0300

    version 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const openMenuBtn = document.querySelector('.nav__burger-menu');
+const closeMenuBtn = document.querySelector('.menu__close-btn');
+const navMenu = document.querySelector('.nav__menu');
+
+const openMenu = () => {
+  navMenu.classList.add('nav__menu--open');
+};
+
+const closeMenu = () => {
+  navMenu.classList.remove('nav__menu--open');
+};
+
+openMenuBtn.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
