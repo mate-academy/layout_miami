fbe8b00d4eee3937b09e705fc2ab0ef8f3ca221b
Andriy Andrieiev <proph7000@gmail.com>
2022-09-04 13:39:20 +0300

    implemented clearing form fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('contacts__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
