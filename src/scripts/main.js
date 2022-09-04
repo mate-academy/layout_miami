60ebc3d3a88962170235489a9287c58e8f38993b
Sasha Kovalov <av.kov14@gmail.com>
2022-09-04 16:24:55 +0300

    Finalize with main.js reset on contact-us form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.js-form');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
