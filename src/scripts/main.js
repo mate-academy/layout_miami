924e833c444ee06b07b8e822e7b65a53a76f0792
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2022-09-04 12:02:52 +0300

    fix form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
