6eb5dd43fef7016b45d80b577484f7cf3381b5d9
Volodymyr Klymkiv <volodymyr.klymkiv@gmail.com>
2022-09-05 19:44:02 +0300

    added hovers and js function for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,16 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--scroll');
   }
 });
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
