dc2f855c99f3c5c7095243180554d75dfd92803a
Artem Hulko <agulko96@gmail.com>
2022-09-05 23:05:59 +0300

    made some changes according to CHECKLIST

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
