abbcf8adedbe3b0c31b4ce713402b1faa8ab9162
Serhii Romaniuk <deepspark@gmail.com>
2022-09-05 13:33:13 +0300

    fix menu scroll, set up form block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 'use strict';
 
+// remove scroll in menu
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+// disable page reload when submit active and clean up form places
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
 });
