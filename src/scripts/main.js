5a6b8372427ca6e4e9a89a6d82c58178682cdd6d
Vova Dzhuryk <dzhurykvovik@gmail.com>
2022-09-06 10:50:27 +0300

    Miami-fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,14 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--menu-open');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--menu-open');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
 const form = document.querySelector('.form');
-const inputs = document.querySelectorAll('.js-test');
+const inputs = document.querySelectorAll('.form-field');
 
 function handler(event) {
   event.preventDefault();
