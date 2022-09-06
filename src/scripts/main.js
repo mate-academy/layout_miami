76d5f704b54b8dbba48ea11bccad7774fc299cbe
igor-1987 <igor.gayvoronsky@gmail.com>
2022-09-07 00:01:11 +0300

    layout has been complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.js-input');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
