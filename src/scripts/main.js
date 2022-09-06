f466ad8de99e5333c8bc5719270bfbd98fc04c17
George.kom <komolovua@gmail.com>
2022-09-06 06:33:11 +0300

    Added other sections, made refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
