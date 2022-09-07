237516f322d3da7ed4847c1b98dfec6cbe6530b1
igor-1987 <igor.gayvoronsky@gmail.com>
2022-09-07 21:41:22 +0300

    fixed 404 error, reload on submit, hover on links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.form-field');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,9 +10,6 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const form = document.querySelector('.contacts__form');
-const inputs = document.querySelectorAll('.js-input');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
