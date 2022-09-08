7c2721afbb528cbba5d8bf318ca189c2fcb4663f
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-09-08 23:35:29 +0300

    newUpdate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page_body-with-menu');
   }
 });
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  event.target.reset();
+});
