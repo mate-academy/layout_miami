4ea5679531dcf7fda1a6d24944d27e321f7dec7a
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-09-09 13:42:28 +0300

    added blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.form-field');
+const inputs = document.querySelectorAll('.button--input');
+const textarea = document.querySelector('.form-field__textarea');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  textarea.value = '';
+});
