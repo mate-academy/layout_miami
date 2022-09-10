87ffe89bd6c42211cf0fc9e3b64d0e1266498d08
Bradley Matjie <bradleysaint45@gmail.com>
2022-09-10 16:18:24 +0200

    MyBiKE

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const menubtn = document.querySelector('.nav__menu');
+const menuList = document.querySelector('.nav__list');
+let menuOpen = false;
+
+menubtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menubtn.classList.add('open');
+    menuOpen = true;
+    menuList.style.left = '0';
+    menuList.style.transition = '2s all';
+  } else {
+    menubtn.classList.remove('open');
+    menuOpen = false;
+    menuList.style.left = '-100%';
+    menuList.style.transition = '2s all';
+  }
+});
+
+const formSubmit = document.querySelector('.contact__submit');
+
+formSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+});
