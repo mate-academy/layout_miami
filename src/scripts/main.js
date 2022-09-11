af168286039ba8942a1af0a7c47d5f9efdc9a633
Anton <shulezhko.antony@gmail.com>
2022-09-11 12:41:06 +0300

    form edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const form = document.querySelector('.myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
