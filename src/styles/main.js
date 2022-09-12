754193e424085eb6bdbf549765082dadb67074cb
omerfarukbalik <merbalk2147@gmail.com>
2022-09-12 19:41:07 +0300

    versions added

---
+++ src/styles/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+if (window.location.hash === '#menu') {
+  document.body.classList.add('page__body--with-menu');
+}
