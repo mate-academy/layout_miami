fd9c8a14f4a6387cd401c495e6fceae2d064e58c
BZOzyurt <zeynep.ozyurt5@gmail.com>
2022-09-12 16:36:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,8 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
