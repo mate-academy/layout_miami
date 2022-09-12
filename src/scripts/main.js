1117151e348786632c03640fa4cb3d2f5f178743
George.kom <komolovua@gmail.com>
2022-09-12 13:13:32 +0300

    Fixed scroll issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,14 @@
 const form = document.querySelector('.form');
 const inputs = document.querySelectorAll('.form__field');
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--scroll');
+  } else {
+    document.body.classList.remove('page__body--scroll');
+  }
+});
+
 function handler(event) {
   event.preventDefault();
 
