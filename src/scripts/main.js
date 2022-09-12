5f7827b2228f394de65aba989b5b37fb556b6dd7
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-09-12 03:09:18 +0200

    created a Bose landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuOpen = document.querySelector('.header__menu-opener');
+const menuClose = document.querySelector('.header__menu-closer');
+const nav = document.querySelector('.header__nav');
+const navLogo = document.querySelector('.header__logo');
+const navList = document.querySelector('.header__nav-list');
+
+menuOpen.addEventListener('click', () => {
+  nav.style.left = '0';
+  body.style.overflow = 'hidden';
+});
+
+menuClose.addEventListener('click', () => {
+  nav.style.left = '';
+  body.style.overflow = '';
+});
+
+navLogo.addEventListener('click', () => {
+  nav.style.left = '';
+  body.style.overflow = '';
+});
+
+navList.addEventListener('click', e => {
+  if (e.target.tagName === 'A') {
+    nav.style.left = '';
+    body.style.overflow = '';
+  }
+});
