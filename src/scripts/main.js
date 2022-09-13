feaa5ebd827ac7f74bf6bc1f4863704505e9fee0
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-09-13 15:42:37 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const menuOpen = document.querySelector('.burger__wrapper');
+const menuClose = document.querySelector('.nav__close-icon');
+const body = document.querySelector('body');
+const menu = document.querySelector('.nav');
+const links = document.querySelectorAll('.nav__list .nav__link');
+
+menuOpen.addEventListener('click', (e) => {
+  menu.classList.add('active');
+  body.style.overflowY = 'hidden';
+});
+
+menuClose.addEventListener('click', (e) => {
+  menu.classList.remove('active');
+  body.style.overflowY = 'visible';
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    menu.classList.remove('active');
+    body.style.overflowY = 'visible';
+  });
+});
