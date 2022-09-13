7352f361a8519cc6780da3dab670b3b8a621b205
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-13 22:53:32 +0300

    font-family set, header img corrected, layout and responsiveness corrected for tab and mobile, page scrool disabled on menu open, unexpected page jamp due to fade-out-bottom on memnu was corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ menuLink.addEventListener('click', () => {
   menuLink.classList.toggle('header__menu--close');
   menuLink.classList.toggle('header__menu');
   menu.classList.toggle('menu--open');
+  document.body.classList.toggle('page--with-menu');
 });
 
 phone.addEventListener('mouseenter', () => {
@@ -23,4 +24,5 @@ menu.addEventListener('focusout', () => {
   menuLink.classList.add('header__menu');
   menuLink.classList.remove('header__menu--close');
   menu.classList.remove('menu--open');
+  document.body.classList.toggle('page--with-menu');
 });
