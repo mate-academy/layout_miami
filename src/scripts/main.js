91655374c1561a9f83982d2e9f637318bb98bc3f
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-09-13 23:45:51 +0300

    MyBike landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const formBtn = document.querySelector('.button--primary');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+formBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+});
