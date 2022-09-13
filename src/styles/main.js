466dfb1ba1b912db4d0defd1b109f07e892b0c44
omerfarukbalik <merbalk2147@gmail.com>
2022-09-13 14:16:52 +0300

    page centered for bigger screens, submit clear inputs and scroll top of the page, mistakes fixed

--- src/styles/main.js
+++ src/styles/main.js
@@ -11,3 +11,9 @@ window.addEventListener('hashchange', () => {
 if (window.location.hash === '#menu') {
   document.body.classList.add('page__body--with-menu');
 }
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+  window.scrollTo(0, 0);
+});
