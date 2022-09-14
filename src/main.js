ea07f479902806a8c8ab39f72059de5b20671d99
Liudmyla Stetsenko <ls8998281@gmail.com>
2022-09-14 22:30:19 +0300

    form was fixed

--- src/main.js
+++ src/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
