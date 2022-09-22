fbdc72efe16723f2fe6ecadfe4891cf6e4fab567
Iryna Konovalova <iruna191020@gmail.com>
2022-09-22 12:58:31 +0200

    The Met lending was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const forButton = document.getElementById('form-button');
+
+forButton.addEventListener('click', function(e) {
+  e.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
