841c8196707d3faacf9bb09142a211ed8e030c06
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-09-26 15:18:35 +0300

    my bike mir

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const opener = document.querySelector('#opener');
+const cross = document.querySelector('#menu');
+
+opener.addEventListener('click', () => {
+  document.body.classList.add('page__body--with-menu');
+});
+
+cross.addEventListener('click', () => {
+  document.body.classList.remove('page__body--with-menu');
+});
