ea6dea9eeaa181c90101d0d3e6d5fef9abfaeb86
Gabriella135 <gabriella.ivanjuk@gmail.com>
2022-10-02 20:26:42 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const nameTextbox = document.getElementById('name__text-box');
+
+nameTextbox.addEventListener('keypress', checkName, false);
+
+const emailTextbox = document.getElementById('email__text-box');
+
+emailTextbox.addEventListener('keypress', checkName, false);
+
+const textareaTextbox = document.getElementById('textarea__text-box');
+
+textareaTextbox.addEventListener('keypress', checkName, false);
+
+function checkName(evt) {
+  const charCode = evt.charCode;
+
+  if (charCode !== 0) {
+    evt.preventDefault();
+  }
+}
