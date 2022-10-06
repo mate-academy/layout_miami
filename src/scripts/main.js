e2c1136cd35d484d0fbcfd059d2a9fadac739bf9
Diana <dianaovb444@gmail.com>
2022-10-06 12:25:00 +0300

    scroll is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,11 @@ form.addEventListener('submit', event => {
     input.value = '';
   });
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
