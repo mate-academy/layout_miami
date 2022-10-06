08afe83334c37093af87b5f7326e8de2e9c90590
Vasyl Pauk <owliom@gmail.com>
2022-10-06 12:53:34 +0300

    miami_landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+})
+
+button.onclick = function(event) {
+  event.preventDefault();
+}
