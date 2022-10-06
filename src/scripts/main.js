0fecc4395fc5b7db8cba35ded77c406d89f85dcb
Alona Kasap <alona_kasap@ukr.net>
2022-10-06 11:42:15 +0300

    contacts fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => {
+    elem.value = '';
+  });
+});
