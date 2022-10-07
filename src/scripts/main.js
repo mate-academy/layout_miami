ed0165ebda948405abf6c400a3237277675d2da6
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-10-07 08:41:02 +0200

    form submission not reload the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,8 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('form').addEventListener("submit", (event) => {
+  form.reset();
+  event.preventDefault();
+});
