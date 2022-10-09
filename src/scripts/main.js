6c5e2065aafbb59a20cf437d07aa8a7f92230471
home2ego <amorati911@gmail.com>
2022-10-09 21:42:51 +0200

    v01 Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
