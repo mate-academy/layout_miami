3d1a760e8c59686f41681df5786966c7a28b7565
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2022-10-12 18:32:47 +0200

    mistakes corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,16 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+const textarea = document.querySelector('.form-field--textarea');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+  textarea.value = '';
+});
