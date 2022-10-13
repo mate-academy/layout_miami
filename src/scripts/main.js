a8e9ec8cc99d758c83b74c16bf077200c8646326
Vasyl Pauk <owliom@gmail.com>
2022-10-13 10:29:36 +0300

    some links were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ window.addEventListener('hashchange', () => {
 });
 
 const form = document.getElementById('form');
+
 form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
   form.reset();
