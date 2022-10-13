49a286aee475ec5b29c2eae8da131dc9dc202637
Andrii Bassarab <andrijbassarab@gmail.com>
2022-10-13 22:55:50 +0300

    header was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const iconMenu = document.querySelector('.icon--menu');
+const iconClose = document.querySelector('.icon--close');
+
+iconMenu.addEventListener('click', function() {
+  menu.classList.add('menu--open');
+});
+
+iconClose.addEventListener('click', function() {
+  menu.classList.remove('menu--open');
+});
