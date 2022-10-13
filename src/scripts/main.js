28121b886772afb43a9f6025bba4d873d3a104c6
Svitlana Yudina <osbka31@gmail.com>
2022-10-13 20:27:59 +0300

    menu almost done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+// Ищем нужные элементы во всём документе
+const toggler = document.querySelector('#toggler');
+const menu = document.querySelector('.menu');
+
+// ждём клики на toggler
+toggler.addEventListener('click', function() {
+  // переключаем класс menu--hidden (если есть - убираем, если нет - добавляем)
+  menu.classList.toggle('menu--hidden');
+});
