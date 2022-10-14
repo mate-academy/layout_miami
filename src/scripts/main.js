21f7184a500872d6994dda4427ce07abef628d59
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-10-14 13:51:27 +0300

    header v-01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const toggler = document.querySelector('.header__menu-openner');
+const menu = document.querySelector('.nav');
+
+toggler.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  toggler.classList.toggle('header__menu-close');
+});
+
+// toggler.addEventListener('click', function() {
+//   menu.classList.toggle('nav-hidden');
+// });
