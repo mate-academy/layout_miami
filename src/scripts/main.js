8efb5d88b04555c429d3480348c6fd6b66f7d94d
Max Skorokhod <mrzandras@gmail.com>
2022-10-14 17:05:25 +0300

    header 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const toggler = document.querySelector('.icon--menu');
+const togglerCross = document.querySelector('.icon--cross');
+const menu = document.querySelector('.page__menu');
+
+toggler.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerCross.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
