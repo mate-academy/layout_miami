09261673f19e9997d89aee1cef2625ccd3105969
Max Skorokhod <mrzandras@gmail.com>
2022-10-15 16:39:02 +0300

    About us initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,11 @@
 
 const toggler = document.querySelector('.icon--menu');
 const togglerCross = document.querySelector('.icon--cross');
+const togglerHome = document.querySelector('.menu__item--home');
+const togglerAboutUs = document.querySelector('.menu__item--about-us');
+const togglerCompare = document.querySelector('.menu__item--compare');
+const togglerDetails = document.querySelector('.menu__item--details');
+const togglerContacts = document.querySelector('.menu__item--contacts');
 const menu = document.querySelector('.page__menu');
 
 toggler.addEventListener('click', function() {
@@ -9,5 +14,25 @@ toggler.addEventListener('click', function() {
 });
 
 togglerCross.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerHome.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerAboutUs.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerCompare.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerDetails.addEventListener('click', function() {
+  menu.classList.toggle('page__menu--open');
+});
+
+togglerContacts.addEventListener('click', function() {
   menu.classList.toggle('page__menu--open');
 });
