1e60a6011f397732c422bb6ecf9a651ff68115a5
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-10-16 01:25:03 +0300

    all sections done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+// 'use strict';
+
+// const iconMenu = document.querySelector('#toggler');
+// // const iconCross = document.querySelector('.icon--cross');
+// const menu = document.querySelector('.menu');
+
+// iconMenu.addEventListener('click', function() {
+//   menu.classList.toggle('.menu--hidden');
+// });
+
+// // iconCross.addEventListener('click', function() {
+// //   menu.classList.remove('.menu--hidden');
+// // });
