be5db1a470976f57cece8940f875a4fa2bd860e2
Spirit <baolt.king@gmail.com>
2022-10-16 22:57:14 +0300

    in procces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// const outputElement = document.getElementById("info");
+
+// const smallDevice = window.matchMedia('(min-width: 576px)');
+
+// smallDevice.addListener(handleDeviceChange);
+
+// function handleDeviceChange(e) {
+//   console.log(e);
+// }
