83600abd2c441ca6da9df14bc3147e58ccf766b2
Viktor Kirichok <viktor.kirichok@gmail.com>
2022-10-17 19:34:09 +0300

    layout_miami MyBike v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const submitButton = document.getElementById('submit-button');
+
+function submitReloadPrevent(event) {
+  event.preventDefault();
+}
+
+submitButton.addEventListener('click', submitReloadPrevent);
