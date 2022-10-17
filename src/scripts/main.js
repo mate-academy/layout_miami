184ae581cb3c3fc71e0e50fb61881bb0e5b83f15
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-10-17 17:25:38 +0300

    landing done v02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,14 +8,14 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const iconMenu = document.querySelector('#toggler');
-const iconCross = document.querySelector('.icon--cross');
-const menu = document.querySelector('.menu__content');
+// const iconMenu = document.querySelector('#toggler');
+// const iconCross = document.querySelector('.icon--cross');
+// const menu = document.querySelector('.menu__content');
 
-iconMenu.addEventListener('click', function() {
-  menu.classList.toggle('menu--hidden');
-});
+// iconMenu.addEventListener('click', function() {
+//   menu.classList.toggle('menu--hidden');
+// });
 
-iconCross.addEventListener('click', function() {
-  menu.classList.remove('menu--hidden');
-});
+// iconCross.addEventListener('click', function() {
+//   menu.classList.remove('menu--hidden');
+// });
