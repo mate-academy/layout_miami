1e1386e9d5b3f4772cec07260ff898e80d6cbd26
Max Fedak <max.fedak@gmail.com>
2022-10-17 11:38:03 +0300

    preventing default button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,7 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.querySelector('#form-send').addEventListener('click', e => {
+  e.preventDefault();
+});
