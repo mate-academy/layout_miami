9f340b21d60f99e2a769aa1ca905192089a1e77b
Max Skorokhod <mrzandras@gmail.com>
2022-10-17 20:05:02 +0300

    added other blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,20 @@
 'use strict';
 
-const toggler = document.querySelector('.icon--menu');
-const togglerCross = document.querySelector('.icon--cross');
-const togglerHome = document.querySelector('.menu__item--home');
-const togglerAboutUs = document.querySelector('.menu__item--about-us');
-const togglerCompare = document.querySelector('.menu__item--compare');
-const togglerDetails = document.querySelector('.menu__item--details');
-const togglerContacts = document.querySelector('.menu__item--contacts');
-const menu = document.querySelector('.page__menu');
-
-toggler.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
 
-togglerCross.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
+const form = document.getElementById('form__submit');
 
-togglerHome.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
+function handleForm(event) {
+  event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
 
-togglerAboutUs.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
-
-togglerCompare.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
-
-togglerDetails.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
-
-togglerContacts.addEventListener('click', function() {
-  menu.classList.toggle('page__menu--open');
-});
+// found the answer here
+// https:
+// stackoverflow.com/questions/19454310/stop-form-refreshing-page-on-submit
