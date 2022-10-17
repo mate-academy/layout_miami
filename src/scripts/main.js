19d813306ab435b28fed8c8e0db2ba54f25b90c5
Yurii-Pozho <46326784+Yurii-Pozho@users.noreply.github.com>
2022-10-17 16:39:32 +0300

    all done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
