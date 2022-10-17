3daa5a7574477c5463cc92f67822b11ab6c38b3b
AndreyChernysh <achernysh744@gmail.com>
2022-10-17 12:32:49 +0300

    Done project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const toggler = document.querySelector('.toggler');
+const closer = document.querySelector('.closer');
+const menu = document.querySelector('.burger-menu');
+
+// ждём клики на toggler
+toggler.addEventListener('click', function() {
+  menu.classList.toggle('burger-menu--hidden');
+});
+
+closer.addEventListener('click', function() {
+  menu.classList.toggle('burger-menu--hidden');
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('wrapper--with-menu');
+  } else {
+    document.body.classList.remove('wrapper--with-menu');
+  }
+});
