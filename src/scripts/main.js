d4c522de22d90963b2e123737a9435aecac220a0
Andrii Bassarab <andrijbassarab@gmail.com>
2022-10-18 02:14:50 +0300

    content was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const menu = document.querySelector('.menu');
+const body = document.querySelector('.page__body');
 const iconMenu = document.querySelector('.icon--menu');
 const iconClose = document.querySelector('.icon--close');
 
 iconMenu.addEventListener('click', function() {
+  body.classList.add('page__body--hidden');
   menu.classList.add('menu--open');
 });
 
 iconClose.addEventListener('click', function() {
+  body.classList.remove('page__body--hidden');
   menu.classList.remove('menu--open');
 });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => event.preventDefault());
