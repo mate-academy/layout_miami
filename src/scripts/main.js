507a3d61aabc292259c029fe3a79eb4b9435ae55
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2022-10-17 16:59:49 +0300

    Fourth commit. All done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('#send');
+
+form.addEventListener('submit', function sendSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
