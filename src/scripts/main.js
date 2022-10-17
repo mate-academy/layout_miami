1a82f2ca612c33ca0edf7dfaf31e7e81348abef6
Vadym Prydatok <vadymprydatok@gmail.com>
2022-10-17 22:04:42 +0300

    hover add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+
+function handleForm(event) {
+  event.preventDefault();
+  form.reset();
+}
+form.addEventListener('submit', handleForm);
