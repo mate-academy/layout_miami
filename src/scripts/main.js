813ee80ad0635a256dac4879801c5f174669c202
taras <nazarko94@gmail.com>
2022-10-17 15:25:16 +0300

    fixing bugs after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function sendSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
