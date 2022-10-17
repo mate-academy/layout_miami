378fabf7bc65005c212fd38f674d8d2c76f440db
Bohdan Stepanets <number1xy@gmail.com>
2022-10-17 22:45:42 +0300

    Make Bose v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+// I've added the fuction below but it dont
+// send a form instead to prevent reload
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('form').addEventListener(
+  'submit', stopDefAction, false
+);
