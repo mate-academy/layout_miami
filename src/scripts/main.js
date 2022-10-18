e186d33815e9bff4c06dd416e8610b18b197ec65
Vadym Baranov <vadym.baranov@hotmail.com>
2022-10-18 18:15:31 +0300

    Added animations; Switched header to flex; Landing completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+  form.reset();
+};
