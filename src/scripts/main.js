496255e3acfc5b40f163c303e716e004c98a746f
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-10-21 14:09:45 +0300

    final - 02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('nav__with-menu');
+  } else {
+    document.body.classList.remove('nav__with-menu');
+  }
+});
+
 // menu options
 const toggler = document.querySelector('.header__menu-openner');
 const menu = document.querySelector('.nav');
@@ -13,14 +21,14 @@ toggler.addEventListener('click', function() {
 const navClose = document.querySelector('.nav__link-close');
 
 navClose.addEventListener('click', function() {
-  menu.classList.add('nav-hidden');
+  menu.classList.remove('nav-hidden');
 });
 
 // menu logo options
 const logo = document.querySelector('.nav__logo-close');
 
 logo.addEventListener('click', function() {
-  menu.classList.add('nav-hidden');
+  menu.classList.remove('nav-hidden');
 });
 
 // submit button
