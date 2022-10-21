4a98167eeab07c40839eda0d3947120c3b8ee86d
Kovalskyi Semen <esceyp249>
2022-10-21 19:13:12 +0300

    lending full version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+document.documentElement.style.setProperty('--scrollbar-width',
+  (window.innerWidth - document.documentElement.clientWidth) + 'px');
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
