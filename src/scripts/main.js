80f7a9fdf6d7f5000692b7971553c9bc38d8695b
Nikitin Evgeny <ngenya81@gmail.com>
2022-10-21 09:33:09 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  e.target.reset();
+});
