797174fa12d3d04cac2069805f17d736cfe57161
Zhenya Khodko <zhodko32@gmail.com>
2022-10-23 21:01:12 +0300

    Added user actions + applied requirements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--overflow-hidden');
+  } else {
+    document.body.classList.remove('page__body--overflow-hidden');
+  }
+});
+
+const formSubmitButton = document.querySelector('.contact-us__form-submit');
+
+formSubmitButton.addEventListener('click', prevDefFromButton, false);
+
+function prevDefFromButton(event) {
+  event.preventDefault();
+};
