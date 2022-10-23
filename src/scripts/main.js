d66c65f50a8308652b7520b82edadba0d57fd64a
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-10-23 14:41:01 +0300

    final - 03

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,14 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('nav__with-menu');
-  } else {
-    document.body.classList.remove('nav__with-menu');
-  }
-});
-
 // menu options
 const toggler = document.querySelector('.header__menu-openner');
 const menu = document.querySelector('.nav');
+const body = document.querySelector('.page__body');
 
 toggler.addEventListener('click', function() {
   menu.classList.toggle('nav-hidden');
   toggler.classList.toggle('header__menu-close');
+  body.classList.toggle('page__body--with-menu');
 });
 
 // menu 'home' button options
@@ -22,6 +16,7 @@ const navClose = document.querySelector('.nav__link-close');
 
 navClose.addEventListener('click', function() {
   menu.classList.remove('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
 });
 
 // menu logo options
@@ -29,6 +24,7 @@ const logo = document.querySelector('.nav__logo-close');
 
 logo.addEventListener('click', function() {
   menu.classList.remove('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
 });
 
 // submit button
@@ -37,4 +33,37 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
   form.reset();
+});
+
+const closemenu = document.querySelector('.nav__menu-openner');
+
+closemenu.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
+});
+
+// munu hiden options
+const navlinkOne = document.querySelector('.nav__link-one');
+const navlinkTwo = document.querySelector('.nav__link-two');
+const navlinkThree = document.querySelector('.nav__link-three');
+const navlinkFour = document.querySelector('.nav__link-four');
+
+navlinkOne.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
+});
+
+navlinkTwo.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
+});
+
+navlinkThree.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
+});
+
+navlinkFour.addEventListener('click', function() {
+  menu.classList.toggle('nav-hidden');
+  body.classList.remove('page__body--with-menu'); // вот это
 });
