e9fb2f91b1458ece50043a98992f5754ac479923
Ihor Kuzhel <kuzhelek@gmail.com>
2022-10-24 14:54:46 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#nav') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
