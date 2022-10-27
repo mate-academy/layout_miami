a6617f8efcdb45ce73e3ae5ce2572a8bfe7d3092
Halyna Rydasova <halyna.rydasova@gmail.com>
2022-10-27 13:24:01 +0300

    added effects for images and for link in contacts section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--no-scroll');
   }
 });
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
