9611822a53705578307901de6b3e682c31e4c7a0
Artem Hirzhev <a.hirzhev@gmail.com>
2022-10-28 21:09:31 +0300

    Hovers for contact links were added, reload on form submit was disabled

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+})
