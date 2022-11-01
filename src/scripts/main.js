adb1e99765496dfcae6d802bc4c9310393c5da91
Hovera-Liubomyr <ljusuk@gmail.com>
2022-11-01 10:21:01 +0200

    layout_met_2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,7 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+});
