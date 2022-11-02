ca6b1b1c2e8fa332ff67fd6fdaeb49e5b3d94fdd
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2022-11-02 20:53:05 +0200

    full version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('hashchange', function() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('.contacts__button')
+.addEventListener('click', function(e) {
+  e.preventDefault();
+});
