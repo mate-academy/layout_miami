fd8e47b9ae16d0f3ec3f313205b228ae6fbee6b4
haduigon <haduigon@gmail.com>
2022-11-03 18:46:15 +0200

    finally, it seems, all mistakes are fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+function clearInputs() {
+  const inputs = document.querySelectorAll('input');
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  const textarea = document.querySelector('textarea');
+
+  textarea.value = '';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  clearInputs();
+});
