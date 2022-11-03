7e7b728c21c9afd7024c33edc263c3f22e872a1a
zbyn3k <coding707@mailbox.org>
2022-11-03 14:49:22 +0100

    Added Event.preventDefault() to the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const formHomepage = document.getElementById('id-form-homepage');
+
+formHomepage.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formHomepage.reset();
+});
