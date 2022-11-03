ec4be78085613b305bfb156725e64ec5d399b893
zbyn3k <coding707@mailbox.org>
2022-11-03 15:05:15 +0100

    Added hashchange listener for the menu (disable ver. scroll in menu)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ formHomepage.addEventListener('submit', (event) => {
   event.preventDefault();
   formHomepage.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
