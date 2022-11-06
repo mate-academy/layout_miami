1ae12a7689304a751008a24e191b39a6fc803524
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2022-11-06 19:10:56 +0200

    fixed inputs with js and added hover effects to categories links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,14 @@ document.querySelector('.contacts__button')
   .addEventListener('click', function(e) {
     e.preventDefault();
 
+    const valueName = document.querySelector('.contacts__input--name').value;
+    const valueText = document.querySelector('.contacts__input--message').value;
+    const valueEmail = document.querySelector('.contacts__input--email').value;
+
+    if (!valueEmail || !valueName || !valueText) {
+      alert('All fields are required!!');
+    }
+
     const docs = document.querySelectorAll('.contacts__input');
 
     docs.forEach(doc => {
