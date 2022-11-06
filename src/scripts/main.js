0ca331df806b4732278ad33c53fcfb18671cb3b1
haduigon <haduigon@gmail.com>
2022-11-06 22:52:27 +0200

    fixed scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,8 +21,8 @@ form.addEventListener('submit', (e) => {
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add ('page__body--with-menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove ('page__body--with-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
