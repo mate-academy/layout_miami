c306911b5e3138d0a5e82ff6e3de853c546b7dc3
Vlad Boikov <vladboikov95@hotmail.com>
2022-11-07 14:38:25 +0200

    landig is finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body_with-menu');
+  } else {
+    document.body.classList.remove('page__body_with-menu');
+  }
+});
