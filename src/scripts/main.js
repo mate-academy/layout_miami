e88dab5be4fd67973a0069c10bf8af4d73eee549
ysrckr <dyt.yasarcakir@gmail.com>
2022-11-08 16:23:48 +0300

    nav slider func added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const openButton = document.querySelector('.header__toggler');
+const closeButton = document.querySelector('.nav__toggler');
+
+openButton.addEventListener('click', () => {
+  nav.classList.add('nav--active');
+});
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('nav--active');
+});
