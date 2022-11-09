5b58235406f7bc1c60fcfb27cf17829a36ff057c
Nazar Brunarskyi <3nnazar836@gmail.com>
2022-11-09 15:43:57 +0200

    98%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--scroll-off');
+  } else {
+    document.body.classList.remove('body--scroll-off');
+  }
+});
