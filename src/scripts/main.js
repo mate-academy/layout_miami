93c2070006396a75f366d9e377a1174f6b2121b0
Ninel Levchenko <blackmaplearts@gmail.com>
2022-11-09 16:52:58 +0200

    fixed header and started about section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// document.querySelector('#iconPhone').addEventListener('mouseup', function() {
+//   document.querySelector('#headerContact').classList.toggle(
+//     'header__contact--visible');
+// });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
