8c9843d6fa8bcf5a1cc2f85e16cecdc5135de12f
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-09 13:13:40 +0200

    navigation was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const navIconOpen = document.querySelector('.icon--nav-burger');
+const navIconClose = document.querySelector('.icon--nav-close');
+const pageNav = document.querySelector('.page__nav');
+
+[navIconOpen, navIconClose].forEach(element => {
+  element.addEventListener('click', () => {
+    pageNav.classList.toggle('page__nav--opened');
+  })
+});
