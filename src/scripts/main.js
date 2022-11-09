4f7c22f7d7b90e8a70c94ea9cfd2ca48e913c2b1
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-09 16:47:32 +0200

    section Move Free was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,5 +7,5 @@ const pageNav = document.querySelector('.page__nav');
 [navIconOpen, navIconClose].forEach(element => {
   element.addEventListener('click', () => {
     pageNav.classList.toggle('page__nav--opened');
-  })
+  });
 });
