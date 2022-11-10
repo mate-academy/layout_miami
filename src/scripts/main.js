f84d5bd50260cd7b67e443d44fee3fb70cdb0a56
Saleh BUD <saleh.7734@gmail.com>
2022-11-10 17:30:20 +0200

    Fix: Cleaned the code a little

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,31 @@
-"use strict";
+'use strict';
 
-const menu = document.querySelector(".menu");
-const menuOpen = document.querySelector("#menu");
-const menuClose = document.querySelector("#close");
+const menu = document.querySelector('.menu');
+const menuOpen = document.querySelector('#menu');
+const menuClose = document.querySelector('#close');
 
-const phone = document.querySelector(".icon--phone");
-const tooltip = document.querySelector("#tooltip");
+const phone = document.querySelector('.icon--phone');
+const tooltip = document.querySelector('#tooltip');
 
-phone.addEventListener("mouseenter", () => {
-  tooltip.classList.add("nav__info--tooltip-show");
+phone.addEventListener('mouseenter', () => {
+  tooltip.classList.remove('display-none');
+
+  setTimeout(() => {
+    tooltip.classList.add('nav__tooltip--show');
+  }, 100);
 });
 
-phone.addEventListener("mouseout", () => {
-  tooltip.classList.remove("nav__info--tooltip-show");
+phone.addEventListener('mouseout', () => {
+  setTimeout(() => {
+    tooltip.classList.remove('nav__tooltip--show');
+  }, 2000);
+  tooltip.classList.add('display-none');
 });
 
-menuOpen.addEventListener("click", () => {
-  menu.classList.add("toggle-menu");
+menuOpen.addEventListener('click', () => {
+  menu.classList.add('toggle-menu');
 });
 
-menuClose.addEventListener("click", () => {
-  menu.classList.remove("toggle-menu");
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('toggle-menu');
 });
