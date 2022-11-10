4ea5992cf414ed61cbfe8d06bca6511260751e65
Saleh BUD <saleh.7734@gmail.com>
2022-11-10 14:32:11 +0200

    Add: Menu with functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const menu = document.querySelector(".menu");
+const menuOpen = document.querySelector("#menu");
+const menuClose = document.querySelector("#close");
+
+menuOpen.addEventListener("click", () => {
+  menu.classList.add("toggle-menu");
+});
+
+menuClose.addEventListener("click", () => {
+  menu.classList.remove("toggle-menu");
+});
