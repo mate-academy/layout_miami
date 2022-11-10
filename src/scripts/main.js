ca228d3b9dfb29144c55203b21b17418d7350069
Oleksandr <sanino343434@gmail.com>
2022-11-10 14:54:17 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.js-input');
+
+function hundler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', hundler);
