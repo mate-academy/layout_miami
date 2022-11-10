1217b170b54116b8bdb164df361816dbcfda8c03
Saleh BUD <saleh.7734@gmail.com>
2022-11-10 15:13:23 +0200

    Add: Tooltip on mouse hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,17 @@ const menu = document.querySelector(".menu");
 const menuOpen = document.querySelector("#menu");
 const menuClose = document.querySelector("#close");
 
+const phone = document.querySelector(".icon--phone");
+const tooltip = document.querySelector("#tooltip");
+
+phone.addEventListener("mouseenter", () => {
+  tooltip.classList.add("nav__info--tooltip-show");
+});
+
+phone.addEventListener("mouseout", () => {
+  tooltip.classList.remove("nav__info--tooltip-show");
+});
+
 menuOpen.addEventListener("click", () => {
   menu.classList.add("toggle-menu");
 });
