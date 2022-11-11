5aa34bc648579c65ee4eb4dc5f621c0651104709
ysrckr <dyt.yasarcakir@gmail.com>
2022-11-11 14:37:14 +0300

    form submit reloading overrided

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const nav = document.querySelector('.nav');
 const openButton = document.querySelector('.header__toggler');
 const closeButton = document.querySelector('.nav__toggler');
+const form = document.querySelector('.contact__form');
 
 openButton.addEventListener('click', () => {
   nav.classList.add('nav--active');
@@ -10,4 +11,8 @@ openButton.addEventListener('click', () => {
 
 closeButton.addEventListener('click', () => {
   nav.classList.remove('nav--active');
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
 });
