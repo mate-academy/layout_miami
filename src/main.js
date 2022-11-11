ad756ca25188a32ce73715343b2853fa1a71b02a
vadiimvooo <ASD4S.Profile@gmail.com>
2022-11-11 15:42:22 +0200

    Fixed

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
