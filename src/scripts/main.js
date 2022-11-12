e482a81906d92de007088baaaae5a2c31f6485f8
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2022-11-12 11:19:29 +0200

    MyBike-V6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,17 @@
 'use strict';
 
-if (window.location.hash === '#menu') {
-  document.body.classList.add('page__menu');
-} else {
-  document.body.classList.remove('page__menu');
-};
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
