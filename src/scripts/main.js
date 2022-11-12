81f53c998f23742323bd4093a1d8f46beb4391a7
Yaroslav <yaroslavfoorgala@gmail.com>
2022-11-12 13:08:46 +0200

    Sixth commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ window.addEventListener('hashchange', () => {
 });
 
 const form = document.getElementById('form');
-console.log (form);
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.reset();
