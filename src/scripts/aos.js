1a284693ae8e7be21d93450fdf753b032eeedb7c
Maksym Levchenko <llllewvvaa@gmail.com>
2022-11-14 21:14:22 +0200

    Added animation

---
+++ src/scripts/aos.js
@@ -0,0 +1,21 @@
+'use strict';
+
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 190,
+  delay: 0,
+  duration: 500,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+
+});
