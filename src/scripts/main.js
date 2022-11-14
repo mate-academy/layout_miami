b2c02c99720cbe663e771583939b4bf23e24f5d7
Ninel Levchenko <blackmaplearts@gmail.com>
2022-11-14 15:06:25 +0200

    finished all sections, but required transition and favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// document.querySelector('#iconPhone').addEventListener('mouseup', function() {
-//   document.querySelector('#headerContact').classList.toggle(
-//     'header__contact--visible');
-// });
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#nav-menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
 });
