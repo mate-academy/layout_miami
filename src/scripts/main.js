89e61ba62ed52aa4227c2405ddbb835f18cc9cc9
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-14 20:34:21 +0200

    nav menu effect added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const pageNav = document.querySelector('.page__nav');
 const navIconOpen = document.querySelector('.icon--nav-burger');
 const navIconClose = document.querySelector('.icon--nav-close');
-const pageNav = document.querySelector('.page__nav');
+const navLinks = document.querySelectorAll('.nav__link');
+const navTestRide = document.querySelector('.nav__test-ride');
 
-[navIconOpen, navIconClose].forEach(element => {
+[navIconOpen, navIconClose, navTestRide].forEach(element => {
   element.addEventListener('click', () => {
     pageNav.classList.toggle('page__nav--opened');
+  });
+});
+
+navLinks.forEach(element => {
+  element.addEventListener('click', () => {
+    pageNav.classList.remove('page__nav--opened');
   });
 });
