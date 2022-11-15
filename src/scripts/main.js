32f376f57152f83948218620c72e265ba4e3ef40
Sergey Mikhalskiy <msa1991work@gmail.com>
2022-11-15 15:28:57 +0200

    Lending mybike

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+window.addEventListener('hashchange', () => {
+  window.location.hash === '#menu'
+    ? document.body.classList.add('page__body--with-menu')
+    : document.body.classList.remove('page__body--with-menu');
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
