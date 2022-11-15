4b8ed69a9cd999e0b0e98b1cce6f10ec58ba4620
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-11-15 19:35:25 +0100

    updated javascript for menu links so that menu closes when links are clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,11 +19,36 @@ function formSubmit(event) {
 const menu = document.querySelector('.menu');
 const openMenu = document.querySelector('#openMenu');
 const closeMenu = document.querySelector('#closeMenu');
+const menuHome = document.querySelector('#menuHome');
+const menuAbout = document.querySelector('#menuAbout');
+const menuCompare = document.querySelector('#menuCompare');
+const menuDetails = document.querySelector('#menuDetails');
+const menuContacts = document.querySelector('#menuContacts');
 
 openMenu.addEventListener('click', () => {
   menu.classList.add('page__menu--active');
 });
 
 closeMenu.addEventListener('click', () => {
+  menu.classList.remove('page__menu--active');
+});
+
+menuHome.addEventListener('click', () => {
+  menu.classList.remove('page__menu--active');
+});
+
+menuAbout.addEventListener('click', () => {
+  menu.classList.remove('page__menu--active');
+});
+
+menuCompare.addEventListener('click', () => {
+  menu.classList.remove('page__menu--active');
+});
+
+menuDetails.addEventListener('click', () => {
+  menu.classList.remove('page__menu--active');
+});
+
+menuContacts.addEventListener('click', () => {
   menu.classList.remove('page__menu--active');
 });
