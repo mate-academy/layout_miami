d54bba08732cb00a0187bb89356417424429e05b
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-15 13:32:20 +0200

    scroll disabled when menu is opened

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,15 @@
 'use strict';
 
+const pageBody = document.querySelector('.page__body');
 const pageNav = document.querySelector('.page__nav');
 const navIconOpen = document.querySelector('.icon--nav-burger');
 const navIconClose = document.querySelector('.icon--nav-close');
 const navLinks = document.querySelectorAll('.nav__link');
 const navTestRide = document.querySelector('.nav__test-ride');
 
-[navIconOpen, navIconClose, navTestRide].forEach(element => {
+[navIconOpen, navIconClose, navTestRide, ...navLinks].forEach(element => {
   element.addEventListener('click', () => {
+    pageBody.classList.toggle('page__body--menu-opened');
     pageNav.classList.toggle('page__nav--opened');
-  });
-});
-
-navLinks.forEach(element => {
-  element.addEventListener('click', () => {
-    pageNav.classList.remove('page__nav--opened');
   });
 });
