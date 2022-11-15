2e342a053bdfa56b8632eed03af709ab6491da11
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-16 00:02:49 +0200

    Button Go To Top was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,19 @@ const navIconOpen = document.querySelector('.icon--nav-burger');
 const navIconClose = document.querySelector('.icon--nav-close');
 const navLinks = document.querySelectorAll('.nav__link');
 const navTestRide = document.querySelector('.nav__test-ride');
+const goToTop = document.querySelector('.go-to-top');
 
 [navIconOpen, navIconClose, navTestRide, ...navLinks].forEach(element => {
   element.addEventListener('click', () => {
     pageBody.classList.toggle('page__body--menu-opened');
     pageNav.classList.toggle('page__nav--opened');
   });
+});
+
+window.addEventListener('scroll', () => {
+  if (pageBody.getBoundingClientRect().top < -150) {
+    goToTop.classList.add('go-to-top--show');
+  } else {
+    goToTop.classList.remove('go-to-top--show');
+  }
 });
