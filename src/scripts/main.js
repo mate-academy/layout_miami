fc20e3a14fc2cf62c08a542031b47576b0e6dea6
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-11-16 15:00:00 +0200

    contacts links are now changing color on hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,16 @@
 
 const pageBody = document.querySelector('.page__body');
 const pageNav = document.querySelector('.page__nav');
+const navLogo = document.querySelector('#nav-logo');
 const navIconOpen = document.querySelector('.icon--nav-burger');
 const navIconClose = document.querySelector('.icon--nav-close');
 const navLinks = document.querySelectorAll('.nav__link');
 const navTestRide = document.querySelector('.nav__test-ride');
 const goToTop = document.querySelector('.go-to-top');
 
-[navIconOpen, navIconClose, navTestRide, ...navLinks].forEach(element => {
+const menuBtns = [navLogo, navIconOpen, navIconClose, navTestRide, ...navLinks];
+
+menuBtns.forEach(element => {
   element.addEventListener('click', () => {
     pageBody.classList.toggle('page__body--menu-opened');
     pageNav.classList.toggle('page__nav--opened');
