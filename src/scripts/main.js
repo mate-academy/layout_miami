058a7224b60ebf6a989d763b82895c349dfe46fb
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2022-11-16 20:20:51 +0100

    complete header and menu toggler

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menu = document.getElementById('menu');
+const openMenu = document.querySelector('.open-toggler');
+const closeMenu = document.querySelector('.close-toggler');
+
+openMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.add('show');
+});
+
+closeMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.remove('show');
+});
