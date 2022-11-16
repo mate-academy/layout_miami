3e09e7a31c07a0738c0dc019d5a7aaea444d6a20
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2022-11-16 16:24:04 +0200

    page Bike #3 (add preventDefault)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,13 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const button = document.querySelectorAll('.button');
+
+function Defaultp(event) {
+  event.preventDefault();
+};
+
+button.forEach(buttonItem => {
+  buttonItem.addEventListener('click', Defaultp);
+});
