4042f2b5ebcfd39e886f9a99bbf1dab57174528f
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-11-17 22:37:44 +0200

    added fixes by checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const contactsFormRef = document.querySelector('.contacts__form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+contactsFormRef.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
