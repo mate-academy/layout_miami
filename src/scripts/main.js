940ac31c17314bd1e01de0862397595f412891ba
Gürsel Çakır <hardmatters@outlook.com>
2022-11-18 15:39:56 +0300

    landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
+
+const button = document.querySelector('#form-button');
+
+button.addEventListener('click', formButtonClick, false);
+
+function formButtonClick(event) {
+  event.preventDefault();
+  document.getElementById('form').reset();
+}
