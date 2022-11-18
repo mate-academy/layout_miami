11cd36dcf1962b8fbd8e73a0128bd6078986ecf3
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2022-11-18 21:34:41 +0100

    header smooth transition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const menu = document.getElementById('menu');
 const openMenu = document.querySelector('.open-toggler');
 const closeMenu = document.querySelector('.close-toggler');
+const navLinks = document.querySelectorAll('.nav__link');
 
 openMenu.addEventListener('click', (e) => {
   e.preventDefault();
@@ -13,3 +14,9 @@ closeMenu.addEventListener('click', (e) => {
   e.preventDefault();
   menu.classList.remove('show');
 });
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', (e) => {
+    menu.classList.remove('show');
+  });
+}
