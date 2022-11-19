5b34dd02ba167a7edf576da825db304c5ae0eace
VladimirEdelmann <VladimirEdelmann@gmail.com>
2022-11-20 00:30:43 +0200

    Add menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const addRule = ((style) => {
+  const sheet = document.head.appendChild(style).sheet;
+
+  return function(selector, css) {
+    const propText = typeof css === 'string'
+      ? css : Object.keys(css).map(function(p) {
+        return p + ':' + (p === 'content' ? "'" + css[p] + "'" : css[p]);
+      }).join(';');
+
+    sheet.insertRule(selector + '{' + propText + '}', sheet.cssRules.length);
+  };
+})(document.createElement('style'));
+
+$('.header__phone').mouseenter(() => {
+  addRule('.header__phone:before', {
+    display: 'block',
+  });
+});
+
+$('.header__phone').mouseleave(() => {
+  addRule('.header__phone:before', {
+    display: 'none',
+  });
+});
