a631c6d3ef0e455f4242b9357b48f83eb81bf563
Andrii Petrinko <apetrinko@gmail.com>
2022-11-19 11:58:58 +0200

    fixed cl p15 issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+const form = document.querySelector('form');
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
@@ -6,4 +7,9 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  this.reset();
 });
