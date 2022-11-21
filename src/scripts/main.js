a434c655cb4f8e4b5e6f85d3236a5b4c76bd06fd
Anton Vasyliev <magisterol963@gmail.com>
2022-11-21 09:05:09 +0200

    myBikeFixes1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  event.target.reset();
+});
