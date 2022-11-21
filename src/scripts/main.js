b9bd64436320f47fa1c8422090bcb9886ab83a01
Veronika Yasinska <yasinskaa.veronika@gmail.com>
2022-11-21 10:11:45 +0200

    fixed code review edits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,20 +14,3 @@ form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
   form.reset();
 });
-
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-      change.target.classList.add('scroll-animation-show');
-    }
-  });
-}
-
-const options = { threshold: [0.5] };
-// eslint-disable-next-line no-undef
-const observer = new IntersectionObserver(onEntry, options);
-const elements = document.querySelectorAll('.scroll-animation');
-
-for (const elm of elements) {
-  observer.observe(elm);
-}
