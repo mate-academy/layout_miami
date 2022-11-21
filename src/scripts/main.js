4209ce3874861ddae17b3db4dd996b48335e9551
Ilija Pejanovic <ikadvm96@gmail.com>
2022-11-21 01:58:16 +0100

    added hover, fixed size, disabled scroll on menu, fixed phone hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with_menu');
+  } else {
+    document.body.classList.remove('page__body--with_menu');
+  }
+});
+
+const butt = document.getElementById('myform');
+
+butt.addEventListener('submit', butSubmit, false);
+
+function butSubmit(event) {
+  event.preventDefault();
+  document.getElementById('myform').reset();
+}
