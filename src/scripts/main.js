df00b1440a4c49307b7a8401d2eebb78ff072410
Janos Lukacs <pszichae@gmail.com>
2022-11-21 18:15:16 +0100

    fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--disable-scroll');
   }
 });
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
