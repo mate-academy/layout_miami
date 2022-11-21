857daf8e43509db6e8740617ae85c7dbbd796ad4
JRijo <99342891+John-Stiff@users.noreply.github.com>
2022-11-21 23:48:27 +0000

    created contacts section, updated field att

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,22 @@ window.addEventListener('hashchange', () => {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
-  };
+  }
+});
+
+const element = document.getElementById('message');
+const placeholder = element.getAttribute('data-placeholder');
+
+element.innerHTML === '' && (element.innerHTML = placeholder);
+
+element.addEventListener('focus', function(e) {
+  const value = e.target.innerHTML;
+
+  value === placeholder && (e.target.innerHTML = '');
+});
+
+element.addEventListener('blur', function(e) {
+  const value = e.target.innerHTML;
+
+  value === '' && (e.target.innerHTML = placeholder);
 });
