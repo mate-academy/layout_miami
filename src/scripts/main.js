cc9752daea863edb962390d94f2106d40cc8909e
Nazar Kharkevych <nkharkevych@gmail.com>
2022-11-22 19:31:06 +0200

    Created header for THE MET layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const button = document.getElementById('menu-button');
+const menuContacts = document.getElementById('menu-contacts');
+
+button.addEventListener('click', () => {
+  if (menuContacts.classList.contains('menu__contacts--active')) {
+    menuContacts.classList.remove('menu__contacts--active');
+  } else {
+    menuContacts.classList.add('menu__contacts--active');
+  }
+});
+
+const phone = document.getElementById('icon-phone');
+const headerContacts = document.getElementById('header-contacts');
+
+phone.addEventListener('mouseover', () => {
+  headerContacts.classList.add('header__contacts--active');
+});
+
+document.body.addEventListener('click', () => {
+  headerContacts.classList.remove('header__contacts--active');
+});
