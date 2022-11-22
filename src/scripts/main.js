d73a55ac9111f93a88af515f6cf0aa3033a6513e
Anatolii <batstolya@gmail.com>
2022-11-22 11:00:58 +0200

    Checklist 9,Checklist 14,Checklist 15 - fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', handleForm);
+
+function handleForm(event) {
+  event.preventDefault();
+  this.reset();
+}
