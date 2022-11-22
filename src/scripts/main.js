a5fe830fc0281af26e250b74e505e2f4d735bcba
volkan yildiz <yildizvolkan91@gmail.com>
2022-11-22 23:37:35 +0300

    corrections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const loginForm = document.querySelector('.contacts__form');
+
+loginForm.addEventListener('submit', login);
+
+function login(e) {
+  e.preventDefault();
+}
