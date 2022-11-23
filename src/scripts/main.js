d88851b44b804ad4a57e79bcf69c3c8d73a9f3fe
Igor Galkin <igor_galkin@ukr.net>
2022-11-23 16:41:37 +0200

    2nd commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('contact-us__form');
+const form = document.querySelector('.contact-us__form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
