8e1d13703811c746f35b2b5141771cb11c1ab477
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-25 15:31:34 +0200

    vol_4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const button = document.querySelector('.contacts__button');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+});
