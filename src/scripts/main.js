ee2e0b0da9a7f3490b3ecfab99af33ccff023a7e
Maryna Mazur <marinemazur@gmail.com>
2022-12-03 22:10:43 +0200

    upgraded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,43 @@ form.addEventListener('submit', function(event) {
 
   form.reset();
 });
+
+function addedFooterAnimation() {
+  const footer = document.querySelector('.footer');
+  const footerHeight = footer.clientHeight;
+
+  const scrollHeight = document.body.scrollHeight;
+  const scrollHeightForAnimationStart = (
+    scrollHeight - document.documentElement.clientHeight - footerHeight
+  );
+
+  if (window.scrollY > scrollHeightForAnimationStart) {
+    footer.style.animation = 'footerAppearance 2s linear';
+  }
+}
+
+function addedAnimationForSection() {
+  const allSections = [...document.querySelectorAll('.page__section')].slice(1);
+
+  allSections.forEach(section => {
+    const windowScroll = window.scrollY;
+    const screenClientHeight = document.documentElement.clientHeight;
+    const scrollForAnimation = windowScroll + screenClientHeight + 25;
+    const sectionOffSetTop = section.offsetTop;
+
+    if (section.classList.contains('contacts')) {
+      if (scrollForAnimation - 20 >= sectionOffSetTop) {
+        section.style.animation = 'sectionAnimation 2.5s';
+      }
+    }
+
+    if (scrollForAnimation >= sectionOffSetTop) {
+      section.style.animation = 'sectionAnimation 2.5s';
+    }
+  });
+}
+
+window.addEventListener('scroll', () => {
+  addedAnimationForSection();
+  addedFooterAnimation();
+});
