b233ccba20e94c74f32dba4a65a74773585a1946
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2022-12-05 18:34:53 +0200

    landing bike was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function onWindowHashChange() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+}
+
+window.addEventListener('hashchange', () => {
+  onWindowHashChange();
+});
+
+const button = document.querySelector('.button--send');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+});
