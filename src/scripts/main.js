72ad80fbd6662ed44e58214c8f8574cf0e4b031c
Nazar Kharkevych <nkharkevych@gmail.com>
2022-12-07 12:31:56 +0200

    Fixed menu contacts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,6 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const button = document.getElementById('menu-button');
-const menuContacts = document.getElementById('menu-contacts');
-
-button.addEventListener('click', () => {
-  if (menuContacts.classList.contains('menu__contacts--active')) {
-    menuContacts.classList.remove('menu__contacts--active');
-  } else {
-    menuContacts.classList.add('menu__contacts--active');
-  }
-});
-
 const phone = document.getElementById('icon-phone');
 const headerContacts = document.getElementById('header-contacts');
 
