f2a5fbf9ee94c2633502f042c5d9430839565887
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2022-12-11 23:47:05 +0200

    landing was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
+window.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
