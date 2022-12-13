babbe661f9b5cba7b000162aa98daa7dee249e58
Olha Yevstifieieva <hob.bilbo@gmail.com>
2022-12-13 13:45:44 +0200

    Add footer and section markup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,1 @@
 'use strict';
-// burger menu
-
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', function() {
-  menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
-}
-);
