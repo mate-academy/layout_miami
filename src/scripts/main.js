741686864e58de50d51e130b1924f38b91222e7b
mdm-21 <mitrohin21020@gmail.com>
2022-12-15 20:52:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const myForm = document.getElementById('form');
+
+myForm.addEventListener('submit', function formReset(event) {
+  event.preventDefault();
+  myForm.reset();
+});
