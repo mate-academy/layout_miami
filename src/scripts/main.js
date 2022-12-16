268572766b6e822aac6038daa6b77da914a65027
Roman <romanmalysh@romans-macbook-air.home>
2022-12-16 15:54:22 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+const [...inputs] = document.querySelectorAll('.form__input');
+
+const [first, second] = [...inputs];
+
+console.log(first.value);
+
+form.addEventListener("submit", prevent, false);
+
+function prevent(event) {
+  event.preventDefault();
+  first.value = '';
+  second.value = '';
+}
