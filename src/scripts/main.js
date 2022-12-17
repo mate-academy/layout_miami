122242cbb17a9b2a0ead692fde6a68df788cdddb
Roshan Jha <kartohajha@gmail.com>
2022-12-17 20:10:43 +0200

    menu added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+window.addEventListener('hashchange', () => {
+  if(window.location.hash === '#menu') {
+    document.body.classList.add('page__boddy--with-menu');
+  } else {
+    document.body.classList.remove('page__boddy--with-menu');
+  }
+})
