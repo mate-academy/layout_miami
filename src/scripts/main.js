7c3c06f1d0e4a2dff4acf2e98665dd61b4d6c253
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2022-12-18 18:21:31 +0100

    layout_miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const pageBody = document.querySelector('.page__body');
+const scrollToTop = document.querySelector('.scroll-to-top');
+const form = document.querySelector('#form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('scroll', function() {
+  if (pageBody.getBoundingClientRect().top < -250) {
+    scrollToTop.classList.add('scroll-to-top--show');
+  } else {
+    scrollToTop.classList.remove('scroll-to-top--show');
+  }
+});
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
