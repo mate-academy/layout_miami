45e329310e981f3730f1be913964dcb93d9b3d23
Roman <romanmalysh@romans-macbook-air.home>
2022-12-18 22:08:58 +0100

    18.12.2022

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,11 @@ function prevent(event) {
   second.value = '';
   textArea.value = '';
 }
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
