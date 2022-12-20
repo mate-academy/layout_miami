29eeee7c187e8845d4f8cc272f54165528f70289
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-12-20 16:50:32 +0200

    make changes according to the check list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 'use strict';
 
+// disable scrolling when the menu is open
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+// preventDefault onSubmit
+
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 });
