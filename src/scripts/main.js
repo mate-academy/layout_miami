1a28daaf373d20453b8266fbd20f74366a3d8b70
Yuliia Andrieieva <andrieieva.y@gmail.com>
2022-12-21 19:41:51 +0200

    Updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const buttonForm = document.querySelector('#send__btn');
+
+buttonForm.addEventListener('click', (e) => {
+  e.preventDefault();
+});
