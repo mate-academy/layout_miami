df8737c2b2e50114fdfe2e09e5d4fdb030f897fa
Avichez <starkavichi@gmail.com>
2022-12-22 17:30:58 +0200

    added section contact-us and hover animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
