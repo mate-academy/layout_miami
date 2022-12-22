30d1e632a1885056e4ada8aa61612f5dc4ae3ae2
Margo <margo@Margos-MBP.home>
2022-12-22 14:25:27 +0000

    6/6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('page__menu');
+//   } else {
+//     document.body.classList.remove('page__menu');
+//   }
+// });
+
+const buttonForm = document.querySelector('#form__button');
+
+buttonForm.addEventListener('click', (e) => {
+  e.preventDefault();
+});
