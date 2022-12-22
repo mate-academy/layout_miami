c3bdca34a7c3b3053fe94aa8406138705571fd90
Olha Yevstifieieva <hob.bilbo@gmail.com>
2022-12-22 10:14:54 +0200

    Add fixes/animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,21 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.reset();
 });
+
+// title opacity
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+
+const options = { threshold: [0.5] };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.element-animation');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
