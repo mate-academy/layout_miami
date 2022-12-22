ccd631533a2b5d3df16ec359986a94043a3efcc7
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-12-23 00:50:48 +0200

    fix styles according to comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,37 @@
 
 // disable scrolling when the menu is open
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
+const burgerBtn = document.querySelector('#burger-menu');
+const closeBtn = document.querySelector('#closeBtn-btn');
+const menuArea = document.querySelectorAll('#menu > *');
+const menu = document.querySelector('.page__menu');
+
+burgerBtn.addEventListener('click', () => {
+  document.body.classList.add('page__body--with-menu');
+  menu.classList.add('page__menu--open');
+});
+
+closeBtn.addEventListener('click', () => {
+  document.body.classList.remove('page__body--with-menu');
+  menu.classList.remove('page__menu--open');
+});
+
+menuArea.forEach((el) => {
+  el.addEventListener('click', () => {
     document.body.classList.remove('page__body--with-menu');
-  }
+    menu.classList.remove('page__menu--open');
+  });
 });
 
 // preventDefault onSubmit
 
 const form = document.querySelector('#form');
+const formFields = document.querySelectorAll('.contact__field');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  formFields.forEach(el => {
+    el.value = '';
+  });
 });
