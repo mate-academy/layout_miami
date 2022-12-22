d3e44fe66990f5f24d4b86f0be764e7722dddd04
Kenneth Delgado <kenvastolord@gmail.com>
2022-12-22 15:31:54 +0100

    hover effect added, form can validate, reset inputs and does not reload the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,13 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--no-scroll');
   }
 });
+
+const loginForm = document.querySelector('#form');
+
+loginForm.addEventListener('submit', login);
+document.getElementById('form').reset();
+
+function login(e) {
+  e.preventDefault();
+  document.getElementById('form').reset();
+}
