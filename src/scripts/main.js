e8d995fbf1e8d1db265e86d8df50144af76bed6b
Your Name <you@example.com>
2022-12-22 16:09:25 +0200

    added new elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const checkbox = document.querySelector('#button');
+
+checkbox.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  event.preventDefault();
+}
