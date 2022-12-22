ced729968e9c7ebfefaab0044afb936006f7a468
oleh <shankar.oleh@gmail.com>
2022-12-22 14:40:41 +0200

    checked the checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,21 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('#form-id');
+const name1 = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const submitSuccess = document.querySelector('.submit-success');
+
+form.addEventListener('submit', (e) => {
+  name1.value = '';
+  email.value = '';
+  message.value = '';
+  submitSuccess.classList.add('submit-success--show');
+
+  setTimeout(() => {
+    submitSuccess.classList.remove('submit-success--show');
+  }, 2500);
+  e.preventDefault();
+});
