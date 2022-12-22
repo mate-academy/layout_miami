e7476d2ac6ce1ede8e0c67ff5a6b735abdc7d5d7
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2022-12-22 17:28:22 +0200

    lending general task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', submitClick, false);
+
+function submitClick(event) {
+  event.preventDefault();
+}
