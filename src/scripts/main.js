16a9f692be47360eae4e9e91f3254315abaeb415
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2022-12-22 19:45:04 +0200

    add header + modal window

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const buttonOpen = document.querySelector('.icon--menu-opener');
+const buttonClose = document.querySelector('.icon--cross');
+const modal = document.querySelector('.nav');
+
+buttonOpen.addEventListener('click', openMenu);
+buttonClose.addEventListener('click', closeMenu);
+
+function openMenu() {
+  modal.classList.remove('nav--hidden');
+}
+
+function closeMenu() {
+  modal.classList.add('nav--hidden');
+}
