90a12aef5f1948a4f3de5775648c3b01142c7014
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2022-12-23 23:02:02 +0200

    add 1 block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,14 @@
 const buttonOpen = document.querySelector('.icon--menu-opener');
 const buttonClose = document.querySelector('.icon--cross');
 const modal = document.querySelector('.nav');
+const menuItem = document.querySelectorAll('.nav__link');
 
 buttonOpen.addEventListener('click', openMenu);
 buttonClose.addEventListener('click', closeMenu);
+
+menuItem.forEach(function(item) {
+  item.addEventListener('click', closeMenu);
+});
 
 function openMenu() {
   modal.classList.remove('nav--hidden');
