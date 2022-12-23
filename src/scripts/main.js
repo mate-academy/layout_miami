144958926f363ce0cb3a4e2a07ba6630fe25a2c8
Bohdan Kutasevich <obvinmusic@gmail.com>
2022-12-23 17:21:35 +0200

    all sections were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const form = document.querySelector('#form');
+const nameField = document.querySelector('#name');
+const emailField = document.querySelector('#email');
+const messageField = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  nameField.value = '';
+  emailField.value = '';
+  messageField.value = '';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
