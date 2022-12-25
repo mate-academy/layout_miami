b737253a139b31a5baa4940ca74178d163e90624
nkondd <nkond222@gmail.com>
2022-12-25 17:04:36 +0200

    Fix code due to Checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const checkbox = document.querySelector('#id-button');
+
+checkbox.addEventListener('click', buttonClick, false);
+
+function buttonClick(event) {
+  const warn = "preventDefault() won't let you check this!<br>";
+
+  document.getElementById('output-box').innerHTML += warn;
+  event.preventDefault();
+};
