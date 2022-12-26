d40a1a6590a1302aac4687d5289ce69147ef7935
Yaroslvna Horban <domkomp2709@gmail.com>
2022-12-26 15:42:02 +0100

    errors were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const buttonForm = document.querySelector('#btn');
+
+buttonForm.addEventListener('click', (e) => {
+  e.preventDefault();
+});
