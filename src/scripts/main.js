f4c0313feeccab8e65dcf491682563c691951d70
maasiimka <mrozisalovec@gmail.com>
2022-12-26 23:51:12 +0200

    added responsive retina img's, added favicon, added hover on images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const submitForm = document.querySelector('.contacts__form');
+
+function onSubmit(event) {
+  event.preventDefault();
+}
+
+submitForm.addEventListener('submit', onSubmit);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with--menu');
