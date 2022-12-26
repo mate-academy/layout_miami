56423fe143e5f11f00a22807d1a9f89b3d955476
Margo <margo@Margos-MBP.home>
2022-12-26 20:51:30 +0000

    Bugs were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// window.addEventListener('hashchange', () => {
-//   if (window.location.hash === '#menu') {
-//     document.body.classList.add('page__menu');
-//   } else {
-//     document.body.classList.remove('page__menu');
-//   }
-// });
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
 
 const buttonForm = document.querySelector('#form__button');
 
