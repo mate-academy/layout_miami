965578b161c7e2f4d4d7d5931204bce772e08711
maasiimka <mrozisalovec@gmail.com>
2022-12-29 15:16:38 +0200

    fixed problems with focus, added max-width for button, changed logo link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 
 const submitForm = document.querySelector('.contacts__form');
 const input = document.querySelectorAll('.js-input');
+const focusItems = document.querySelectorAll('.focus');
+const focusMenuItems = document.querySelectorAll('.focus--menu');
 
 function onSubmit(event) {
   event.preventDefault();
@@ -16,7 +18,23 @@ submitForm.addEventListener('submit', onSubmit);
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with--menu');
+
+    for (const item of focusItems) {
+      item.classList.add('focus--disabled');
+    }
+
+    for (const item of focusMenuItems) {
+      item.classList.add('focus--menu--enabled');
+    }
   } else {
     document.body.classList.remove('page__body--with--menu');
+
+    for (const item of focusItems) {
+      item.classList.remove('focus--disabled');
+    }
+
+    for (const item of focusMenuItems) {
+      item.classList.remove('focus--menu--enabled');
+    }
   }
 });
