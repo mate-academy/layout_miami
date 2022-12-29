01705520d4c3e089831c4077ac7c0a42fd90283b
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2022-12-29 13:43:14 +0200

    add some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,15 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const btn = document.getElementById('submit');
+
+btn.addEventListener('click', function handleClick(event) {
+  event.preventDefault();
+
+  const inputs = document.querySelectorAll('#name, #email, #message');
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
