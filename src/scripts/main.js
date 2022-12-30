e1f23341caaf997aac92695fc9c6f6071d74cef5
Anastasiia Kapustiak <anastasiakapustiak@gmail.com>
2022-12-30 13:38:23 +0100

    fixed problem with safe and other mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,16 @@
-// eslint-disable-next-line strict
-const page = document.querySelector('.page');
-const menu = document.querySelector('.menu');
-const form = document.querySelector('.form');
-const fromSubmit = document.querySelector('#form-submit');
-const textArea = document.querySelector('#form-textarea');
-const textAreaSize = document.querySelector('#textarea-resizer');
-
-const getScrollbarWidth = () => {
-  const el = document.createElement('div');
-
-  el.style.cssText = 'overflow:scroll; visibility:hidden; position:absolute;';
-  document.body.appendChild(el);
-
-  const width = el.offsetWidth - el.clientWidth;
-
-  el.remove();
-
-  return width;
-};
-
-const scrollbarWidth = getScrollbarWidth();
-
-menu.style.paddingRight = `${scrollbarWidth}px`;
+'use strict';
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    page.style.paddingRight = `${scrollbarWidth}px`;
-    page.style.overflowY = `hidden`;
+    document.body.classList.add('page__body--with-menu');
   } else {
-    page.style.paddingRight = `0`;
-    page.style.overflowY = `visible`;
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
-textArea.addEventListener('input', () => {
-  textAreaSize.innerText = textArea.value;
-});
+const form = document.querySelector('form');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  form.classList.remove('form--failed-submit');
-  form.reset();
-});
-
-fromSubmit.addEventListener('click', () => {
-  form.classList.add('form--failed-submit');
+  e.target.reset();
 });
