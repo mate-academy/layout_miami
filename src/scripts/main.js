e5824f190e791b2a000c4bcf43e80dfca843c247
Nikita211112 <nikita211112@icloud.com>
2023-01-02 20:57:49 +0100

    landing was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const submit = document.querySelector('#formSubmit');
+
+submit.addEventListener('click', submitClick, false);
+
+function submitClick(e) {
+  e.preventDefault();
+}
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--width-menu');
+  } else {
+    document.body.classList.remove('page__body--width-menu');
+  }
+});
