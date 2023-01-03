b17eee5c2a6d101cb370e2c9c0a3e7fe49e119a6
Eugene Farafonov <eu.farafonov@gmail.com>
2023-01-03 12:29:22 +0100

    project bike - release

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+}
+
+const form = document.getElementById('myform');
+
+form.addEventListener('submit', onFormSubmit);
