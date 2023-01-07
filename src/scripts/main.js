139cf3f9279c6875ca42f4b513a59795cedb0a53
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-07 08:52:17 +0200

    Remove redundant commented iframe

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
-window.addEventListener("hashchange", () => {
-  if (window.location.hash === "#menu") {
-    document.body.classList.add("page__body--with-menu");
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove("page__body--with-menu");
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
 document
-  .getElementsByClassName("contacts__form")[0]
-  .addEventListener("submit", handleForm);
+  .getElementsByClassName('contacts__form')[0]
+  .addEventListener('submit', handleForm);
 
 document
-  .getElementById("send-contacts-button")
-  .addEventListener("click", submitForm);
+  .getElementById('send-contacts-button')
+  .addEventListener('click', submitForm);
 
 function submitForm() {
-  document.getElementsByClassName("contacts__form")[0].reset();
-  document.getElementById("name").required = false;
-  document.getElementById("email").required = false;
-  document.getElementById("message").required = false;
+  document.getElementsByClassName('contacts__form')[0].reset();
+  document.getElementById('name').required = false;
+  document.getElementById('email').required = false;
+  document.getElementById('message').required = false;
 }
 
 function handleForm(event) {
