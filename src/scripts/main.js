567aa94ed0945f1306e83998416c733f7addd833
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-01-08 14:11:16 +0100

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,27 @@
 'use strict';
 
-const submitForm = document.querySelector('.contacts__form');
-const input = document.querySelectorAll('.js-input');
-const focusItems = document.querySelectorAll('.focus');
-const focusMenuItems = document.querySelectorAll('.focus--menu');
-
-function onSubmit(event) {
-  event.preventDefault();
-
-  for (const item of input) {
-    item.value = '';
-  }
-}
-
-submitForm.addEventListener('submit', onSubmit);
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with--menu');
-
-    for (const item of focusItems) {
-      item.classList.add('focus--disabled');
-    }
-
-    for (const item of focusMenuItems) {
-      item.classList.add('focus--menu--enabled');
-    }
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--with--menu');
-
-    for (const item of focusItems) {
-      item.classList.remove('focus--disabled');
-    }
-
-    for (const item of focusMenuItems) {
-      item.classList.remove('focus--menu--enabled');
-    }
+    document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('#form-id');
+const name1 = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const submitSuccess = document.querySelector('.submit-success');
+
+form.addEventListener('submit', (e) => {
+  name1.value = '';
+  email.value = '';
+  message.value = '';
+  submitSuccess.classList.add('submit-success--show');
+
+  setTimeout(() => {
+    submitSuccess.classList.remove('submit-success--show');
+  }, 2500);
+  e.preventDefault();
 });
