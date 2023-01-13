68f1ac81351c00065ae9d29ac8c2a19f99bec93a
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-13 19:02:25 +0200

    the problem with fields was solved

--- src/scripts/resetFields.js
+++ src/scripts/resetFields.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const btn = document.getElementById('btn');
+const form = document.getElementById('my_form');
 
-btn.addEventListener('click', function handleClick(event) {
+form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
 
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
 
-  nameInput.value = '';
-  emailInput.value = '';
+  form.reset();
 });
