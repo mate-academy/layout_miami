c92f6033b56795fcdc3d07120d5b33950dfa8ea8
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-13 19:14:46 +0200

    test issue #2 was solved

--- src/scripts/resetFields.js
+++ src/scripts/resetFields.js
@@ -5,8 +5,5 @@ const form = document.getElementById('my_form');
 form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
 
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-
   form.reset();
 });
