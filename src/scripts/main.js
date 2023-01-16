4a57ff81b311a763de459eeafeadd1c0ce796d08
Yaroslav Liutyi <slavko5481@gmail.com>
2023-01-16 15:46:55 +0200

    fixed errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,9 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--width-menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--width-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
