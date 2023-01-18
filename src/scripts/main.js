f2f8670e7030f1510175473d0e55cea75fc7692a
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-01-18 18:20:54 +0200

    added favicon and remove scrolling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuOpener = document.querySelector('.icon--menu');
+const menuCloser = document.querySelector('.icon--cross');
+const page = document.querySelector('.page');
+
+menuOpener.addEventListener('click', () => {
+  page.style.overflow = 'hidden';
+});
+
+menuCloser.addEventListener('click', () => {
+  page.style.overflow = 'visible';
+});
