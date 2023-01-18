16dace039734d6a18ca1cf3c7408eb5f94fe2edd
Nikita Cherkashin <mikitkaaist@gmail.com>
2023-01-18 11:33:17 +0200

    fixed all

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+const form = document.querySelector('.page__section--send');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
