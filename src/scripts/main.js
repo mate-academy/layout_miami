e05fe18439002d35e05120d597b67a5f79c786b6
Yana <yanamelnychuk10@gmail.com>
2023-01-18 17:50:56 +0200

    full version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.documentElement.style.setProperty('--scrollbar-width',
+  (window.innerWidth - document.documentElement.clientWidth) + 'px');
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
