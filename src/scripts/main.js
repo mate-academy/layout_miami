a778478695faac043c89353b852a651988b281d4
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-01-23 17:18:52 +0200

    page does not refresh after submitting the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// page do not scroll when menu is open
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +8,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// page do not refresh after submitting form
+const form = document.getElementById('formId');
+const mess = document.getElementById('opTag');
+
+function submitForm(event) {
+  event.preventDefault();
+  mess.innerHTML = 'Form submit successful &#10003';
+  form.reset();
+}
+form.addEventListener('submit', submitForm);
