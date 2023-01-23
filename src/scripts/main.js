6a1d86900031a5fea7fd1bf06c98e86a30285273
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-01-23 14:56:27 +0200

    form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,14 @@
 const menuOpener = document.querySelector('.icon--menu');
 const menuCloser = document.querySelector('.icon--cross');
 const menuLink = document.querySelectorAll('.menu__link');
+const button = document.querySelector('.button');
+const form = document.querySelector('.form');
 const page = document.querySelector('.page');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  form.reset();
+});
 
 menuOpener.addEventListener('click', () => {
   page.style.overflow = 'hidden';
