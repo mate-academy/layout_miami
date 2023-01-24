db2572d34b6a65989c8b9d6e8f13075a33d94fca
Intellium7 <lomaleks@gmail.com>
2023-01-24 19:57:37 +0200

    Eleventh commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,15 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu')
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu')
+    document.body.classList.remove('page__body--with-menu');
   }
-
 });
 
+const form = document.querySelector('.contacts-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
