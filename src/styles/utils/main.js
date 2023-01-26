67690d8abb61aca142a9873c549cc9dfcfc0c1c7
spencerspe <spencerspe@gmail.com>
2023-01-26 11:19:23 +0000

    most elements done issue with form

--- src/styles/utils/main.js
+++ src/styles/utils/main.js
@@ -1,1 +1,8 @@
 
+'use strict';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('bose-form').addEventListener('submit', function(e) {
+    e.preventDefault();// Cancel the default action
+  });
+});
