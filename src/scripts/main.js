bb87b69361d74a84a39c455c47d8b3e8ae0d9a10
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-01-26 18:54:24 +0200

    created header block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// nav menu script
+document.querySelector('.burger-menu').addEventListener('click', () => {
+  document.querySelector('.burger-menu').classList.toggle('opened');
+  document.querySelector('.burger-menu').classList.toggle('closed');
+  document.querySelector('.header').classList.toggle('header--nav-opened');
+  document.querySelector('.logo').classList.toggle('logo--nav-opened');
+  document.body.classList.toggle('page__body--with-menu');
+
+  document.querySelector('.header__call').classList
+    .toggle('header__call--nav-opened');
+});
