4aff13cc118ed01aff94fc8e1f3410e9c239831f
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-01-27 14:03:36 +0200

    adaptive header with navigation menu created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelector('.burger').addEventListener('click', () => {
+  document.querySelector('.burger').classList.toggle('burger--close');
+  document.querySelector('.burger').classList.toggle('burger--open');
+  document.querySelector('.nav').classList.toggle('nav--active');
+});
