db1032b2c90b4993e984afae4a73d04e8b550c67
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-01-27 14:20:41 +0200

    improved header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,40 @@
 'use strict';
 
 // nav menu script
-document.querySelector('.burger-menu').addEventListener('click', () => {
-  document.querySelector('.burger-menu').classList.toggle('opened');
-  document.querySelector('.burger-menu').classList.toggle('closed');
-  document.querySelector('.header').classList.toggle('header--nav-opened');
-  document.querySelector('.logo').classList.toggle('logo--nav-opened');
-  document.body.classList.toggle('page__body--with-menu');
+document.querySelector('.burger-menu--closed').addEventListener('click', () => {
+  document
+    .querySelector('.section-hero')
+    .classList.add('section-hero--nav-opened');
+  document.body.classList.add('page__body--with-menu');
+  document.querySelector('.nav').style.visibility = 'visible';
 
-  document.querySelector('.header__call').classList
-    .toggle('header__call--nav-opened');
+  let transitionEnded = false;
+
+  document
+    .querySelector('.nav')
+    .addEventListener('transitionend', (event) => {
+      transitionEnded = true;
+    });
+
+  if (transitionEnded) {
+    // to disable tab selection before and after nav
+    document.querySelector('.header__content').style.visibility = 'hidden';
+    document.querySelector('.main-content').style.visibility = 'hidden';
+  }
+});
+
+document.querySelector('.burger-menu--opened').addEventListener('click', () => {
+  document
+    .querySelector('.section-hero')
+    .classList.remove('section-hero--nav-opened');
+  document.body.classList.remove('page__body--with-menu');
+  // to enable tab selection before and after nav
+  document.querySelector('.header__content').style.visibility = 'visible';
+  document.querySelector('.main-content').style.visibility = 'visible';
+
+  document
+    .querySelector('.nav')
+    .addEventListener('transitionend', (event) => {
+      document.querySelector('.nav').style.visibility = 'hidden';
+    });
 });
