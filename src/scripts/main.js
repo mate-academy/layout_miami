63d7863366c2370888c4060a08f36748e02cc65a
Max Vovk <vovkmax1@gmail.com>
2023-01-27 19:19:41 +0200

    nav_menu was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+if (window.location.hash === '#menu') {
+  document.body.classList.add('page__body--with-menu');
+}
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
