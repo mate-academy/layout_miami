b7b771e2702774a0076889cb86ca6b87de2e7bb3
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-01-27 17:49:45 +0200

    nav-toggle fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,35 +6,11 @@ document.querySelector('.burger-menu--closed').addEventListener('click', () => {
     .querySelector('.section-hero')
     .classList.add('section-hero--nav-opened');
   document.body.classList.add('page__body--with-menu');
-  document.querySelector('.nav').style.visibility = 'visible';
-
-  let transitionEnded = false;
-
-  document
-    .querySelector('.nav')
-    .addEventListener('transitionend', (event) => {
-      transitionEnded = true;
-    });
-
-  if (transitionEnded) {
-    // to disable tab selection before and after nav
-    document.querySelector('.header__content').style.visibility = 'hidden';
-    document.querySelector('.main-content').style.visibility = 'hidden';
-  }
 });
 
-document.querySelector('.burger-menu--opened').addEventListener('click', () => {
+document.getElementById('closeNavBtn').addEventListener('click', () => {
   document
     .querySelector('.section-hero')
     .classList.remove('section-hero--nav-opened');
   document.body.classList.remove('page__body--with-menu');
-  // to enable tab selection before and after nav
-  document.querySelector('.header__content').style.visibility = 'visible';
-  document.querySelector('.main-content').style.visibility = 'visible';
-
-  document
-    .querySelector('.nav')
-    .addEventListener('transitionend', (event) => {
-      document.querySelector('.nav').style.visibility = 'hidden';
-    });
 });
