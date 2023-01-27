470c80bb10cd21e78936503a079b9129f75fc9fd
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-01-27 23:00:55 +0200

    move block added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,7 @@ document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.burger').classList.toggle('burger--close');
   document.querySelector('.burger').classList.toggle('burger--open');
   document.querySelector('.nav').classList.toggle('nav--active');
+
+  document.querySelector('.header__phone')
+    .classList.toggle('header__phone--unvisible');
 });
