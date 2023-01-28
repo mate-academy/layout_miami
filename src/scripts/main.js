f9658ad244cc6e670c244a61aa37131ebbc6b1e5
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-01-28 13:55:57 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,17 @@
 'use strict';
 
-const menuOpener = document.querySelector('.icon--menu');
-const menuCloser = document.querySelector('.icon--cross');
-const menuLink = document.querySelectorAll('.menu__link');
-const button = document.querySelector('.button');
-const nameOfPerson = document.querySelectorAll('.form__input')[0];
-const email = document.querySelectorAll('.form__input')[1];
-const message = document.querySelector('.form__textarea');
 const form = document.querySelector('.form');
 const page = document.querySelector('.page');
 
-button.addEventListener('click', (e) => {
-  if (nameOfPerson.value.length !== 0
-      && message.value.length !== 0
-      && email.value.includes('@')) {
-    form.reset();
-    e.preventDefault();
-  }
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
 });
 
-menuOpener.addEventListener('click', () => {
-  page.style.overflow = 'hidden';
-});
-
-menuCloser.addEventListener('click', () => {
-  page.style.overflow = 'visible';
-});
-
-menuLink.forEach(link => {
-  link.addEventListener('click', () => {
+window.addEventListener('hashchange', (e) => {
+  if (e.newURL === 'http://localhost:8080/#menu') {
+    page.style.overflow = 'hidden';
+  } else {
     page.style.overflow = 'visible';
-  });
+  }
 });
