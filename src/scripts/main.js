64b40b3d39a52bee4afa1a4f11e6f65c5e7fb9e8
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-01-28 16:14:53 +0200

    landing almost ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
-document.querySelector('.burger').addEventListener('click', () => {
-  document.querySelector('.burger').classList.toggle('burger--close');
-  document.querySelector('.burger').classList.toggle('burger--open');
+const toggleNavigation = () => {
+  document.querySelector('.burger').classList.toggle('burger--closed');
+  document.querySelector('.burger').classList.toggle('burger--opened');
   document.querySelector('.nav').classList.toggle('nav--active');
 
   document.querySelector('.header__phone')
     .classList.toggle('header__phone--unvisible');
-});
+};
+
+document
+  .querySelector('.burger')
+  .addEventListener('click', toggleNavigation);
+
+document
+  .querySelector('.nav__link')
+  .addEventListener('click', toggleNavigation);
