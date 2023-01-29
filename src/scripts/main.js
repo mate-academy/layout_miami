93ec5bada7c131bd17bd1f34be65e8ad461ace37
Andrii Kulaev <andrejkulaev@gmail.com>
2023-01-29 16:50:49 +0200

    Form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,17 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__field');
+const textarea = document.querySelector('.form__field--textarea');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+
+  textarea.value = '';
+});
