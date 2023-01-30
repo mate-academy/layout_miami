1b9a05ad3fa2ab630a1e642e204d52adc3a02d44
Polina Kostyria-Mihal <polina.kostyria.mihal@gmail.com>
2023-01-30 13:26:41 +0200

    Page reload bug was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,7 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
+window.addEventListener('hashchange', addaptClasses);
+window.addEventListener('load', addaptClasses);
 
 const form = document.querySelector('#form');
 
@@ -14,3 +9,11 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+function addaptClasses() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+}
