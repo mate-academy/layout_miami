e382ded14037bbd0a6e0e1eba40fd2a7b3158862
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-01-30 18:05:28 +0200

    outline changed to arrow, toggle tabindex when menu open/close

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
+//  toggle nav__links tabindex when menu toggle
+const toggleTabIndex = () => {
+  const toggle = elem => {
+    elem.tabIndex === -1 ? elem.tabIndex = 0 : elem.tabIndex = -1;
+  };
+
+  const links = document.querySelectorAll('.nav__link');
+
+  for (const link of links) {
+    toggle(link);
+  }
+
+  toggle(document.querySelector('.nav__logo'));
+  toggle(document.querySelector('.nav__call'));
+};
+
+// tollge navigation on click on desired buttons
 const toggleNavigation = () => {
   document.querySelector('.burger').classList.toggle('burger--closed');
   document.querySelector('.burger').classList.toggle('burger--opened');
@@ -21,6 +38,8 @@ const toggleNavigation = () => {
 
   document.querySelector('.header__phone')
     .classList.toggle('header__phone--fixed');
+
+  toggleTabIndex();
 };
 
 const clickToToggleNav = element => {
@@ -30,6 +49,7 @@ const clickToToggleNav = element => {
 clickToToggleNav('.burger');
 clickToToggleNav('.nav__logo');
 
+// setting close navigation and smooth scroll after click on nav__link
 const anchors = document.querySelectorAll('.nav__link');
 
 for (const anchor of anchors) {
@@ -46,3 +66,22 @@ for (const anchor of anchors) {
     });
   });
 }
+
+//  clearing the form input after click on submit
+const clearFormInput = () => {
+  const getField = document.querySelectorAll('.contact-form__field');
+
+  for (const field of getField) {
+    field.value = '';
+  }
+
+  document.querySelector('.contact-form__message').value = '';
+};
+
+document
+  .querySelector('.contact-form__submit')
+  .addEventListener('click', elem => {
+    elem.preventDefault();
+    clearFormInput();
+    // alert('Your message is sent');
+  });
