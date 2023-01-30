198d817abda6956096257e0dfc95aaab75df0fb3
Serhii Sorokotiaha <dp080793ssk@gmail.com>
2023-01-30 23:12:42 +0200

    added explore

---
+++ src/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+if (window.location.hash === '#menu') {
+  document.body.classList.add('page__body--menu');
+}
