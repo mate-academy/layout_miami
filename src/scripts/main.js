797f408a609e4557713dbcf1c10ee26cc1a35103
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-02 18:49:29 +0200

    Add preventDefault to Submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,7 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault();
+});
