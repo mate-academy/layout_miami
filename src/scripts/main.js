e2f9e3fab2b7505e4815b56f661e29ae16176187
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-02 22:11:06 +0200

    reset last 3 commits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,23 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-function stopDefAction(evt) {
-  evt.preventDefault();
-}
-
-document.getElementById('form').addEventListener(
-  'click', stopDefAction, false
-);
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function handleClick(event) {
-  event.preventDefault();
-
-  const inputs = document.querySelectorAll('#name, #email, #message');
-
-  inputs.forEach(input => {
-    input.value = '';
-  });
-});
