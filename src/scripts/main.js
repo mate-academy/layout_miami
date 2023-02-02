d23040057ac11363fcb154e6c52809adfbd90c6a
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-02-02 13:43:24 +0200

    fixed code style, added form validation and hover effects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,31 @@
 'use strict';
 
-// nav menu script
-document.querySelector('.burger-menu--closed').addEventListener('click', () => {
+// nav menu
+document.getElementById('openNavButton').addEventListener('click', () => {
   document
     .querySelector('.section-hero')
     .classList.add('section-hero--nav-opened');
   document.body.classList.add('page__body--with-menu');
 });
 
-document.getElementById('closeNavBtn').addEventListener('click', () => {
-  document
-    .querySelector('.section-hero')
-    .classList.remove('section-hero--nav-opened');
-  document.body.classList.remove('page__body--with-menu');
+const closeNavElements = [
+  document.getElementById('closeNavButton'),
+  ...document.querySelectorAll('.nav__link'),
+];
+
+closeNavElements.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    document.body.classList.remove('page__body--with-menu');
+
+    document
+      .querySelector('.section-hero')
+      .classList.remove('section-hero--nav-opened');
+  });
 });
+
+// form submit
+document
+  .getElementById('form')
+  .addEventListener('submit', (event) => {
+    event.preventDefault();
+  });
