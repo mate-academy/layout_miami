428281cbc122cc9d223bdbb9423e4e5130f5ea95
Serhii Sorokotiaha <dp080793ssk@gmail.com>
2023-02-02 21:46:08 +0200

    added contact

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+'use strict';
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
+
 if (window.location.hash === '#menu') {
   document.body.classList.add('page__body--menu');
 }
