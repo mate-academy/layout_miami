fa06dd54963705c97d922d2ae044045e30f2b683
Novyk Anton <novyk.anton@gmail.com>
2023-02-03 12:43:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefAction(event) {
+  document.getElementById('contacts-form').reset();
+  event.preventDefault();
+}
+
+document.getElementById('contacts-form').addEventListener(
+  'submit', stopDefAction, false
+);
