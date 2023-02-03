d671373d9f97112f5764cad76a42b4a8f6de878f
Oleh Rybachuk <ribachuk202@gmail.com>
2023-02-03 21:54:16 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+document.addEventListener('submit', stopDefAction);
