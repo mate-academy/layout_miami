50301da322224a9c764188e6f02401d6e6bcf949
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-02-03 15:25:21 +0200

    fixed form validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,9 +23,62 @@ closeNavElements.forEach((elem) => {
   });
 });
 
-// form submit
-document
-  .getElementById('form')
-  .addEventListener('submit', (event) => {
-    event.preventDefault();
-  });
+// form validation
+
+const submitButton = document.getElementById('formSubmitButton');
+
+const emailIsValid = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+const displayFormError = (message) => {
+  const errorMessageElement = document.getElementById('formErrorMessage');
+
+  errorMessageElement.innerText = message;
+  errorMessageElement.style.display = 'block';
+
+  setTimeout(() => {
+    errorMessageElement.innerText = '';
+    errorMessageElement.style.display = 'none';
+  }, 5000);
+};
+
+const validate = (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById('username');
+  const userEmail = document.getElementById('userEmail');
+  const userMessage = document.getElementById('userMessage');
+
+  if (username.value === '') {
+    displayFormError('Please enter your username!');
+    username.focus();
+
+    return false;
+  }
+
+  if (userEmail.value === '') {
+    displayFormError('Please enter your email address!');
+    userEmail.focus();
+
+    return false;
+  }
+
+  if (!emailIsValid(userEmail.value)) {
+    displayFormError('Please enter a valid email address!');
+    userEmail.focus();
+
+    return false;
+  }
+
+  if (userMessage.value === '') {
+    displayFormError('Please enter your message!');
+    userMessage.focus();
+
+    return false;
+  }
+
+  return true;
+};
+
+submitButton.addEventListener('click', validate);
