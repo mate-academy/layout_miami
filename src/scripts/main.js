e2c76d12b91fdc40e6152f1bdc99f67c8adf896d
Pindel Kateryna <katpin1400@gmail.com>
2023-02-06 16:13:30 +0100

    fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contacts__form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    event.target.reset();
+    alert(`Information is sended`);
+})
