44ccd922df48559967d28529bf7cbafc88836f4f
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-08 16:40:30 +0200

    Fixed small problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
-"use strict";
+'use strict';
 
-window.addEventListener("hashchange", () => {
-  if (window.location.hash === "#menu") {
-    document.body.classList.add("page__body--with-menu");
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove("page__body--with-menu");
+    document.body.classList.remove('page__body--with-menu');
   }
 });
+
+let form = document.querySelector("#contacts_form")
+
+function submitHandler(submitEvent) {
+  submitEvent.preventDefault();
+}
+
+form.addEventListener("submit", submitHandler);
