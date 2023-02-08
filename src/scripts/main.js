7cfd57e94305bd4716801d90d241a90639860847
Vladyslav Penkin <vy891078@gmail.com>
2023-02-09 00:43:41 +0200

    header block completed on 80%. Now header block optimized only for mobile versiongit add README.md!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menu = document.querySelector('.nav');
+
+const openbutton = document.querySelector('.header__toggle');
+
+openbutton.addEventListener('click', function() {
+  menu.classList.remove('nav--closed');
+  menu.classList.add('nav--open');
+});
+
+const closebutton = document.querySelector('.nav__button-close');
+
+closebutton.addEventListener('click', function() {
+  menu.classList.remove('nav--open');
+  menu.classList.add('nav--closed');
+});
