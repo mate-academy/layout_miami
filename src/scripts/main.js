4a1e9a2c00a28164b032c4b48c8d1b21860a385d
timoxa2t <alex2t172@gmail.com>
2023-02-08 18:11:33 +0200

    mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const menuToggler = document.querySelector('.nav__link--burger-menu');
+const menu = document.querySelector('.header__menu');
+
+menuToggler.addEventListener('click', (event) => {
+  menu.classList.toggle('is-active');
+});
