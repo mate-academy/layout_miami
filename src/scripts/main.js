aec1dc6b9b0a65b7402882c7f40e8b4770780dbf
andrej <andersontwentytwo@gmail.com>
2023-02-09 20:54:28 +0200

    Why Buy block implemented, added some animations as well

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 // function onEntry(entry) {
 //   entry.forEach(change => {
 //     if (change.isIntersecting) {
-//       change.target.classList.add('page__block-anpearence');
+//       change.target.classList.add('animation');
 //     }
 //   });
 // }
 // let options = { threshold: [0.5] };
 // let observer = new IntersectionObserver(onEntry, options);
-// let elements = document.querySelectorAll('.page__section');
+// let elements = document.querySelectorAll('.header__title');
 // for (let elm of elements) {
 //   observer.observe(elm);
 // }
