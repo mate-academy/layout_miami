f241a0c7527b3427327d966c34436836211dd595
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-02-09 11:59:02 +0200

    Landing - 1st block created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const menuBtn = document.querySelector('.header__menu-button');
+
+const menu = document.querySelector('.header__hidden-menu');
+
+menuBtn.addEventListener('click', function() {
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
