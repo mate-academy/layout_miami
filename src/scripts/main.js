169f628d3c93d17cfa305e3e072d94db3fb6fce3
Rashyd <rashydlezgi@gmail.com>
2023-02-09 19:17:32 +0200

    site was improved to fit checklist requirements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// toggle body classes when menu is opened/closed
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__content--menu-opened');
@@ -7,3 +8,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__content--menu-opened');
   }
 });
+
+// don't refresh page on submit
+const form = document.getElementById('#form');
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
