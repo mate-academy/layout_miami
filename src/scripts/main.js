8b90a57a939e046508dec7e9792e99b909584c21
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-02-09 22:44:31 +0200

    hide scroll in navigation was implement, made smooth scroll behavior for all page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bgImage = document.querySelector('.content__header-wrapper');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#navigation') {
+    bgImage.classList.add('content__header-wrapper--scroll-hide');
+    document.body.classList.add('content--with-menu');
+  } else {
+    bgImage.classList.remove('content__header-wrapper--scroll-hide');
+    document.body.classList.remove('content--with-menu');
+  }
+});
