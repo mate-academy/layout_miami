0998d190a7a2b5fb744466f9ebef38be0d502ffc
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-02-09 15:57:53 +0200

    details block was finished and adapted for all screen widths

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 'use strict';
+
+// const content = document.querySelector('.content__container');
+// const menuBtn = document.querySelector('.top-actions__menu-btn');
+// const crossBtn = document.querySelector('.top-actions__cross--dark');
+
+// menuBtn.addEventListener('click', () => {
+//   setTimeout(
+//     content.classList.add('content__container--close'), 300
+//   );
+// });
+
+// crossBtn.addEventListener('click', () => {
+//   content.classList.remove('content__container--close');
+// });
