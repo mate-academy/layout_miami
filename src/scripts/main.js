80fe82841be82ab521de056edff142c817b1fafe
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-02-09 23:39:35 +0200

    add hover effect for details photos, information links and cross button, made prevent default actions function for form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const bgImage = document.querySelector('.content__header-wrapper');
+const submitButton = document.querySelector('.contact-us__button');
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#navigation') {
@@ -10,4 +11,8 @@ window.addEventListener('hashchange', () => {
     bgImage.classList.remove('content__header-wrapper--scroll-hide');
     document.body.classList.remove('content--with-menu');
   }
+});
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
 });
