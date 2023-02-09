800240d9d3caf664385de1f32ae9f5453e74b22e
andrej <andersontwentytwo@gmail.com>
2023-02-09 16:46:36 +0200

    Browse Bose products block implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('page__block-anpearence');
+//     }
+//   });
+// }
+// let options = { threshold: [0.5] };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.page__section');
+// for (let elm of elements) {
+//   observer.observe(elm);
+// }
