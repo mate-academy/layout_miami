d3166fc0b555dc179a7e230a1c643ed456d4489c
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-02-10 14:42:03 +0200

    MyBike landing created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,15 @@ const menuBtn = document.querySelector('.header__menu-button');
 
 const menu = document.querySelector('.header__hidden-menu');
 
+const menuLinks = document.querySelector('.menu__nav');
+
 menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
+});
+
+menuLinks.addEventListener('click', function() {
+  menuLinks.classList.toggle('active');
+  menu.classList.toggle('active');
+  menuBtn.classList.toggle('active');
 });
