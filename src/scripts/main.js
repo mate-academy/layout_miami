f2c6ea80a149078839f067ff318622a521268478
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-02-10 17:58:01 +0200

    Landing finish v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+// animation start
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  }
+  );
+}
+
+const options = {
+  threshold: [0.5],
+};
+/* eslint-disable */
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.element-animation');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
+// animation end
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('.button').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.querySelectorAll.forEach('.form-field').reset();
+});
