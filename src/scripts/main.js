d2a367fd38b1326e055fe3b4645aceb0adb213f8
Vadim Bratus <vadim.bratus@gmail.com>
2023-02-10 14:16:47 +0200

    did all lending

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const menuBtn = document.querySelector('.icon--menu');
+const menuClose = document.querySelector('.icon--close');
+
+menuClose.addEventListener('click', () => {
+  document.body.classList.remove('page__body--width-menu');
+});
+
+menuBtn.addEventListener('click', () => {
+  document.body.classList.add('page__body--width-menu');
+});
