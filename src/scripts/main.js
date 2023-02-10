46e0bf0a6f7e62dd87230d33b41458ca89e4ebad
Denys Dubas <den.dubas.44@gmail.com>
2023-02-10 19:56:07 +0200

    fixed mistakes and added all blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const checkbox = document.querySelector('.button--form');
+
+checkbox.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  const warn = "preventDefault() won't let you check this!<br>";
+
+  document.getElementById('.button--form').innerHTML += warn;
+  event.preventDefault();
+}
