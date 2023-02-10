d5f9a7842488a074330e98b05044f165a4727d89
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-02-10 18:10:04 +0200

    fixed menu scrolling issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
+const page = document.querySelector('.page');
+const form = document.querySelector('.form');
+
 window.addEventListener('hashchange', () => {
   if (document.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    page.style.overflow = 'hidden';
   } else {
     document.body.classList.remove('page__body--with-menu');
+    page.style.overflow = 'auto';
   }
 });
-
-const form = document.querySelector('.form');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
