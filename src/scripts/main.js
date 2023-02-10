4d91bbe2d90973fa087e57067f7b8a21cd2cf7e2
timoxa2t <alex2t172@gmail.com>
2023-02-10 19:30:05 +0200

    finished grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,1 @@
 'use strict';
-
-const menuToggler = document.querySelector('.nav__link--burger-menu');
-const menuTogglerClose = document.querySelector('.nav__link--close-menu');
-
-const menu = document.querySelector('.menu');
-
-menuToggler.addEventListener('click', toggleMenu);
-menuTogglerClose.addEventListener('click', toggleMenu);
-
-function toggleMenu(event) {
-  menu.classList.toggle('menu--hidden');
-}
