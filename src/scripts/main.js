a3d061816df9976c6d4d899a89391593935267c5
Natalia <savenets.natalia30@gmail.com>
2023-02-11 14:23:29 +0000

    project done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function stopDefAction(e) {
+  e.preventDefault();
+}
+
+document.getElementById('submitButton').addEventListener(
+  'submit', stopDefAction, false
+);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
