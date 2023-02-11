ee62ed53b7cb5c1173988d3196634adc239f1522
Anna <arkushenko98@gmail.com>
2023-02-11 17:36:53 +0300

    reload submit cancel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const pageReload = document.querySelector('#submitButton');
+
+pageReload.addEventListener('submit', function formReload(event) {
+  event.preventDefault();
+
+  pageReload.reset();
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
