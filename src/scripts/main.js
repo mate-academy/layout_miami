966ac4c0c5ad575f4cd929da99056959014ac4ed
Viktor Vaskovics <vasha2020@gmail.com>
2023-02-11 18:22:12 +0000

    almost final commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,6 @@ window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--width-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.body.classList.remove('page__body--width-menu');
   }
 });
