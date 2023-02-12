38904215884daf6cf9205a412b01f42a60231de1
Sasha Kononenko <nurchiksasha@gmail.com>
2023-02-12 12:37:30 +0200

    fixed bugs and inplement AOS lib

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
-'use strict';
+// 'use strict';
+
+const form = document.querySelector('.contacts__form');
+const inputValue = document.querySelectorAll('.contacts__field');
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
@@ -6,4 +9,33 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__content--with-menu');
   }
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputValue.forEach(input => {
+    input.value = '';
+  });
+});
+
+/* eslint-disable  */
+ AOS.init({
+  // Global settings:
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
 });
