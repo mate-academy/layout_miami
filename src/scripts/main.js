911810cc36ffcfe1b5bac8e0422b98cea5a06533
Yaroslav <yaroslav.dzyadiv@gmail.com>
2023-02-14 00:45:41 +0200

    Disabled nav scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--with-nav');
+  } else {
+    document.body.classList.remove('page__body--with-nav');
+  }
+});
