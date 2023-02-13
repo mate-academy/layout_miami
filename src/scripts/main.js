feab75169b49cfed28d9254612e8742b99e58209
Anastasiia Tarasova <tarasova0721@gmail.com>
2023-02-13 10:30:04 +0200

    animation on scroll was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,44 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   document.getElementById('form').reset();
 });
+
+const scrollElements = document.querySelectorAll('.scroll');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop
+    <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('animated');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('animated');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
