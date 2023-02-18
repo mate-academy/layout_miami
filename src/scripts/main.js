d0c90bbd64da91b414442fcdcf5c07502a913a0c
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2023-02-18 19:11:48 +0200

    header and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menu = 'menu';
+const menuOpen = 'menu--open';
+const pageBody = 'page-body';
+const blur = 'page__body--blur';
+
+document
+  .getElementById('menu-icon')
+  .addEventListener('click', function(e) {
+    document.getElementById(pageBody).classList.add(blur);
+    document.getElementById(menu).classList.add(menuOpen);
+  });
+
+document
+  .getElementById('menu-close')
+  .addEventListener('click', function(e) {
+    document.getElementById(pageBody).classList.remove(blur);
+    document.getElementById(menu).classList.remove(menuOpen);
+  });
