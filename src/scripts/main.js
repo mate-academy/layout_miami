956245f2c71ee804fd9a44f74e15cb5149558220
Serhii Kompaniets <Kompaniets2014@gmail.com>
2023-02-19 20:00:46 +0200

    error correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,16 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu')
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu') 
+    document.body.classList.remove('page__body--with-menu');
   }
-})
+});
+
+const form = document.querySelector('YOUR_FORM_SELECTOR');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
