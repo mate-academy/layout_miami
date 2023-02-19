d1db7756c9f641c39cb2875cd034f7aba674b61b
Dmytro Vovk <bellboy.dv@gmail.com>
2023-02-19 17:32:06 +0200

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const buttonId = document.getElementById('buttonId');
+
+buttonId.addEventListener('click', function(event) {
+  event.preventDefault();
+});
