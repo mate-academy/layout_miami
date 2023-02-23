f8ba376366b027f7d6a303c520883098c660f9e0
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2023-02-23 17:39:49 +0200

    fixed button and added js code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,28 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('content--with-menu');
   }
 });
+
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', onButtonClick);
+
+// function onButtonClick(event) {
+//   event.preventDefault();
+//   form.reset();
+// }
+
+// const form = document.querySelector('.contact-us__field');
+
+// form.addEventListener('submit', submit);
+
+// function submit(event) {
+//   event.preventDefault();
+//   form.reset();
+// }
+
+const form = document.querySelector('.form-field');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
