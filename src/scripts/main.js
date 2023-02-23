bdfd459f2ddd066568897cff3d59d1af92d28a00
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2023-02-23 20:39:45 +0200

    added corect js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,25 +8,7 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// const form = document.querySelector('form');
-
-// form.addEventListener('submit', onButtonClick);
-
-// function onButtonClick(event) {
-//   event.preventDefault();
-//   form.reset();
-// }
-
-// const form = document.querySelector('.contact-us__field');
-
-// form.addEventListener('submit', submit);
-
-// function submit(event) {
-//   event.preventDefault();
-//   form.reset();
-// }
-
-const form = document.querySelector('.form-field');
+const form = document.querySelector('.form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
