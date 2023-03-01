0ba4125265f5c84681497d05905548ce344cdb53
Yurii Komanitskiy <komanitskiy200320@gmail.com>
2023-03-01 13:12:56 +0200

    fix 1, 4, 8, 9. 10, 14, 15 and  styles according to mockup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const submitButton = document.querySelector('#id-submit');
+
+submitButton.addEventListener('click', submitButtonClick);
+
+function submitButtonClick(event) {
+  event.preventDefault();
+}
