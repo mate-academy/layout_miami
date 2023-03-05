c60f4b3a4e148d839c78ec10c0c51dc77a02cd48
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-03-05 14:25:34 +0200

    Fixed page reload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const pageReload = document.querySelector('#form-reload');
+
+pageReload.addEventListener('submit', function formReload(event) {
+  event.preventDefault();
+  pageReload.reset();
+});
