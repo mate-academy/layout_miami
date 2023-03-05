eb5963b144b21d7488c4ba9ddd565fe8396fd960
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-03-05 12:36:48 +0100

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const formNotReset = document.getElementById('form');
+
+formNotReset.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formNotReset.reset();
+});
