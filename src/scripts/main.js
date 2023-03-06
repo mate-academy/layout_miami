51b67afc73ec38b31ad0ddc341266793f8ce3b62
Roman <rdyakovich@gmail.com>
2023-03-06 16:51:03 +0200

    bike-update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu ');
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("page__body--with-menu");
   } else {
-    document.body.classList.remove('page__body--with-menu ');
+    document.body.classList.remove("page__body--with-menu");
   }
+});
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  form.reset();
 });
