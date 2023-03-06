b271f99de7064ea7ee07ae6c3c9d4ccedbfc6e8d
Antonio S <soi4.toxa@ukr.net>
2023-03-06 16:02:24 +0200

    MyBike - finisher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,8 @@ window.addEventListener('hashchange', () => {
 });
 
 const form = document.querySelector('.form');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   form.reset();
-}) 
+});
