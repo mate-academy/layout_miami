257acc7e4efb2cbff53eb53e3d50db9e70ef5bcc
Andrii <remote.makandriival@gmail.com>
2023-03-07 15:05:46 +0200

    menuBtnFuncAdded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menuIcon = document.querySelector('.header__menuIcon');
+const menuItems = document.querySelector('.header__nav');
+const menuClose = document.querySelector('.header__nav__closeBtn');
+
+const handleOpenClose = () => {
+  menuItems.classList.toggle('header__nav--open');
+};
+
+menuIcon.addEventListener('click', () => {
+  handleOpenClose();
+});
+
+menuClose.addEventListener('click', () => {
+  handleOpenClose();
+});
