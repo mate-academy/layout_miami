1f18dcef8349461504f36b60964e60e69613549c
glots-a <glots@ukr.net>
2023-03-08 16:20:48 +0200

    layoyt finish commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.querySelector('wrapper').classList.add('wrapper--with-menu');
+  } else {
+    document.querySelector('wrapper').classList.remove('wrapper--with-menu');
+  }
+});
