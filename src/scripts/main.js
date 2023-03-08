dd945095d6cdde429baa4c16b9fc56b8e46e593d
Myla Crystal <code.crystal.008@gmail.com>
2023-03-08 21:25:29 +0200

    lint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,23 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+const userName = document.getElementById('user-name');
+const userEmail = document.getElementById('user-email');
+const userMessage = document.getElementById('user-message');
+const success = document.getElementById('success');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  userName.value = '';
+  userEmail.value = '';
+  userMessage.value = '';
+
+  success.classList.add('success--open');
+
+  setTimeout(function() {
+    success.classList.remove('success--open');
+  }, 3000);
+});
