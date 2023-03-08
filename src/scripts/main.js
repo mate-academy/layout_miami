81ca45113646a972a2c900c839873dbdf5a61dca
Denys Krysanov <denis.krisanov321@gmail.com>
2023-03-08 14:45:21 +0200

    js added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const checkForOpenMenu = () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-open');
+  } else {
+    document.body.classList.remove('page__body--menu-open');
+  }
+};
+
+window.addEventListener('hashchange', () => {
+  checkForOpenMenu();
+});
+
+const formElement = document.getElementById('form');
+const nameElement = document.getElementById('formName');
+const emailElement = document.getElementById('formEmail');
+const textareaElement = document.getElementById('formTextarea');
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  nameElement.value = '';
+  emailElement.value = '';
+  textareaElement.value = '';
+
+  window.alert('Form submitted!');
+});
+
+checkForOpenMenu();
