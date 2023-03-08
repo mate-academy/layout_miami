9cadecf753c598f48fb432ae7573882a5538242c
Ihor <ikruzhynskyy@gmail.com>
2023-03-08 22:25:48 +0200

    added the overflow hidden to the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const checkForOpenMenu = () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--menu-open');
+  } else {
+    document.body.classList.remove('page__body--menu-open');
+  }
+};
+
+window.addEventListener('hashchange', () => {
+  checkForOpenMenu();
+});
