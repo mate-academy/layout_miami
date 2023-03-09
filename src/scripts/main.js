4d1a6f4c194c8a33a7e8036baab841b49460399d
glots-a <glots@ukr.net>
2023-03-09 17:34:15 +0200

    form correction and owerflowY added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,8 +10,28 @@ window.addEventListener('hashchange', () => {
 
 // prevent default form submit
 
+const form = document.getElementById('form');
+const respond = document.getElementById('email');
 const formButton = document.querySelector('.contact__button');
+const allItems = document.querySelectorAll('.form-field');
 
 formButton.addEventListener('click', function(e) {
   e.preventDefault();
+
+  allItems.forEach(item => {
+    if (!item.value) {
+      item.classList.add('form--request');
+    } else {
+      item.classList.remove('form--request');
+    }
+  });
+
+  const reqExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+
+  if (!reqExp.test(respond.value)) {
+    respond.classList.add('form--request');
+  } else {
+    respond.classList.remove('form--request');
+    form.reset();
+  }
 });
