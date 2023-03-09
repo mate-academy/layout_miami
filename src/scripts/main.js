c5a26e0aff2966dc7d57fdb95aa3177228bfd38c
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2023-03-10 00:29:22 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const buttonContants = document.body.querySelector('.contacts__buttom');
+
+buttonContants.addEventListener('click', (event) => {
+  const inputs = document.querySelectorAll('.form-field');
+
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
