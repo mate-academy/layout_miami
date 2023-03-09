727f2aa21d2d9f6a2fa87740a1448ce8e5e55843
Oleh Kolodii <bigknife4@gmail.com>
2023-03-09 13:12:18 +0200

    made small improvements according to the st review

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+window.onload = function() {
+  const subForm = document.getElementById('submitForm');
+
+  subForm.addEventListener('submit', clear);
+
+  function clear() {
+    subForm.reset();
+  }
+};
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__menu--active');
