047be11190a99e07db8ec9de0cae6ab67cedd0a8
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-03-09 20:08:37 +0100

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-  document.body.classList.add('page__body--with-menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
