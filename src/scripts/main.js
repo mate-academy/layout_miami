ffb485eb7767dac6001e6979b511349b13b01601
Rage1502 <karaulchenkoilja@gmail.com>
2023-03-09 16:08:52 +0200

    refactoring and hot-fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const buttonForm = document.querySelector('#form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+buttonForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  buttonForm.reset();
+});
