8fe2bb64d790aa3ec36ca81067753453b11edc84
kukla1989 <kukla.1989@ukr.net>
2023-03-10 17:28:39 +0200

    add contact us section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+});
+
+const form = document.querySelector('.contact__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
