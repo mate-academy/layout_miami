44b57bef96a8c394b6245c9f08db9264ca85e063
Bohdan Humeniuk <gumenukbogdan947@gmail.com>
2023-03-10 18:45:59 +0200

    finish line

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
-// window.addEventListener('hashchange', () => {
-//   if (window.location.hash === '#menu') {
-//     document.body.classList.add('page__body--with-menu');
-//   } else {
-//     document.body.classList.remove('page__body--with-menu');
-//   }
-// });
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
 
-/* page__body--with-menu */
+const form = document.querySelector('.page__section--contact');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
