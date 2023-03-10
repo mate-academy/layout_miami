03ee73bd9aff30b2547f85791173b5e8f1a00184
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-10 14:50:42 +0100

    nav html + css + js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const navBtn = document.querySelector('#nav-menu-trigger');
+const nav = document.querySelector('.nav');
+const navLogo = document.querySelector('.banner__logo');
+
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav--show');
+  navBtn.classList.toggle('banner__menu-btn--close');
+  navLogo.classList.toggle('banner__logo--black');
+});
