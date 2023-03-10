2e554adb6c51c7da87f71d6aee3c7fd100dcb1f7
Mykola Plekan <zikzar@gmail.com>
2023-03-10 23:19:35 +0200

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-document.getElementById('myAnchor').addEventListener('click', function(event) {
-  event.preventDefault();
-});
