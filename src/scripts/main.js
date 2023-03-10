f38261e568a8e321e0f81224389676d72c728bd2
kozlo <kozlovilla@gmail.com>
2023-03-10 15:21:20 +0200

    corrected some mistaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page--with-menu');
   }
 });
+
+const form = document.getElementById('contacts');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
