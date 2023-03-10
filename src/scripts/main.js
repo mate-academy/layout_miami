d1caab8f1fcbb8e8e0bcb57fa09d258ced7c538a
Ostap Sokolov <thefallcon2001@gmail.com>
2023-03-10 11:52:13 -1200

    fixed scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
