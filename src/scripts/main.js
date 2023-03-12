fc163311ba05ff9f3ba5536811a3d511f7b7bfd6
kukla1989 <kukla.1989@ukr.net>
2023-03-12 15:41:06 +0200

    adapt for tablet, improve pictures

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,11 @@ form.addEventListener('submit', function(event) {
 
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
