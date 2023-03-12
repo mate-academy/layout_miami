f08648911ee6dd37c6ad636eb88b2968ef37a5af
Petro Boyko <petroboyko198608@gmail.com>
2023-03-12 11:27:06 +0200

    development header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const menuBtnOpen = document.querySelector('.top-actions__btn--open');
+const menuBtnClose = document.querySelector('.top-actions__btn--close');
+const menu = document.querySelector('.menu');
+const scrollHidden = document.querySelector('.page__body');
+
+menuBtnOpen.addEventListener('click', function() {
+  menu.classList.add('active');
+  scrollHidden.classList.add('active');
+});
+
+menuBtnClose.addEventListener('click', function() {
+  menu.classList.remove('active');
+  scrollHidden.classList.add('active');
+});
