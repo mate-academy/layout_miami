bb39281141b5b290334016bc9a9229761ac9da56
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-03-12 21:44:50 +0200

    added exhibition block style, changed favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+function submitForm(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
