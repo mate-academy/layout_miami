ce670c86889451fe32fb588b3b2d7f1e7dc9df6c
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-03-13 16:19:58 +0200

    landing done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButtonOpen = document.querySelector('.top-actions__icon--menu');
+  const menuButtonClose = document.querySelector('.top-actions__icon--cross');
+  const body = document.querySelector('.body');
+  const menuHeaderPage = document.querySelector('.header__menu');
+
+  menuButtonOpen.addEventListener('click', () => {
+    menuHeaderPage.classList.add('active');
+    body.classList.add('lock');
+  });
+
+  menuButtonClose.addEventListener('click', () => {
+    menuHeaderPage.classList.remove('active');
+    body.classList.remove('lock');
+  });
+});
