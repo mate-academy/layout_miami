a86e7ef0fbcef678e2829e2d7ff273b6fba6500c
EvgenBabin <ebabin000@gmail.com>
2023-03-13 15:22:05 +0200

    some changes

--- src/main.js
+++ src/main.js
@@ -7,3 +7,11 @@ form.addEventListener('submit', function(event) {
 
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
