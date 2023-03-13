da7be66a6ae046279c97bc14b33967db0baed59e
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-03-13 10:17:43 +0200

    fixed smooth scroll, removed box-shadow, added js when  clicking on logo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+document.querySelector('.logo').addEventListener('click', function(event) {
+  event.preventDefault(); // цей рядок запобігає перезавантаженню сторінки
+  window.location.href = './index.html'; // змінюємо URL на адресу головн стор
+});
+
 document.querySelector('.contactForm')
   .addEventListener('submit', function(event) {
     this.reset(); // очищення полів форми
