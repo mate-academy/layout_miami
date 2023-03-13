b331008707ebeca5f43e01bd0d8d1c7ae82248d8
Danylo Dobrovolskyi <d.dobrovolsky44@gmail.com>
2023-03-13 17:37:14 +0200

    added scroll, fixed size, removed hover and added transition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const form = document.querySelector('.contact-us__form');
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contacts__form');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
