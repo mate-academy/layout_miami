3796dd7aef2c750b9544f34a753a37b64d21afc7
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-03-13 22:45:06 +0200

    fixed according mentors recommendations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonsSubmit = document.querySelector('.form .button[type="submit"]');
 
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('lock');
-  } else {
-    document.body.classList.remove('lock');
-  }
+  window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+      document.body.classList.add('lock');
+    } else {
+      document.body.classList.remove('lock');
+    }
+  });
+
+  buttonsSubmit.addEventListener('click', (event) => {
+    buttonsSubmit.parentElement.reset();
+    event.preventDefault();
+  });
 });
