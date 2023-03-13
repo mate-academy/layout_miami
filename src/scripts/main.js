d58bf18abdd2cc012c91820715a0c1d1723550df
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2023-03-13 18:15:52 +0200

    Met landing completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,49 @@
 
 const menu = 'menu';
 const menuOpen = 'menu--open';
-const pageBody = 'page-body';
-const blur = 'page__body--blur';
+const header = 'header';
+const pageWithMenu = 'page__body--with-menu';
+const blur = 'header--blur';
+
+const bodyElement = document.body;
+const headerElement = document.getElementById(header);
+const menuElement = document.getElementById(menu);
+const menuList = document.getElementById('menuList');
+const formElement = document.getElementById('form');
 
 document
   .getElementById('menu-icon')
   .addEventListener('click', function(e) {
-    document.getElementById(pageBody).classList.add(blur);
-    document.getElementById(menu).classList.add(menuOpen);
+    bodyElement.classList.add(pageWithMenu);
+    headerElement.classList.add(blur);
+    menuElement.classList.add(menuOpen);
   });
 
 document
   .getElementById('menu-close')
   .addEventListener('click', function(e) {
-    document.getElementById(pageBody).classList.remove(blur);
-    document.getElementById(menu).classList.remove(menuOpen);
+    closeMenu();
   });
+
+document
+  .getElementById('menuLogo')
+  .addEventListener('click', function(e) {
+    closeMenu();
+  });
+
+menuList.addEventListener('click', function(e) {
+  if (e.target.classList.contains('menu__link')) {
+    closeMenu();
+  }
+});
+
+const closeMenu = () => {
+  bodyElement.classList.remove(pageWithMenu);
+  headerElement.classList.remove(blur);
+  menuElement.classList.remove(menuOpen);
+};
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formElement.reset();
+});
