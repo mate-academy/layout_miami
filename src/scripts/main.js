9c3bcf723e8a5b9b6181e9b8a4393f647d66046d
Nikolai Melnichenko <nikolaymelni@gmail.com>
2023-03-14 22:36:31 +0300

    100% done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
