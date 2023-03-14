27f00bccd831f57c22e3b806c0315f05c5db7c73
Пользователь <valerii.footbolist@gmail.com>
2023-03-14 23:27:16 +0200

    add script for contact form and add hover on gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,18 @@ window.addEventListener('hashchange', () => {
 });
 
 const form = document.querySelector('.contact-form');
-form.addEventListener('submit', function (e) {
+
+form.addEventListener('submit', function(e) {
+  let name = document.getElementById('input-name').value;
+  let email = document.getElementById('input-email').value;
+  let message = document.getElementById('input-textarea').value;
+
+  if (name !== "" && email !== "" && message !== "") {
+    alert("Your form send successfully");
+  }  else {
+    alert("Your form is invalid");
+  }
+
   e.preventDefault();
-  form.reset();
-})
+    form.reset();
+});
