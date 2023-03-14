e13d42f9bb19fbafb3996effb7e7edd2ddee2734
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-14 11:52:43 +0100

    changes requested in review video

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,30 @@ navBtn.addEventListener('click', () => {
   navBtn.classList.toggle('banner__menu-btn--close');
   navLogo.classList.toggle('banner__logo--black');
 });
+
+const navLink = document.querySelectorAll('.nav__link--scroll');
+
+navLink.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('nav--show');
+    navBtn.classList.remove('banner__menu-btn--close');
+    navLogo.classList.remove('banner__logo--black');
+  });
+});
+
+const contactBtn = document.querySelector('.contact__btn');
+const cItems = document.querySelectorAll('.contact__input, .contact__textarea');
+
+contactBtn.addEventListener('click', () => {
+  cItems.forEach(item => {
+    if (item.value === '') {
+      item.classList.add('failed');
+    } else {
+      item.classList.remove('failed');
+    }
+  });
+
+  if (document.querySelectorAll('.failed').length === 0) {
+    contactBtn.closest('form').reset();
+  }
+});
