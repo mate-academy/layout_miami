1ee22bf0a5c7a5498e61b48c47f485dd986ff1d5
NikitaStambul <stambul.n@gmail.com>
2023-03-15 14:13:51 +0200

    scroll when menu-opened fix && button cursor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-opened');
+  } else {
+    document.body.classList.remove('page__body--menu-opened');
+  }
+});
+
 const form = document.querySelector('.contacts__form');
 
 form.addEventListener('submit', function(event) {
