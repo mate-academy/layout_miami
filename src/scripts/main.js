1564be0aa5e5d30dd562986b1f0278820579b2ce
Daniel <henio09@wp.pl>
2023-03-16 10:48:16 +0100

    added a side navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let navIsActive = false;
+const menuIcon = document.querySelector('.header__icons-menu-icon');
+
+menuIcon.addEventListener('click', openNav);
+
+function openNav() {
+  if (navIsActive) {
+    document.querySelector('.sidenav').style.left = '100%';
+    menuIcon.classList.remove('isActive');
+    navIsActive = false;
+  } else {
+    document.querySelector('.sidenav').style.left = '0';
+    menuIcon.classList.add('isActive');
+    navIsActive = true;
+  }
+}
+
+menuIcon.addEventListener('click', openNav);
