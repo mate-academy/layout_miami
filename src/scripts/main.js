a2947622821f6e0939f4fba87d48c2a9d81fb4f7
Denys Stronin <den.stronin@gmail.com>
2023-03-16 17:10:20 +0200

    Bose landing. 9th commit - modified styles, form validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,29 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form');
+const respond = document.getElementById('email');
+const formButton = document.querySelector('.contact-us__button');
+const allItems = document.querySelectorAll('.form-field');
+
+formButton.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const reqExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+
+  allItems.forEach(item => {
+    if (!item.value) {
+      item.classList.add('form-field--unvalid');
+    } else {
+      item.classList.remove('form-field--unvalid');
+    }
+  });
+
+  if (!reqExp.test(respond.value)) {
+    respond.classList.add('form-field--unvalid');
+  } else if (allItems[2].value) {
+    respond.classList.remove('form-field--unvalid');
+    form.reset();
+  }
+});
