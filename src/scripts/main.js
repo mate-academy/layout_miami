40c7afebb5bc2b971bab97ef3beefdcfc951e704
Daniel <henio09@wp.pl>
2023-03-16 12:10:23 +0100

    the menu closes when any link is clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 
 let navIsActive = false;
 const menuIcon = document.querySelector('.header__icons-menu-icon');
+const navLinks = document.querySelectorAll('.sidenav__link');
 
 menuIcon.addEventListener('click', openNav);
 
@@ -10,11 +11,17 @@ function openNav() {
     document.querySelector('.sidenav').style.left = '100%';
     menuIcon.classList.remove('isActive');
     navIsActive = false;
+    document.body.classList.remove('page__body--with-menu');
   } else {
     document.querySelector('.sidenav').style.left = '0';
     menuIcon.classList.add('isActive');
     navIsActive = true;
+    document.body.classList.add('page__body--with-menu');
   }
 }
 
 menuIcon.addEventListener('click', openNav);
+
+navLinks.forEach(link => {
+  link.addEventListener('click', openNav);
+});
