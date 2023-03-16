f30b5dd1a4b4da397bd8dee13a2df0a194ad57a7
Serj984 <serj90klishin@gmail.com>
2023-03-16 23:28:22 +0200

    fix some issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us--last-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
