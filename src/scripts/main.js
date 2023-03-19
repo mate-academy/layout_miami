535ded243a6a924b42adc01c14d6804c48ed6061
Yuriy Postrygan <petrikeich@gmail.com>
2023-03-20 00:24:23 +0300

    after amendments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+/* eslint-disable no-unused-vars */
+/* eslint-disable strict */
+function submitForm(event) {
+  event.preventDefault();
+}
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
