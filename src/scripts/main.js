65512533f71fb5486edd7bf07bdbc73d022a4d5e
Paweł Smoliński <ski1993@o2.pl>
2023-03-20 14:28:55 +0100

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const observer = new window.IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.article');
+
+hiddenElements.forEach((el) => observer.observe(el));
