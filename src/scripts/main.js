9646c310c370e6bbaedd8154409edb315c74ce0f
Absolution26 <zet2571428@gmail.com>
2023-03-21 02:10:59 +0200

    add another blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#navigation') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const buttonForm = document.getElementsByClassName('form__button');
+const inputsForm = document.getElementsByClassName('form__input');
+
+buttonForm[0].addEventListener('click', buttonNotReload);
+
+function buttonNotReload(event) {
+  event.preventDefault();
+
+  for (let i = 0; i < inputsForm.length; i++) {
+    inputsForm[i].value = '';
+  }
+}
