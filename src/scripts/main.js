ca5bdb73631aaeac004093a120ffe76010ebbda0
sylcym <prusak.syl@gmail.com>
2023-03-23 22:25:41 +0100

    Add mobile, tablet, desktop view

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const menuButton = document.querySelectorAll('.nav__btn');
+const sideNavigation = document.querySelector('.nav');
+
+menuButton.forEach(btn => {
+  btn.addEventListener('click', () => {
+    sideNavigation.classList.toggle('nav__is-open');
+  });
+});
