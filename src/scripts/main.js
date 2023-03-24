3524238f577a88c894676e8ce0e1e8827d9f994c
sylcym <prusak.syl@gmail.com>
2023-03-24 20:57:12 +0100

    Mobile refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 'use strict';
-
-const menuButton = document.querySelectorAll('.nav__btn');
-const sideNavigation = document.querySelector('.nav');
-
-menuButton.forEach(btn => {
-  btn.addEventListener('click', () => {
-    sideNavigation.classList.toggle('nav__is-open');
-  });
-});
