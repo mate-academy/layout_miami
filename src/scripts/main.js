86da20bd2ed227b56f654949beef5d4e674cab6c
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-03-25 17:54:02 +0200

    continue layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const checkbox = document.querySelector('#id-send');
+
+checkbox.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  const warn = 'Thank you for the application!<br>';
+
+  document.getElementById('output-box').innerHTML += warn;
+  event.preventDefault();
+}
