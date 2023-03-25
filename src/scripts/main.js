580a9ad840e33919bf5e9880938d78858398e2b9
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-03-25 18:13:43 +0100

    checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.getElementById('send').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('form').reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
