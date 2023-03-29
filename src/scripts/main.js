7057ce3bc1d214a40f585a6c05b2ca2bf5c94326
Misha Vynnyk <vinnikmisha94@gmail.com>
2023-03-29 20:14:29 +0200

    deleted code in  .js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const form = document.querySelector('button--send');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  form.reset();
-});
