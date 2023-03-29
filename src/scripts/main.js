1d534746fa6faac71590a4eb077976ec41fc56a4
Misha Vynnyk <vinnikmisha94@gmail.com>
2023-03-29 19:33:40 +0200

    added hover to email phone adress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('button--send');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
