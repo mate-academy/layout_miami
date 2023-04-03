2ba83b086ff1e44bb408590899d5fc7b84ffc4ca
testApex4yk <4yk.roman@gmail.com>
2023-04-03 22:35:33 +0300

    add footer and form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 
 const HEADER = document.querySelector('.js-header');
 const BODY = document.body;
+const FORM = document.querySelector('.js-form');
 const menuButton = document.querySelectorAll('.js-menu-toggle');
 
 menuButton.forEach(el => {
@@ -16,3 +17,9 @@ function toggleMenu() {
 function toggleBodyScroll() {
   BODY.classList.toggle('off-scroll');
 }
+
+FORM.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  alert('prevent Form send as part of test work');
+});
