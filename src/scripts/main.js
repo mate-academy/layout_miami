ebf6fbc8f12c0b844e70f092bec2d86e7b4f6060
testApex4yk <4yk.roman@gmail.com>
2023-04-03 17:56:23 +0300

    added header and banner

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const HEADER = document.querySelector('.js-header');
+const BODY = document.body;
+const menuButton = document.querySelectorAll('.js-menu-toggle');
+
+menuButton.forEach(el => {
+  el.addEventListener('click', toggleMenu);
+});
+
+function toggleMenu() {
+  HEADER.classList.toggle('header--open');
+  toggleBodyScroll();
+}
+
+function toggleBodyScroll() {
+  BODY.classList.toggle('off-scroll');
+}
