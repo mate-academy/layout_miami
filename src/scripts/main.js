be8d59f4c279e4f3cd379ec76f91affdc5be57d4
Andrii Torhashov <torgashov19q@gmail.com>
2023-04-03 11:17:56 +0300

    030423

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopUp(e) {
+  e.preventDefoult();
+}
+
+document.getElementsById('#form').addEventListener('submit', stopUp, false);
