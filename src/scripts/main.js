12d0aa01e376dab39aadadcaff490e3f9593e6e7
vladimirshub <vladimirscloud@icloud.com>
2023-04-04 17:35:32 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,18 @@
 'use strict';
 
+const form = document.getElementById('contact-form');
+
+function submitForm(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
-});
-
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  form.reset();
 });
