612b2bae19b99079c97ea66a7007076a8f80d300
Nazar Hotko <nazar.hotko@outlook.com>
2023-04-08 18:26:52 +0200

    changes made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const formElement = document.getElementsByClassName('contact__form')[0];
+
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formElement.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
