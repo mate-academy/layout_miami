22b9bdba6ed1d4ad5dc1986d916e88d7780c4b19
Katia Gamal <vgkateryna1@gmail.com>
2023-04-10 10:22:36 +0300

    BOSE page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener('click', submitButtonClick, false);
+
+function submitButtonClick(event) {
+  event.preventDefault();
+}
