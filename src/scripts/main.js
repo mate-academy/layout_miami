bc79b6414896cac7c8419e4d1daa917710ebb4fc
Hanna Anishyna <ann.986@gmail.com>
2023-04-13 12:36:52 -0700

    fix scrtoll issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--no-scroll');
+  } else {
+    document.body.classList.remove('page--no-scroll');
+  }
+});
