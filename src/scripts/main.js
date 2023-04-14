98ffb165459605b7fb9f80f3827324a0e1630fae
Bogdan Pohilko <bodi5@bigmir.net>
2023-04-14 17:56:38 +0300

    add logo href, rebuild bloc contacts disable button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('non_event').addEventListener(
+  'click', stopDefAction, false
+);
