00c0f7d75be1db8c69058bf8ebaf6c8fd1b3b412
Volodymyr Shostak <shostak_1@proton.me>
2023-04-17 21:23:53 +0300

    page refresh after submission is blocked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(event) {
+  event.preventDefault();
+});
