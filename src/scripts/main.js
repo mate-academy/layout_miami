0b34384949dcf0ce5aa4fc5f3a86ad196249fc89
Liza Strykharchuk <ol220681vol@gmail.com>
2023-04-18 19:35:20 +0300

    clear

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--width-menu');
+  } else {
+    document.body.classList.remove('page__body--width-menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
