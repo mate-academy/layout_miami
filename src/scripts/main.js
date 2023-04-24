81381ecbf02741af743cb0e2541ad1a12241429b
MartaChobaniuk <marta.chobaniuk@gmail.com>
2023-04-24 16:20:18 +0300

    add fourth section - contscts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('main-page__body--with-menu');
+  } else {
+    document.body.classList.remove('main-page__body--with-menu');
+  }
+});
