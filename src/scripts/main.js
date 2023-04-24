942da14ec2deec7640859c4df5915f105524bfa5
Oleksandr Rudiuk <Alvlru629@gmail.com>
2023-04-24 18:16:58 +0300

    bose_lastEdition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,9 +14,17 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('contacts__inputs--button');
+const formfieldElement = document.getElementById('formfield');
+const nameInputElement = document.getElementById('formfield-name');
+const emailInputElement = document.getElementById('formfield-email');
+const messageInputElement = document.getElementById('formfield-massage');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.reset();
+formfieldElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  nameInputElement.value = '';
+  emailInputElement.value = '';
+  messageInputElement.value = '';
+
+  window.alert('Sent successfully!');
 });
