1504979f873c83f66de19f4255a9d6434ab09fb6
Oleksandr Rudiuk <Alvlru629@gmail.com>
2023-04-24 12:25:11 +0300

    bose_24_04

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 'use strict';
 
 window.addEventListener('hashchange', () => {
+  if (window.innerWidth > 1100) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('contacts__inputs--button');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
 });
