959984c27210b90bc9c9e11bfc7c18a5a4f0c36b
Vadym Kapeliushnyi <angelus_2008@ukr.net>
2023-04-26 01:09:52 +0300

    bose landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('load', function() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body-menu');
+  } else {
+    document.body.classList.remove('body-menu');
+  }
+});
+
+window.addEventListener('hashchange', function() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body-menu');
+  } else {
+    document.body.classList.remove('body-menu');
+  }
+});
