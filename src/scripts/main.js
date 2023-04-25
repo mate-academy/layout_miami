9a396be95de3cd82649bd8cd3329eca2add5318a
Roman Didusenko <albertushoffman@proton.me>
2023-04-25 13:14:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
+const button = document.querySelector('#send');
+
+button.addEventListener('click', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+}
