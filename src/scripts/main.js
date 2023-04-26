afbe6cdecf024681ac171267b49da6d9fcfcb3a4
Denys Didenko <ddidenko441@gmail.com>
2023-04-26 18:19:21 +0300

    add animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// scroll-block
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-open');
+  } else {
+    document.body.classList.remove('page__body--menu-open');
+  }
+});
+
+// form validation
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  if (name.value === '' || email.value === '') {
+    window.alert('Please, enter your data before submitting');
+  }
+  e.preventDefault();
+});
+
+// upward
+const scroll = document.querySelector('.upward');
+
+window.addEventListener('scroll', () => {
+  scroll.classList.toggle('upward--active', window.scrollY > 500);
+});
+
+scroll.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
