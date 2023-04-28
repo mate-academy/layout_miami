f2b49b8a6664d73ef6a807f2d33a098dfb4befe0
Asya Yeromina <asyayeromina@gmail.com>
2023-04-28 16:15:21 +0200

    add image scalability on hover; prevent default reload on form submitting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--menu-open');
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+});
