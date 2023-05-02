4687abddb1e9be0153930bae9b77fd6e41ec398f
Ruslan Liapin <joy.esenin@gmail.com>
2023-05-02 13:06:22 +0300

    scroll menu fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,3 +27,11 @@ formReload.addEventListener('submit', (e) => {
   e.preventDefault();
   formReload.reset();
 });
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  if (link.getAttribute('href').startsWith('http')) {
+    link.setAttribute('target', '_blank');
+  }
+});
