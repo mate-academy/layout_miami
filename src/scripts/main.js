ee190244aaa6d84671cc5108dd249c2c687fdc31
Denys Didenko <ddidenko441@gmail.com>
2023-05-02 22:08:44 +0300

    rewrite according to suggestions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,24 +9,11 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// form validation
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const form = document.getElementById('form');
-
-form.addEventListener('submit', (e) => {
-  if (name.value === '' || email.value === '') {
-    window.alert('Please, enter your data before submitting');
-  }
-  e.preventDefault();
-});
-
 // upward
 const scroll = document.querySelector('.upward');
 
 window.addEventListener('scroll', () => {
   scroll.classList.toggle('upward--active', window.scrollY > 500);
-  scroll.classList.toggle('upward--dark', window.scrollY > 4190);
 });
 
 scroll.addEventListener('click', () => {
