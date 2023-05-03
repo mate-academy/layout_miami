409a778c8dd58f623433abf8fb85c085787924dd
Oleksii Lysenko <nano77@ukr.net>
2023-05-03 15:44:03 +0300

    improve responsivity on header, add menu block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.nav__button--menu');
+const menu = document.querySelector('.menu');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    menu.classList.toggle('menu--opened');
+  });
+});
