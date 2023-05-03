291072d901c2e731e8a0cc6a93ff06b74e02a9db
Ivan Khoroshylov <host513@gmail.com>
2023-05-03 21:21:19 +0300

    add animated navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const openButton = document.querySelector('#navOpenButton');
+const closeButton = document.querySelector('#navCloseButton');
+const nav = document.querySelector('#nav');
+const navList = document.querySelector('#navList');
+const body = document.querySelector('body');
+
+const openNavClass = 'page__menu--open';
+const turnOffScrollClass = 'page__body--scroll-off';
+
+function openMenu() {
+  nav.classList.add(openNavClass);
+  body.classList.add(turnOffScrollClass);
+}
+
+function closeMenu() {
+  nav.classList.remove(openNavClass);
+  body.classList.remove(turnOffScrollClass);
+}
+
+openButton.addEventListener('click', openMenu);
+
+closeButton.addEventListener('click', closeMenu);
+
+navList.addEventListener('click', function(event) {
+  if (event.target.tagName === 'A') {
+    closeMenu();
+  }
+});
+
+// openButton.addEventListener('click', function() {
+//   console.log('+');
+// });
