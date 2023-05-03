719f00b4cefe1ee143f6bf765932c16a9d99d91a
Vasiliy22 <hitretsukrnet@gmail.com>
2023-05-03 20:25:30 +0300

    fix comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,20 +8,7 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const myForm = document.querySelector('.my-form');
-
-const sendButton = document.getElementById('clicks');
-
-sendButton.addEventListener('click', function(event) {
-  const userEmail = document.getElementById('email').value;
-  const userName = document.getElementById('name').value;
-
-  if (
-    userEmail && userName
-    && /^[\w-]+@([\w-]+)+[\w-]{2,4}$/g.test(userEmail)) {
-    event.preventDefault();
-    myForm.reset();
-  }
+window.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
 });
-
-myForm.addEventListener('clicks', sendButton);
