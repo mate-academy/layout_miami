c8a92ae1619332ee312eab9b031c81ff0128dacb
Dolnys <thedolnys@gmail.com>
2023-05-03 18:18:37 +0200

    add tablet media queries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const btnNavEl = document.querySelector('.navigation__icons--burger');
+const menuEl = document.querySelector('.menu');
+const menuClose = document.querySelector('.menu__navigation--close-button');
+
+btnNavEl.addEventListener('click', function() {
+  menuEl.classList.add('menu--active');
+});
+
+menuClose.addEventListener('click', function() {
+  menuEl.classList.remove('menu--active');
+});
