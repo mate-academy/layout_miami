277a857c50c94f8e7de35ddece793769f72dfc72
Annbpiu <Annbpiu@gmail.com>
2023-05-03 15:57:04 +0300

    bug fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitButtonClick,);
+
+function submitButtonClick(event) {
+  event.preventDefault();
+  form.reset();
+}
