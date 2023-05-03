041c6538940977ce14bdab45fb368fa85c2a52af
andrusb21 <andrusyshyn1996@gmail.com>
2023-05-03 12:06:55 +0300

    added menu and second block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const openButton = document.querySelector('.header__menu-opener');
+const closeButton = document.querySelector('.icon--cross');
+const nav = document.querySelector('#menu');
+const navList = document.querySelector('.menu__list');
+const body = document.querySelector('body');
+
+const openNavClass = 'page__menu--open';
+const turnOffScrollClass = 'body--scroll-off';
+
+function openMenu() {
+  nav.classList.add(openNavClass);
+  body.classList.add(turnOffScrollClass);
+}
+
+function closeMenu() {
+  nav.classList.remove(openNavClass);
+  body.classList.remove(turnOffScrollClass);
+}
+
+openButton.addEventListener('click', openMenu);
+
+closeButton.addEventListener('click', closeMenu);
+
+navList.addEventListener('click', function(event) {
+  if (event.target.tagName === 'A') {
+    closeMenu();
+  }
+});
