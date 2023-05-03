bd24e1a19146f604eea71a843851f4c7b773afd9
Anna Holovko <anutkagolovko1472@gmail.com>
2023-05-04 02:33:48 +0300

    added footer and buy-block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const openButton = document.querySelector('#menuOpenButton');
+const closeButton = document.querySelector('#menuCloseButton');
+const menu = document.querySelector('#menu');
+const menuList = document.querySelector('.menu__list');
+const body = document.querySelector('.page__body');
+
+const openMenuClass = 'page__menu--open';
+const turnOffScrollClass = 'page__body--scroll-off';
+
+function openMenu() {
+  menu.classList.add(openMenuClass);
+  body.classList.add(turnOffScrollClass);
+}
+
+function closeMenu() {
+  menu.classList.remove(openMenuClass);
+  body.classList.remove(turnOffScrollClass);
+}
+
+openButton.addEventListener('click', openMenu);
+
+closeButton.addEventListener('click', closeMenu);
+
+menuList.addEventListener('click', function(event) {
+  if (event.target.tagName === 'A') {
+    closeMenu();
+  }
+});
