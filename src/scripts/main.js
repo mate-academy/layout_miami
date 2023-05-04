34060ab913009790789b4ab5e5164ea8e90924e0
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-04 13:56:06 +0300

    Add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const openButton = document.querySelector('#navOpenButton');
+const closeButton = document.querySelector('#navCloseButton');
+const menu = document.querySelector('#menu');
+const navList = document.querySelector('#navList');
+
+const openMenuClass = 'page__menu--open';
+
+function openMenu() {
+  menu.classList.add(openMenuClass);
+}
+
+function closeMenu() {
+  menu.classList.remove(openMenuClass);
+}
+
+openButton.addEventListener('click', openMenu);
+
+closeButton.addEventListener('click', closeMenu);
+
+navList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'A') {
+    closeMenu();
+  }
+});
