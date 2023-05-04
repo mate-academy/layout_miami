a6edc50a8d25181d5773c97fbd084bd0e7ffc715
Viktor Panchenko <panch.v1811@gmail.com>
2023-05-04 17:03:11 +0200

    added page structure and basic styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.documentElement.classList.add('page--menu-open');
+  } else {
+    document.documentElement.classList.remove('page--menu-open');
+  }
+});
