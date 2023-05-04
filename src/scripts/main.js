be0e9027543e1466d24c13d2972e900f07cbb572
Vadym Vitruk <vadym_v@ukr.net>
2023-05-04 18:33:27 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const openBtn = document.querySelector('.icon--menu');
+const closeBtn = document.querySelector('.icon--cross');
+const menuList = document.querySelector('.menu__list');
+const menu = document.querySelector('.page__menu');
+const body = document.querySelector('body');
+
+const openSlide = 'page__menu--open';
+const turnOffScroll = 'page__body--scroll-off';
+
+const addClasses = () => {
+  menu.classList.add(openSlide);
+  body.classList.add(turnOffScroll);
+};
+
+const removeClasses = () => {
+  menu.classList.remove(openSlide);
+  body.classList.remove(turnOffScroll);
+};
+
+openBtn.addEventListener('click', addClasses);
+closeBtn.addEventListener('click', removeClasses);
+
+menuList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    removeClasses();
+  }
+});
