794a572beeed01a9ce5e983e9f3b9cb96daddf0c
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-05-04 23:13:49 +0300

    finished the MyBiKE landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menuButton = document.querySelector('.icon--menu');
+const crossButton = document.querySelector('.icon--cross');
+const body = document.querySelector('.page__body');
+const menuList = document.querySelector('.menu__list');
+
+menuButton.addEventListener('click', function() {
+  body.classList.add('scroll-off');
+});
+
+crossButton.addEventListener('click', function() {
+  body.classList.remove('scroll-off');
+});
+
+menuList.addEventListener('click', function(e) {
+  if (e.target.tagName === 'A') {
+    body.classList.remove('scroll-off');
+  }
+});
