e9e330bb6f1b8a36cec02500436141ef713ef46a
Ivan Marchuk <imarchuk90@gmail.com>
2023-05-04 19:28:33 +0300

    All website`s blocks with responsive. Open/close menu logic. better utils

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const openBtn = document.querySelector('.nav-opener');
+const closeBtn = document.querySelector('.nav-closer');
+const menu = document.querySelector('.nav');
+
+openBtn.addEventListener('click', () => {
+  menu.classList.remove('nav--closed');
+  menu.classList.add('nav--opened');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('nav--opened');
+  menu.classList.add('nav--closed');
+});
