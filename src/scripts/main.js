4189f84c51d573f17619320f2b2c1f6fdd3b57d3
Oleksii Lysenko <nano77@ukr.net>
2023-05-04 15:38:54 +0300

    remove scroll on menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,13 @@ const openMenuButton = document.querySelector(
 const closeMenuButton = document.querySelector(
   '.top-controls__button--menu-close'
 );
+const body = document.querySelector('.body');
 const menuLinks = document.querySelectorAll('.menu__nav-list-link');
 const menu = document.querySelector('.menu');
 
 [openMenuButton, closeMenuButton, ...menuLinks].forEach((button) => {
   button.addEventListener('click', () => {
     menu.classList.toggle('menu--opened');
+    body.classList.toggle('body--menu-open');
   });
 });
