4c5fbd10a50077e54e644a9ab92a0707e7efad7d
Mykola Maik <mykoluamaik@gmail.com>
2023-05-04 16:59:52 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,5 @@
 'use strict';
+
+// const openButton = document.querySelector('#menuOpenButton');
+// const closeButton = document.querySelector('#menuCloseButton');
+// const menu = document.querySelector('#menu');
