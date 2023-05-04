c871af7ab5c09411fb5985efae5f30bdca9b4ee1
Oleksii Lysenko <nano77@ukr.net>
2023-05-04 15:19:12 +0300

    finish header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const buttons = document.querySelectorAll('.nav__button--menu');
+const openMenuButton = document.querySelector(
+  '.top-controls__button--menu-open'
+);
+const closeMenuButton = document.querySelector(
+  '.top-controls__button--menu-close'
+);
+const menuLinks = document.querySelectorAll('.menu__nav-list-link');
 const menu = document.querySelector('.menu');
 
-buttons.forEach((button) => {
+[openMenuButton, closeMenuButton, ...menuLinks].forEach((button) => {
   button.addEventListener('click', () => {
     menu.classList.toggle('menu--opened');
   });
