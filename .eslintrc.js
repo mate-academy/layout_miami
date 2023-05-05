d5e3e0bb73ae9e111bd890c0af3c2d08000e0bee
Bohdan <6alfabravo9@gmail.com>
2023-05-05 14:32:30 +0300

    add aos animatios and refactor file structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
 };
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+  },
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1500,
+});
