1379f15ec87b3de7beab56cd3cb7a3c46d7b2941
holytimy4 <ez.lvl.faceit@gmail.com>
2023-05-06 02:17:16 +0300

    added landing solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-'use strict';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const form = document.querySelector('.form-contact__content');
+const emailInput = document.querySelector('.form-contact__email');
+
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (emailInput.checkValidity()) {
+    form.reset();
+    window.alert('Thanks for your message!');
+  }
+});
+
+AOS.init({
+  duration: 1200,
+});
