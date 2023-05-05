dfcdc24b73c23c05a008ef38680e078956531b34
Ivan Marchuk <imarchuk90@gmail.com>
2023-05-05 21:42:17 +0300

    myBike v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,26 @@
 'use strict';
 
-const openBtn = document.querySelector('.nav-opener');
-const closeBtn = document.querySelector('.nav-closer');
-const menu = document.querySelector('.nav');
+const openBtn = document.querySelector('.menu-opener');
+const closeBtn = document.querySelector('.menu-closer');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.menu__list');
 
-openBtn.addEventListener('click', () => {
-  menu.classList.remove('nav--closed');
-  menu.classList.add('nav--opened');
+openBtn.addEventListener('click', (e) => {
+  menu.classList.remove('menu--closed');
+  menu.classList.add('menu--opened');
+  document.body.classList.add('body--overflow-hidden');
 });
 
 closeBtn.addEventListener('click', () => {
-  menu.classList.remove('nav--opened');
-  menu.classList.add('nav--closed');
+  menu.classList.remove('menu--opened');
+  menu.classList.add('menu--closed');
+  document.body.classList.remove('body--overflow-hidden');
+});
+
+nav.addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'menu__link') {
+    menu.classList.remove('menu--opened');
+    menu.classList.add('menu--closed');
+    document.body.classList.remove('body--overflow-hidden');
+  }
 });
