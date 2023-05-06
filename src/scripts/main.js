a33f8b37b831ed9a3fb7af7f976805444e988a2f
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-06 17:27:51 +0300

    Add some changes in code style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,19 @@ const openButton = document.querySelector('#navOpenButton');
 const closeButton = document.querySelector('#navCloseButton');
 const menu = document.querySelector('#menu');
 const navList = document.querySelector('#navList');
+const page = document.querySelector('body');
 
 const openMenuClass = 'page__menu--open';
+const turnOffScrollClass = 'page--scroll-off';
 
 function openMenu() {
   menu.classList.add(openMenuClass);
+  page.classList.add(turnOffScrollClass);
 }
 
 function closeMenu() {
   menu.classList.remove(openMenuClass);
+  page.classList.remove(turnOffScrollClass);
 }
 
 openButton.addEventListener('click', openMenu);
