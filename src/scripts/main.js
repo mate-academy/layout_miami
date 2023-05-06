9a69d5c151b53e49e59919dd8246348105542cb9
Viktor P <vpanya97@gmail.com>
2023-05-07 01:32:54 +0300

    add style fixes, animations, scroll, linking,  Complete all checklist.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.documentElement.classList.add('page--menu-open');
+  } else {
+    document.documentElement.classList.remove('page--menu-open');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+});
