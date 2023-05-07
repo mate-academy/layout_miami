c8665c31b928de3dc560fb11ec9c7328b307993a
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-07 19:16:48 +0300

    add page animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,9 @@
-'use strict';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 800,
+});
 
 const menuButton = document.querySelector('#burger-menu');
 const menuOpen = document.querySelector('#menu-open');
