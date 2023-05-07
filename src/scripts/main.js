d85a05f455185b78415cd7734e8abe57c48f1ba6
Mykola Maik <mykoluamaik@gmail.com>
2023-05-07 13:03:35 +0300

    add additional hover effects and fix code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,11 @@
 // const openButton = document.querySelector('#menuOpenButton');
 // const closeButton = document.querySelector('#menuCloseButton');
 // const menu = document.querySelector('#menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
