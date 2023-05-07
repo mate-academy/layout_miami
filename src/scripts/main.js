24f4b1b2c393cde6ed3f0fae52da948ed51d6ed5
Ivan Khoroshylov <host513@gmail.com>
2023-05-08 00:27:12 +0300

    add hover animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ const closeButton = document.querySelector('#navCloseButton');
 const nav = document.querySelector('#nav');
 const navList = document.querySelector('#navList');
 const body = document.querySelector('body');
+const form = document.querySelector('#submitForm');
 
 const openNavClass = 'page__menu--open';
 const turnOffScrollClass = 'page__body--scroll-off';
@@ -19,6 +20,12 @@ function closeMenu() {
   body.classList.remove(turnOffScrollClass);
 }
 
+function preventSubmit(event) {
+  event.preventDefault();
+}
+
+form.addEventListener('submit', preventSubmit);
+
 openButton.addEventListener('click', openMenu);
 
 closeButton.addEventListener('click', closeMenu);
@@ -28,4 +35,3 @@ navList.addEventListener('click', function(event) {
     closeMenu();
   }
 });
-
