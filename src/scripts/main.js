6e5edc7e85ddf7979b42ac0a42209b1c1eef1c61
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-07 13:08:19 +0300

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
+
+// lock body on menu open
+const iconBurger = document.querySelector(".navbar__icon--type-burger");
+const iconClose = document.querySelector(".navbar__icon--type-close");
+
+iconBurger.addEventListener("click", () =>
+  document.body.classList.add("page__body--lock")
+);
+
+iconClose.addEventListener("click", () =>
+  document.body.classList.remove("page__body--lock")
+);
+
+// form submit
+const form = document.querySelector(".contact__form");
+
+form.addEventListener("submit", (event) => event.preventDefault());
