01c176842149665bccd44f77cafdc6bac1ea0ed5
Stepan Haida <stepanhaida@gmail.com>
2023-05-08 11:07:17 +0300

    Fix link behaviour, form sending, and link hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const formButton = document.querySelector('#form-button');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +9,5 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+formButton.addEventListener('click', (event) => event.preventDefault());
