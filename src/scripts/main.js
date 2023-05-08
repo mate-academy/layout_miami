f495889bc3d93fc69fe28f34fcfe29179dd1c509
Bohdan <6alfabravo9@gmail.com>
2023-05-08 21:34:37 +0300

    fix form, scroll and hover issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,15 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+
+  document.location = '#';
+});
+
 AOS.init({
   duration: 1500,
 });
