bde612440f4990c6d099e1050bd946d461a19b81
Ivan Khoroshylov <host513@gmail.com>
2023-05-08 22:53:59 +0300

    fix links and tab selection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,8 @@ const closeButton = document.querySelector('#navCloseButton');
 const nav = document.querySelector('#nav');
 const navList = document.querySelector('#navList');
 const body = document.querySelector('body');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 const form = document.querySelector('#submitForm');
 
 const openNavClass = 'page__menu--open';
@@ -13,15 +15,20 @@ const turnOffScrollClass = 'page__body--scroll-off';
 function openMenu() {
   nav.classList.add(openNavClass);
   body.classList.add(turnOffScrollClass);
+  main.hidden = true;
+  footer.hidden = true;
 }
 
 function closeMenu() {
   nav.classList.remove(openNavClass);
   body.classList.remove(turnOffScrollClass);
+  main.hidden = false;
+  footer.hidden = false;
 }
 
 function preventSubmit(event) {
   event.preventDefault();
+  document.getElementById('submitForm').reset();
 }
 
 form.addEventListener('submit', preventSubmit);
