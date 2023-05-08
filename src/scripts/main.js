d4d9d5c72bc91a7d67a7516784a9077e23dbe32a
Mykola Maik <mykoluamaik@gmail.com>
2023-05-08 18:21:31 +0300

    fix issues and delete AOS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// const openButton = document.querySelector('#menuOpenButton');
-// const closeButton = document.querySelector('#menuCloseButton');
-// const menu = document.querySelector('#menu');
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -11,9 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-// window.addEventListener('scroll', () => {
-//   const scrolled = window.scrollY;
-
-//   console.log(scrolled);
-// });
