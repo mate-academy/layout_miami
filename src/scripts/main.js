2908afd5df24b583e726421601e614f91a778c8a
anastasiia <anastasiia.mykhayliv@gmail.com>
2023-05-08 14:28:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const menuBtn = document.querySelector('.icon--menu');
+const menu = document.querySelector('#menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+});
