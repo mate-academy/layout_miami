b6d35971cf9e0b116c5a116a63d7c33db9e51084
Wowa Snuber <snuber.w@gmail.com>
2023-05-08 17:52:06 +0300

    fixed after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const contactsForm = document.querySelector('#contacts-form');
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -7,3 +9,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+contactsForm.addEventListener('submit', submitReload, false);
+
+function submitReload(event) {
+  event.preventDefault();
+  contactsForm.reset();
+}
