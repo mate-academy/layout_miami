8631892e92dbd783175bf62d432f90475d37983b
Bohdan <6alfabravo9@gmail.com>
2023-05-09 17:47:04 +0300

    fix aos animation and font issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,14 @@ form.addEventListener('submit', function(event) {
   document.location = '#';
 });
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
 AOS.init({
   duration: 1500,
 });
