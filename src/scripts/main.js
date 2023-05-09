88fae0d60da487e796069803c5e9ccc53bdc0a06
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-09 12:17:25 +0300

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,28 @@
+/* eslint-disable space-before-function-paren */
 /* eslint-disable quotes */
 "use strict";
 
 // lock body on menu open
+const menu = document.querySelector(".menu");
 const iconBurger = document.querySelector(".navbar__icon--type-burger");
 const iconClose = document.querySelector(".navbar__icon--type-close");
 
-iconBurger.addEventListener("click", () =>
-  document.body.classList.add("page__body--lock")
-);
+iconBurger.addEventListener("click", (event) => {
+  event.preventDefault();
+  document.body.classList.add("page__body--lock");
+  menu.classList.add("menu--open");
+});
 
-iconClose.addEventListener("click", () =>
-  document.body.classList.remove("page__body--lock")
-);
+iconClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  document.body.classList.remove("page__body--lock");
+  menu.classList.remove("menu--open");
+});
 
 // form submit
 const form = document.querySelector(".contact__form");
 
-form.addEventListener("submit", (event) => event.preventDefault());
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  this.reset();
+});
