bf8f57d00e4e37fd65109b610cb5b5d8ef36785c
Yuliya1907 <violet1907@ukr.net>
2023-05-09 15:06:10 +0300

    fixed BOSE

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    document.querySelector('.menu').style.zIndex = '1';
   } else {
     document.body.classList.remove('page__body--with-menu');
+    document.querySelector('.menu').style.zIndex = '0';
   }
 });
 
