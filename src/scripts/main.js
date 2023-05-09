357e4874da76980b0b116d982420352072e2dcf7
Ulia Losytska <ulositska@gmail.com>
2023-05-09 23:37:01 +0300

    Clean up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-/*const menuOpen = document.querySelectorAll('[class^="menu"]');*/
+/* const menuOpen = document.querySelectorAll('[class^="menu"]'); */
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
-    /*menuOpen.attr('tabindex', -1);*/
+    /* menuOpen.attr('tabindex', -1); */
   }
 });
 
