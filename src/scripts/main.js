096fba36029f095fb341c33234be5bf18a7f31b6
Ulia Losytska <ulositska@gmail.com>
2023-05-09 23:34:11 +0300

    Fix urls in menu and contacts. Change form method. Disable the submit of empty form. Improve tooltip

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 'use strict';
 
+/*const menuOpen = document.querySelectorAll('[class^="menu"]');*/
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
+    /*menuOpen.attr('tabindex', -1);*/
   }
 });
+
+const form = document.getElementById('form');
+
+function submitForm(event) {
+  event.preventDefault();
+  window.scrollTo(0, 0);
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
