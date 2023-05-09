ab5fc24f506eca298f99b82139c3783ea79cf275
Maksim Kadygrob <kadygrobmaksim33@gmail.com>
2023-05-09 18:05:16 +0300

    links and tab fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,10 +8,39 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+const pageLinks = document.querySelectorAll('.page--link');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    pageLinks.forEach(link => {
+      link.setAttribute('tabindex', '-1');
+    });
+  } else {
+    pageLinks.forEach(link => {
+      link.removeAttribute('tabindex');
+    });
+  }
+});
+
+const menuLinks = document.querySelectorAll('.menu--link');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash !== '#menu') {
+    menuLinks.forEach(link => {
+      link.setAttribute('tabindex', '-1');
+    });
+  } else {
+    menuLinks.forEach(link => {
+      link.removeAttribute('tabindex');
+    });
+  }
+});
+
 const form = document.querySelector('#formId');
 
 function submitForm(event) {
   event.preventDefault();
+  event.target.reset();
 }
 
 form.addEventListener('submit', submitForm);
