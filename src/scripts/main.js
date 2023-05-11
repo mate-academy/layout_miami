e7229059fa3fe8674e347398e6fbed69de778c62
Dima Astapenko <bonum_taurus@ukr.net>
2023-05-11 12:44:59 +0300

    add last fixing problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const button = document.querySelector('#submit');
-
-button.addEventListener('click', function(event) {
-  event.preventDefault();
-});
