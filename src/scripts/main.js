cfbcf42b296d50cc2ea9343b42ddac95e545b3eb
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-11 15:49:16 +0300

    added more animations/transforms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('body--with-menu');
   }
 });
+
+const form = document.querySelector('.contacts--form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
