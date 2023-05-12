5e58c0da30ad8e7231fe435f01b4274d31d1d050
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-12 18:43:25 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const forms = document.querySelector('form');
+
+forms.addEventListener('submit', function(event) {
+  event.preventDefault();
+  forms.reset();
+});
