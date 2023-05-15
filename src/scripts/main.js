10017c521031230ae04a4aaa47dfe99378b106cd
Mager <magerazp@gmail.com>
2023-05-15 12:32:47 +0300

    Landing complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.getElementById('form');
+
+function submitForm(event) {
+  event.preventDefault();
+  window.scrollTo(0, 0);
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
