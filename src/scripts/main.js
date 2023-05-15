e1847e8bb7df12c2d123ffdf8e6d9d15657b5d2e
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-05-15 16:20:25 +0300

    Fixed: header background pos for mobile, details text postion for desktop, form (now without reload after submission).

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const checkbox = document.querySelector('#submit-button');
+
+checkbox.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  event.preventDefault();
+}
