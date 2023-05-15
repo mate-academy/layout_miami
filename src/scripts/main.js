c9394bf9acb35eaf17f7e5f9cb42e7b87652ab53
Sergio47ua <serhiivoitykappleid@gmail.com>
2023-05-15 20:07:50 +0200

    bose

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  document.getElementById('form').reset();
+});
