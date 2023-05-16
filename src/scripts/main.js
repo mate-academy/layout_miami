e1637ed99e3956f854ba06701e8fb67cbfb75a95
Melnychuk Vova <kivertsivova@gmail.com>
2023-05-16 11:32:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__content--with-menu');
   }
 });
+
+const form = document.querySelector('#contactForm');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
