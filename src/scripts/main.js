bfcee248e26e3bb2c9b2e967bbeb58b9bedce559
Antonio Zuccon Ghiotto <antonio.zucconghiotto@gmail.com>
2023-05-18 16:57:44 +0200

    Solution__new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const checkbox = document.querySelector('#form-button');
+
+checkbox.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  event.preventDefault();
+
+  document.getElementById('contacts__form').reset();
+}
