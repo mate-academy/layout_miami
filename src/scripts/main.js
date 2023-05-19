1488a6358aa6d71571b91cc2e995f52e61f0940a
Kristina Bekher <bekherkr@gmail.com>
2023-05-19 23:40:59 +0200

    add contact section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('#menu-icon').addEventListener('click', () => {
+  document.body.classList.add('page__body--with-menu');
+});
+
+document.querySelector('#cross-icon').addEventListener('click', () => {
+  document.body.classList.remove('page__body--with-menu');
+});
