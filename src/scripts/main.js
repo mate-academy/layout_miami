50622c7b3dfd31df496f6d836b7a11beed4ec7bf
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-05-19 11:08:17 +0200

    find mistake in main.js add some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,9 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--no-scroll');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--no-scroll');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
