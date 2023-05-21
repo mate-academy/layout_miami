728a279aad949b83784c8b5552eacf94bab69f7c
Sonya Liubchenko <conilub@gmail.com>
2023-05-21 21:28:10 +0300

    fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,23 @@ const contactForm = document.getElementById('contactForm');
 contactForm.addEventListener('submit', function(event) {
   event.preventDefault();
 });
+
+function submitForm() {
+  const form = document.getElementById('contactForm');
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const messageInput = document.getElementById('message');
+
+  if (nameInput.value !== '' && emailInput.value !== ''
+  && messageInput.value !== '') {
+    form.submit();
+
+    form.reset();
+  } else {
+    window.alert('Please fill in all fields.');
+  }
+}
+
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', submitForm);
