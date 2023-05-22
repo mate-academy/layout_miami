fbe43c6d833121e7a7fe7d9367b0a3e0d39369ad
Jenya Golovko <u9on1210@gmail.com>
2023-05-22 20:15:19 +0300

    landing has been done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+window.addEventListener('hashchange', () =>  {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--width-menu');
+  }
+  else {
+    document.body.classList.remove('page__body--width-menu');
+  }
+});
