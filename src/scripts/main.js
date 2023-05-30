97d46c1178c2c0bec14e7efbf486272e419e2ff0
catgode <105614630+catgode@users.noreply.github.com>
2023-05-30 16:13:30 +0200

    add email validation to contact form and prevent the 'submit' btn from refreshing the site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const formSubmit = document.getElementById('contact_form');
+
+formSubmit.addEventListener('submit', function(event) {
+  event.preventDefault();
+  formSubmit.reset();
+});
