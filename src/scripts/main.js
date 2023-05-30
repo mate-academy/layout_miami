bce089ac1bcd7e476c50851822219fc6ae2fa9cd
catgode <105614630+catgode@users.noreply.github.com>
2023-05-31 00:07:41 +0200

    finalize

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ formSubmit.addEventListener('submit', function(event) {
   event.preventDefault();
   formSubmit.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
