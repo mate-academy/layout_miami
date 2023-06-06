464d66df368910b8b0b639ea3f84b7c7d06c08be
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-06-06 16:28:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,10 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contacts__inputs');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
