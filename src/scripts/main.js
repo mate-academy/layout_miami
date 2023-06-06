cefbb2480e5a47754f88587297c20fa1b2065d22
marshalek37 <patrykmarp@gmai.com>
2023-06-06 11:57:39 +0200

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,36 @@
 'use strict';
 
-const form = document.querySelector('.form');
+const form = document.getElementById('form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     return emailRegex.test(email);
   }
 
   function isValidName(name) {
     const nameRegex = /^[a-zA-Z\s]*$/;
-
     return nameRegex.test(name);
   }
 
   if (!isValidName(nameInput.value.trim())) {
     alert('Please enter a valid name');
-
     return;
   }
 
   if (!isValidEmail(emailInput.value.trim())) {
     alert('Please enter a valid email');
-
     return;
   }
 
-  form.submit();
+  form.reset();
+
+  const currentSection = document.getElementById('contacts');
+  currentSection.scrollIntoView({ behavior: 'smooth' });
 });
 
 const lenis = new Lenis();
