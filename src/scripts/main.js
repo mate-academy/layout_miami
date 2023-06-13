fc1630974939ad89737bc7533d33d0743015f265
Irina Kulish <irinakulish20@gmail.com>
2023-06-13 14:23:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
-const form = document.getElementById('form');
+const form = document.getElementById('contact-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+function submitForm(event) {
+  event.preventDefault();
   form.reset();
+}
+
+form.addEventListener('submit', submitForm);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
