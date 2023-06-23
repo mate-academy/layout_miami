c4f87c37a50907abfd3f2d856de1d33ad14cb978
kyrylomanko <kyrylo.manko@gmail.com>
2023-06-23 17:44:55 +0200

    Form fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function submitForm(event) {
+  event.preventDefault();
+  document.querySelector('#form').reset();
+}
+
+document.querySelector('#form').addEventListener('submit', submitForm);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
