b026d00f09e0c37111406a176af09666b272bec7
Kacper <leman_kacper@o2.pl>
2023-06-30 14:03:06 +0200

    add script for unscrollable menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const menu = document.getElementById('menu');
+
+menu.addEventListener('wheel', function(event) {
+  event.preventDefault();
+}, { passive: false });
