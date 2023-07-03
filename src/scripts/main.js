26fc0da44100f4989e7454f7ea97c34f5ca2534b
Anastasiia Semenova <semenovaanastasiiaa@gmail.com>
2023-07-03 17:50:40 +0300

    fixed comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ window.addEventListener('hashchange', ()=> {
   }
 })
 
-var form=document.getElementById("formId");
+var form=document.getElementById("#form");
 function submitForm(event){
   event.preventDefault();
 }
