d08c4affa31f11a3cdf14b0633650af49e86aae6
Artem Velychko <avelychko@icloud.com>
2023-07-07 15:04:24 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
+
+const form = document.querySelector('.contacts__form');
+
+function submit(event) {
+  event.preventDefault();
+
+  form.reset();
+}
+
+form.addEventListener('submit', submit);
