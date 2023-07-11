7593528c3b96f0c434fafb38ff87f84239152518
Andrzej Pietkiewicz <pietan@me.com>
2023-07-11 16:21:25 +0200

    Menu scroll added

--- src/myscripts.js
+++ src/myscripts.js
@@ -1,3 +1,11 @@
 function onFormSubmit() {
   event.preventDefault();
 }
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
