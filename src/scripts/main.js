aee97ec51da13ad2a5454189d06356f02c7ecb03
piotr <piotr_as@yahoo.com>
2023-07-12 19:52:05 +0200

    first part (hader + hero section + cards), no navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', e => {
+  const element = document.querySelector('#header');
+
+  const className = 'header-transform';
+
+  const scrollPosition = 200; // Adjust this value as needed
+
+  const toggleClassOnScroll = () => {
+    if (window.scrollY >= scrollPosition) {
+      element.classList.add(className);
+    } else {
+      element.classList.remove(className);
+    }
+  };
+
+  window.addEventListener('scroll', toggleClassOnScroll);
+});
