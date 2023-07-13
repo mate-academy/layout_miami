c8dd1f31fb4dc51d0d298a4ea847f6dfed8bd381
Andrzej Pietkiewicz <pietan@me.com>
2023-07-13 21:57:40 +0200

    added 'toTop' button and few fixes

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -5,22 +5,24 @@ const submit = document.getElementById('myForm');
 
 submit.addEventListener('submit', (e) => e.preventDefault());
 
-// const mybutton = document.getElementById("myBtn");
-
-// window.onscroll = function() {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 100
-//     || document.documentElement.scrollTop > 100) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// function topFunction() {
-//   document.body.scrollTop = 0;
+//  CODE BELOW WORKS ONLY WHEN ISERTED DIRECTLY IN HTML
+//
+// function scrollToTop() {
 //   document.documentElement.scrollTop = 0;
+//   document.body.scrollTop = 0;
 // }
+
+const mybutton = document.getElementById('myBtn');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100
+    || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
