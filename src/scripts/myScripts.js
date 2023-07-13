6f5f0b270d7915a41bf20c86b7005195d52d5558
Andrzej Pietkiewicz <pietan@me.com>
2023-07-13 18:41:49 +0200

    smartphone, tablet & desktop finished

---
+++ src/scripts/myScripts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// submit off
+// Gets a reference to the form element
+// document.getElementById("submitButton").addEventListener("submit", (e) => e.preventDefault());
+
+// const mybutton = document.getElementById("myBtn");
+
+// window.onscroll = function() {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100
+//     || document.documentElement.scrollTop > 100) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
