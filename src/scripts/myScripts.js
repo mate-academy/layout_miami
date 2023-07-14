65a8550cf211b0c2b98a7dc33bfec44b386bdfee
Andrzej Pietkiewicz <pietan@me.com>
2023-07-14 11:49:02 +0200

    scroll-lock added

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -26,3 +26,11 @@ function scrollFunction() {
     mybutton.style.display = 'none';
   }
 }
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
