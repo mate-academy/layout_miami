4023252d9280d31e5ef7a21e909991e473513a0b
piotr <piotr_as@yahoo.com>
2023-07-14 10:19:41 +0200

    products section - mobile beginning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,7 +13,49 @@ document.addEventListener('DOMContentLoaded', e => {
     } else {
       element.classList.remove(className);
     }
+
+    if (menu.classList.contains('--menu-toggle')) {
+      element.classList.remove(className);
+    }
   };
 
   window.addEventListener('scroll', toggleClassOnScroll);
+
+  // TOGGLING NAVIGATION
+
+  const hamburgerBtn = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburgerBtn.addEventListener('click', event => {
+    menu.classList.toggle('--menu-toggle');
+    hamburgerBtn.classList.toggle('--hamburger-transform');
+  });
+
+  // SCROLL TO TOP ANIMATION
+
+  const toTopButton = document.getElementById('toTop');
+
+  function scrollToTop() {
+    if (window.scrollY !== 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, window.scrollY - window.scrollY / 8);
+    }
+  }
+
+  toTopButton.addEventListener('click', function(ev) {
+    ev.preventDefault();
+    scrollToTop();
+  });
+
+  // Toggle class based on scroll positio
+
+  function toggleToTopButton() {
+    if (window.scrollY > 200) {
+      toTopButton.classList.add('--toTop-visible');
+    } else {
+      toTopButton.classList.remove('--toTop-visible');
+    }
+  }
+
+  window.addEventListener('scroll', toggleToTopButton);
 });
