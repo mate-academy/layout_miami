2a222c108d294384ec9cfcca9f6803e8ed979357
Patryk Bąboń <patryk.babon@gmail.com>
2023-07-16 19:17:27 +0200

    fix for mobile viewport 100vh & added delay to redirect from menu link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
