1e3f1fddc84a8065dda8f51d7c0daffd14df2d86
Patryk Bąboń <patryk.babon@gmail.com>
2023-07-16 20:44:14 +0200

    added contact form preventDefault() & info for message success send

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,26 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const contactForm = document.getElementById('contact-us-form');
+
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  contactForm.reset();
+
+  const submitButton = document.getElementById('contact-us-submit');
+
+  submitButton.blur();
+
+  const orgContent = submitButton.textContent;
+
+  submitButton.classList.add('send');
+  submitButton.setAttribute('disabled', '');
+  submitButton.textContent = 'Your message has been sent!';
+
+  setTimeout(function() {
+    submitButton.classList.remove('send');
+    submitButton.removeAttribute('disabled');
+    submitButton.textContent = orgContent;
+  }, 3000);
+});
