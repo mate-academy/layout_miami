d5af2d5e57958215add13556fa05da700974626d
Patryk Filipiak <patryk.fy@o2.pl>
2023-07-17 07:52:11 +0200

    JS: block scrolling on menu, page dont reload on submit form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
+
+const formSubmit = document.querySelector('#contact-form__button');
+
+formSubmit.addEventListener('click', e => e.preventDefault(), false);
