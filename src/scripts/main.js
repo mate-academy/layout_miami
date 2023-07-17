c5c03c67bb300d8ea6a6665a643dc3f80ce60683
piotr <piotr_as@yahoo.com>
2023-07-17 22:08:55 +0200

    hamburger animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', e => {
   const header = document.querySelector('#header');
-
   const headerTransform = 'header-transform';
-
   const scrollPosition = 200;
 
   const toggleClassOnScroll = () => {
     if (window.scrollY >= scrollPosition) {
       header.classList.add(headerTransform);
     } else {
-      header.classList.remove(headerTransform);
-    }
-
-    if (menu.classList.contains('--menu-toggle')) {
       header.classList.remove(headerTransform);
     }
   };
@@ -25,6 +19,13 @@ document.addEventListener('DOMContentLoaded', e => {
 
   const hamburgerBtn = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
+  const boxShadow = `0 0 2.2px rgba(0, 0, 0, 0.02),
+  0 0 5.3px rgba(0, 0, 0, 0.03),
+  0 0 10px rgba(0, 0, 0, 0.04),
+  0 0 17.9px rgba(0, 0, 0, 0.04),
+  0 0 33.4px rgba(0, 0, 0, 0.05),
+  0 0 80px rgba(0, 0, 0, 0.07);
+  `;
 
   hamburgerBtn.addEventListener('click', event => {
     event.preventDefault();
@@ -34,9 +35,11 @@ document.addEventListener('DOMContentLoaded', e => {
     if (menu.classList.contains('--menu-toggle')) {
       document.body.classList.add('page__body--with-menu');
       document.body.style.removeProperty('overflow-x');
+      header.style.boxShadow = 'none';
     } else {
       document.body.classList.remove('page__body--with-menu');
       document.body.style.addProperty('overflow-x');
+      header.style.boxShadow = boxShadow;
     }
   });
 
