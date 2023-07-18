f027f5fab6fe9c159f9f8902b2d6eb560a61fff4
Dominika Karmel <dkarmel@op.pl>
2023-07-18 14:49:53 +0200

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.documentElement.classList.add('page--menu-open');
+  } else {
+    document.documentElement.classList.remove('page--menu-open');
+  }
+});
+
+// upward
+const scroll = document.querySelector('.upward');
+
+window.addEventListener('scroll', () => {
+  scroll.classList.toggle('upward--active', window.scrollY > 500);
+});
+
+scroll.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
