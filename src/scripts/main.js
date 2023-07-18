2f7f41fc51925555ad81a32eca2181e8e6ee7876
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-07-18 17:33:51 +0200

    disabled page scrolling under the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,11 @@
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
