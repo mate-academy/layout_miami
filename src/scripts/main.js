7bd4785828caef1b381aafb8dd3999c28cd115fc
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-07-19 15:05:48 +0200

    add scroll behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,6 +18,7 @@ function sendForm() {
 
 function handleMenu() {
   const menu = document.getElementById('menu');
+  const htmlBody = document.getElementById('body');
   const showMenuBtn = document.getElementById('show-menu-btn');
   const hideMenuBtn = document.getElementById('hide-menu-btn');
   const menuLinks = document.querySelectorAll('.page-navigation__link');
@@ -26,6 +27,7 @@ function handleMenu() {
     e.preventDefault();
 
     if (!menu.classList.contains('menu__open')) {
+      htmlBody.classList.add('body--with-menu');
       menu.classList.add('menu__open');
     }
   });
@@ -34,13 +36,17 @@ function handleMenu() {
     e.preventDefault();
 
     if (menu.classList.contains('menu__open')) {
+      htmlBody.classList.remove('body--with-menu');
       menu.classList.remove('menu__open');
     }
   });
 
   menuLinks.forEach(el => {
     el.addEventListener('click', (e) => {
+      // e.preventDefault();
+
       if (menu.classList.contains('menu__open')) {
+        htmlBody.classList.remove('body--with-menu');
         menu.classList.remove('menu__open');
       }
     });
