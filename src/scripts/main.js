72ba16ea022b882a10b73b60d3b8f94e6d3b39ab
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-07-19 11:55:25 +0200

    add open/hide menu basic script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,39 @@
 'use strict';
-sendForm();
+init();
 
 // ------- F U N C T I O N S -------
+function init() {
+  sendForm();
+  handleMenu();
+}
+
 function sendForm() {
   const form = document.getElementById('form');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     form.reset();
+  });
+}
+
+function handleMenu() {
+  const menu = document.getElementById('menu');
+  const showMenuBtn = document.getElementById('show-menu-btn');
+  const hideMenuBtn = document.getElementById('hide-menu-btn');
+
+  showMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (!menu.classList.contains('menu__open')) {
+      menu.classList.add('menu__open');
+    }
+  });
+
+  hideMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (menu.classList.contains('menu__open')) {
+      menu.classList.remove('menu__open');
+    }
   });
 }
