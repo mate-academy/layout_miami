f2b20406444657f02a1569216f49811bea562a7f
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-07-19 10:10:32 +0200

    add submit listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+sendForm();
+
+// ------- F U N C T I O N S -------
+function sendForm() {
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.reset();
+  });
+}
