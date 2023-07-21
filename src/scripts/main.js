b3f6c910c5aa8ac7a3bd79da151b9b92bd8cf193
Piotr <piotrkrasucki@gmail.com>
2023-07-21 13:23:03 +0200

    solution no4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
+function preventMobileScroll(event) {
+  event.preventDefault();
+}
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#nav') {
+  // prevent touch scroll
+    document.addEventListener('touchmove', preventMobileScroll,
+      { passive: false });
+
     const elementHtml = document.documentElement;
 
     elementHtml.classList.add('page__html--with-menu');
@@ -9,6 +17,10 @@ window.addEventListener('hashchange', () => {
     const elementHtml = document.documentElement;
 
     elementHtml.classList.remove('page__html--with-menu');
+
+    // usuwam touch event
+    document.removeEventListener('touchmove', preventMobileScroll,
+      { passive: false });
   }
 });
 
