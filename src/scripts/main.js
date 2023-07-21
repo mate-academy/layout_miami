50467d6b73e5f4646b9a5febf97e5d276a28ce89
Piotr <piotrkrasucki@gmail.com>
2023-07-21 17:17:03 +0200

    solution no4 with additional  body overflow class on nav#

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,3 +30,11 @@ function handleForm(event) {
   event.preventDefault();
 }
 form.addEventListener('submit', handleForm);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
