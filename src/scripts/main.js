205402c3884624503d3b568bf0c356dbc504afdb
patryk177m <patrykmajewski177@gmail.com>
2023-07-21 23:12:20 +0200

    corrections have been made. No tablet view.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,11 @@
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', e => e.preventDefault());
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
