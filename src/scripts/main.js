6a007f825cad1b571a57dabec6bebc63cc3b0965
Artur Wiśniewski <wisnia3008@interia.pl>
2023-07-23 22:17:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-'use strict';
+// 'use strict';
+
+// const btnNavEl = document.querySelector('.icon--menu');
+// const menuEl = document.querySelector('.menu');
+// const menuClose = document.querySelector('.menu--close-button');
+
+// btnNavEl.addEventListener('click', function() {
+//   menuEl.classList.add('menu--active');
+// });
+
+// menuClose.addEventListener('click', function() {
+//   menuEl.classList.remove('menu--active');
+// });
+
+// const menuLinks = document.querySelectorAll('.menu__link');
+
+// menuLinks.forEach(function(link) {
+//   link.addEventListener('click', function() {
+//     menuEl.classList.remove('menu--active');
+//   });
+// });
