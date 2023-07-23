4258d2bd39f734a6889ed21c1534b5c57546f9fb
Patryk Bąboń <patryk.babon@gmail.com>
2023-07-24 00:53:57 +0200

    added miami layout to catalog

---
+++ src/pages/miami/scripts/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+import './modules/menu.js';
+import './modules/contact-us.js';
---
+++ src/pages/miami/scripts/modules/menu.js
@@ -0,0 +1,34 @@
+const menu = document.getElementById('menu');
+const openMenuButton = document.getElementById('menu-open-button');
+const closeMenuButton = document.getElementById('menu-close-button');
+
+openMenuButton.addEventListener('click', function() {
+  document.body.style.overflowY = 'hidden';
+
+  menu.classList.add('menu--active');
+});
+
+closeMenuButton.addEventListener('click', function() {
+  menu.classList.remove('menu--active');
+
+  document.body.style.overflowY = null;
+});
+
+const menuLinks = document.getElementsByClassName('menu__list-link');
+
+Array.from(menuLinks).forEach(link =>
+  link.addEventListener('click', redirect));
+
+function redirect(event) {
+  menu.classList.remove('menu--active');
+
+  const url = event.target.getAttribute('value');
+
+  setTimeout(function() {
+    document.body.style.overflowY = null;
+
+    const element = document.getElementById(url);
+
+    element.scrollIntoView();
+  }, 300);
+}
