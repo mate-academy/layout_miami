5c06e81c00b3243d738566b6a557e193a007a14b
Andrzej Pec <pimatstudio@gmail.com>
2023-07-24 11:31:03 +0200

    yet new fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,14 @@ document.querySelector('#form').addEventListener('submit', (e) =>
   e.preventDefault()
 );
 
-document.querySelector('#button').addEventListener('click', function(event) {
-  event.preventDefault();
-});
-
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.header__icon:last-child')
     .addEventListener('click', openNav);
 
   document.querySelector('.header__logo--black')
+    .addEventListener('click', closeNav);
+
+  document.querySelector('.menu__list')
     .addEventListener('click', closeNav);
 
   document.querySelector('.closebtn').addEventListener('click', closeNav);
@@ -29,41 +28,6 @@ document.addEventListener('DOMContentLoaded', function() {
         behavior: 'smooth',
       });
     });
-  });
-
-  document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-
-    let isValid = true;
-
-    if (nameInput.value.trim() === '') {
-      isValid = false;
-      nameInput.classList.add('error');
-    } else {
-      nameInput.classList.remove('error');
-    }
-
-    if (emailInput.value.trim() === '') {
-      isValid = false;
-      emailInput.classList.add('error');
-    } else {
-      emailInput.classList.remove('error');
-    }
-
-    if (messageInput.value.trim() === '') {
-      isValid = false;
-      messageInput.classList.add('error');
-    } else {
-      messageInput.classList.remove('error');
-    }
-
-    if (isValid) {
-      this.submit();
-    }
   });
 });
 
