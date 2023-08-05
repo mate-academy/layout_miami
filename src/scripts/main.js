ba7afafc0730041672d45e7a1b440a4e9d682b30
LukashevychSergey <lukashevychsergey@gmail.com>
2023-08-05 15:36:45 +0300

    added some edits and some js code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.getElementById('myForm').addEventListener('submit', formSubmit, false);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(this);
+  let outputMessage = 'Form Data:\n';
+
+  formData.forEach((value, key) => {
+    outputMessage += `${key}: ${value}\n`;
+  });
+
+  console.log(outputMessage);
+}
