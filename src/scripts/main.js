e9b35b450780f0ac1436a8ae7e3080c699f31fbc
LabPetro <lamppetro30@outlook.com>
2023-09-22 21:57:07 +0300

    task almost complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
+
+document.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
