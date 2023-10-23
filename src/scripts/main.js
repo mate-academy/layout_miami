66c6eb41406ac38fe1d3e2ba28bf8d69b11043ab
kate-prakofyeva <catherine.smiff@gmail.com>
2023-10-23 13:23:48 +0200

    fix: disable page scrolling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
