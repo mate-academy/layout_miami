667b8e8ffb4ac6eb94268c203690d6d7ecf8f348
web3-sorcerer <formyriadofsetups@gmail.com>
2023-10-23 16:14:27 +0200

    first 2 blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Function to open the modal
+function openModal(event) {
+  event.preventDefault(); // Prevent default behavior of the click event
+
+  const modal = document.querySelector('.modal'); // Select the modal element
+
+  modal.style.display = 'block'; // Set modal display to 'block' to show it
+}
+
+// Attach click event listener to the burger icon to open the modal
+document.querySelector('.header__icon--burger')
+  .addEventListener('click', openModal);
+
+// Function to close the modal
+function closeModal() {
+  const modal = document.querySelector('.modal'); // Select the modal element
+
+  modal.style.display = 'none'; // Set modal display to 'none' to hide it
+}
+
+// Attach click event listener to the close icon in the modal
+document.querySelector('.modal__close')
+  .addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default behavior of the click event
+    closeModal(); // Call the closeModal function
+  });
