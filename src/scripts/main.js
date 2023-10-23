39cd3f6b02d19fb7712a795acaa28a21c6c9b7bc
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2023-10-23 20:27:32 +0200

    Background-image resolution change, remove scroll if menu open, change page title, add required for form inputs, change autocomplete styles, textarea noresize

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// Pobierz elementy przycisków
+const openMenuButton = document.querySelector('.header__nav-item--burger');
+const closeMenuButton = document.querySelector('.menu__nav-item--close');
+
+// Pobierz element body
+const pageBody = document.getElementById('page');
+
+// Funkcja do obsługi przycisku "Hamburger"
+function openMenu() {
+  // Otwórz menu
+  document.location.href = '#menu';
+
+  // Zablokuj scroll
+  pageBody.style.overflow = 'hidden';
+}
+
+// Funkcja do obsługi przycisku "Close"
+function closeMenu() {
+  // Zamknij menu
+  document.location.href = '#home';
+
+  // Odblokuj scroll
+  pageBody.style.overflow = '';
+}
+
+// Dodaj obsługę zdarzeń kliknięcia
+openMenuButton.addEventListener('click', openMenu);
+closeMenuButton.addEventListener('click', closeMenu);
