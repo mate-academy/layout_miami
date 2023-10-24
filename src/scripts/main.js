1ce18816586838a1a04267bc260fa2a609701f08
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2023-10-24 10:53:01 +0200

    Fix menu overflow, add hover for links, link logo to home page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,10 @@
 // Pobierz elementy przycisków
 const openMenuButton = document.querySelector('.header__nav-item--burger');
 const closeMenuButton = document.querySelector('.menu__nav-item--close');
+const logoButton = document.querySelector('.menu__nav-item--logo');
+const callButton = document.querySelector('.menu__container-call-link');
 
-// Pobierz element body
-const pageBody = document.getElementById('page');
+const pageBody = document.body;
 
 // Funkcja do obsługi przycisku "Hamburger"
 function openMenu() {
@@ -16,7 +17,7 @@ function openMenu() {
   pageBody.style.overflow = 'hidden';
 }
 
-// Funkcja do obsługi przycisku "Close"
+// Funkcja do obsługi zamknięcia menu
 function closeMenu() {
   // Zamknij menu
   document.location.href = '#home';
@@ -25,6 +26,16 @@ function closeMenu() {
   pageBody.style.overflow = '';
 }
 
-// Dodaj obsługę zdarzeń kliknięcia
+// Dodaj obsługę zdarzeń kliknięcia na przyciski
 openMenuButton.addEventListener('click', openMenu);
 closeMenuButton.addEventListener('click', closeMenu);
+logoButton.addEventListener('click', closeMenu);
+callButton.addEventListener('click', closeMenu);
+
+// Pobierz wszystkie linki w menu
+const menuLinks = document.querySelectorAll('.menu__links-list-item-link');
+
+// Dodaj obsługę zdarzenia kliknięcia do każdego linku w menu
+menuLinks.forEach((link) => {
+  link.addEventListener('click', closeMenu);
+});
