9d81aba629ec83f50110b655b14935a7c50a192a
Mirosław Wandyk <barosz1111@gmail.com>
2023-10-24 16:10:56 +0200

    navigation complete

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 const menu = document.querySelector('.hamburger-menu');
+const links = document.querySelector('.links');
+const logo = document.querySelector('.navigation__logo');
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('active');
+  links.classList.toggle('active');
+  logo.classList.toggle('active');
 });
