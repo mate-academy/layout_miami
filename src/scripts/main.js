10d773493dc12b9d53feb33e40e9e835f1726851
Adrian <adrian.krause.ak@gmail.com>
2023-10-26 18:29:19 +0200

    Grid changes and code improvement.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+sendForm();
+
+function sendForm() {
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name');
+
+    name.value = '';
+
+    const email = document.getElementById('email');
+
+    email.value = '';
+
+    const message = document.getElementById('message');
+
+    message.value = '';
+  });
+}
