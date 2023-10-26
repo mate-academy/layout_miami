7a017eccba630db2500c9e197353fffbac42fba0
Marcin Ligmann <marcinligmann2@gmail.com>
2023-10-26 10:51:40 +0200

    added :hover for all buttons and cleaning form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const menu = document.querySelector('.navigation__item--hamburger');
+const links = document.querySelector('.naviagtion__icon');
+const logo = document.querySelector('.navigation__item--logo');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  links.classList.toggle('active');
+  logo.classList.toggle('active');
+});
+
+// clean the form
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+  // event.target.querySelectorAll('.form-item__label + *')
+  //   .forEach(input => input.value = '');
+});
