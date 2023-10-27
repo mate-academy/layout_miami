72b7726feed0ada0498a82ac5f01ed2f93c1b311
Adrian <adrian.krause.ak@gmail.com>
2023-10-27 10:36:37 +0200

    Disabled page scrolling under menu.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,3 +21,11 @@ function sendForm() {
     message.value = '';
   });
 }
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
