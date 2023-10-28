2a3520953840ba6b165a5baabcdb6cf7b12cc8cd
web3-sorcerer <formyriadofsetups@gmail.com>
2023-10-28 14:45:16 +0200

    finished project - improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,3 +24,33 @@ document.querySelector('.modal__close')
     event.preventDefault(); // Prevent default behavior of the click event
     closeModal(); // Call the closeModal function
   });
+
+document.getElementById('resetForm').addEventListener('click', function() {
+  document.getElementById('contact__form').reset();
+});
+
+const modalLinks = document.querySelectorAll('.modal .modal__anchor');
+
+modalLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Close the modal
+    const modal = document.querySelector('.modal');
+
+    modal.style.display = 'none';
+
+    // Get the text inside the clicked link to determine the target section
+    const targetSectionText = this.textContent.trim().toLowerCase()
+      .replace(' ', '-');
+
+    // Scroll smoothly to the target section based on the link text
+    const targetSection = document.getElementById(targetSectionText);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+});
