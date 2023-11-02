2afb2c519c1d6c8708d2212c3ee406da3c1268aa
olek03 <olekg48@gmail.com>
2023-11-02 18:58:32 +0100

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const submitButton = document.querySelector('button[type=submit]');
+const nameInput = document.querySelector('input[name=name]');
+const emailInput = document.querySelector('input[name=email]');
+const messageInput = document.querySelector('textarea[name=message]');
+
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+});
