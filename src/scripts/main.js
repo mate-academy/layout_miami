b023a6b01ed9f3b3fe4f2a2b8c932b9083609a45
Radoslaw Bielski <bielskiradek@gmail.com>
2023-11-02 19:03:29 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 'use strict';
-const form = document.querySelector("#form");
 
-form.addEventListener("submit", onFormSubmit, false);
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', onFormSubmit, false);
 
 function onFormSubmit(event) {
   form.reset();
