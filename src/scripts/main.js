b60cba11ea8501c981943bb96973e1b2b6bd703c
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-11-20 22:50:16 +0200

    refactored issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const contactForm = document.getElementById('contact-us__form');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.alert('This form has been successfully submitted!');
+  contactForm.reset();
+});
