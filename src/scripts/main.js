5dacc633eb8762a03a8813a3b692849b20cc2478
Arturo <af.pacacanowski@gmail.com>
2023-12-25 09:20:36 +0100

    implemented solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', submitClick, false);
+
+function submitClick(event) {
+  const myForm = document.getElementById('myForm');
+
+  if (myForm.checkValidity()) {
+    const warn = 'Your message has been received! Thanks for getting in touch!';
+
+    document.getElementById('output-box').innerHTML += warn;
+
+    event.preventDefault();
+  }
+}
