e134434e82ee70d03e8a72270c99369fce5039f1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-01-12 22:09:23 +0100

    form fixed, checklist made

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+});
