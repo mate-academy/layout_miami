35b0ec19fbdfd7561ed77c7816e33cfd9c7d51f1
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-19 15:22:48 +0100

    fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,15 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('body--no-scroll');
   }
 });
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const formElement = form.querySelectorAll('.form__in');
+
+  formElement.forEach(element => {
+    element.value = '';
+  });
+});
