69c0d0cbe9d33cb8375d182625c08ab54c96616c
Oles Partyka <oles.partyka.29@gmail.com>
2024-02-09 10:59:53 +0100

    changes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,13 @@ function stopDefAction(event) {
 document.getElementById('form').addEventListener(
   'submit', stopDefAction, false,
 );
+
+const pageBody = document.querySelector('.page__body');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    pageBody.classList.add('pagebody--scroll');
+  } else {
+    pageBody.classList.remove('page__body--scroll');
+  }
+});
