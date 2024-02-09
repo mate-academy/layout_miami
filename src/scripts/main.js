80df6d98d2ec43fc371db1883af6b44239be1611
Oles Partyka <oles.partyka.29@gmail.com>
2024-02-09 12:50:48 +0100

    second option without JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,12 +9,12 @@ document.getElementById('form').addEventListener(
   'submit', stopDefAction, false,
 );
 
-const pageBody = document.querySelector('.page__body');
+// const pageBody = document.querySelector('.page__body');
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    pageBody.classList.add('page__body--scroll');
-  } else {
-    pageBody.classList.remove('page__body--scroll');
-  }
-});
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     pageBody.classList.add('page__body--scroll');
+//   } else {
+//     pageBody.classList.remove('page__body--scroll');
+//   }
+// });
