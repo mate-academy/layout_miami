5e65120b21692b471a7f1df9738aebb1f64085d3
Konrad Wasilewski <konradwasilewski444@gmail.com>
2024-02-15 16:22:20 +0100

    everything fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const buttonPrimary = document.querySelector('#button-primary');
+
+buttonPrimary.addEventListener('click', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+
+  const message = 'Button clicked!<br>';
+
+  document.getElementById('output-box').innerHTML += message;
+}
+
+// ..........
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
