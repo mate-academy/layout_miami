502d10cbb716f0992e34c5461a9bcc61ac3eacaf
Wojciech <weswojciech@gmail.com>
2024-02-17 15:44:08 +0100

    add preventdefault to form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const preventDefault = document.querySelectorAll(".form__prevent");
+
+preventDefault.addEventListener("click", check, false);
+
+function check(e) {
+  e.preventDefault();
+}
