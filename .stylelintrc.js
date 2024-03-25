e204ea6f2e0ccb5cff6206123db51ca0791828f4
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-03-25 21:02:20 +0100

    task

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,13 +3,5 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {
-    "rule-empty-line-before": [
-      "always",
-      {
-        "except": ["first-nested"],
-        "ignore": ["after-comment"]
-      }
-    ]
-  }
+  rules: {}
 };
