a6642118e997d4fe4a22e42162903aadcd38411d
MarekSax <mareksax@wp.pl>
2024-05-15 11:14:53 +0200

    chore: clean up the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+window.addEventListener("hashchange", () => {
+  console.log("test");
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("page__body--with-menu");
+  } else {
+    document.body.classList.remove("page__body--with-menu");
+  }
+});
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
