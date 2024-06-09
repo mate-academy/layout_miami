7b6c956f02a94ab61e9d7948683f25add652fbce
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-09 17:13:49 +0200

    Adding some features

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const burgerMenuButton = document.querySelector(
+  '.navigation__item--burger-menu',
+);
+const burgerMenu = document.querySelector('.burger-menu');
+const logo = document.querySelector('.navigation__logo');
+
+burgerMenuButton.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger-menu--active');
+  burgerMenuButton.classList.toggle('navigation__item--burger-menu--active');
+  logo.classList.toggle('navigation__item--burger-menu--active');
+});
