bea8785489be3f0aee37165d8389548aabe4d6e9
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-20 21:02:59 +0200

    Finale (hope so XD)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,4 +8,9 @@ const burgerMenu = document.querySelector('.burger-menu');
 burgerMenuButton.addEventListener('click', () => {
   burgerMenu.classList.toggle('burger-menu--active');
   burgerMenuButton.classList.toggle('navigation__item--burger-menu--active');
+  if (burgerMenu.classList.contains('burger-menu--active')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
 });
