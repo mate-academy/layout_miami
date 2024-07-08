0c83e7007a3f535e3a4f872ce24396ef79e4423a
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-07-08 18:42:39 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 'use strict';
+init();
+
+// ------- F U N C T I O N S -------
+function init() {
+  sendForm();
+  handleMenu();
+}
+
+function sendForm() {
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.reset();
+  });
+}
+
+function handleMenu() {
+  const menu = document.getElementById('menu');
+  const htmlBody = document.getElementById('body');
+  const showMenuBtn = document.getElementById('show-menu-btn');
+  const hideMenuBtn = document.getElementById('hide-menu-btn');
+  const menuLinks = document.querySelectorAll('.page-navigation__link');
+
+  showMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (!menu.classList.contains('menu__open')) {
+      htmlBody.classList.add('body--with-menu');
+      menu.classList.add('menu__open');
+    }
+  });
+
+  hideMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (menu.classList.contains('menu__open')) {
+      htmlBody.classList.remove('body--with-menu');
+      menu.classList.remove('menu__open');
+    }
+  });
+
+  menuLinks.forEach(el => {
+    el.addEventListener('click', (e) => {
+
+      if (menu.classList.contains('menu__open')) {
+        htmlBody.classList.remove('body--with-menu');
+        menu.classList.remove('menu__open');
+      }
+    });
+  });
+}
