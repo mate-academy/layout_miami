89afad6b6f2a33aa4c23dac597fa2bef6525075c
Grzegorz <grze.pra@gmail.com>
2025-01-27 18:39:48 +0100

    Hover and focus color on black. Added prevenDefault() to submit button on form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,12 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Zablokuj domyślne zachowanie formularza
+    const submitButton = document.getElementById("submit-btn");
+    submitButton.disabled = true; // Wyłącz przycisk
+    console.log("Formularz został wysłany, przycisk wyłączony.");
+});
