2c3a2237dde051e1eed5707b6e39688a01c10f3b
Artur Gorniak <arturgurniak@gmail.com>
2025-01-28 07:09:59 +0100

    next try to disable scroling with menu opened

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
-// function openMenu() {
-//   document.body.classList.add('menu-open');
-// }
+function openMenu() {
+  document.body.classList.add('menu-open');
+}
 
-// function closeMenu() {
-//   document.body.classList.remove('menu-open');
-// }
+function closeMenu() {
+  document.body.classList.remove('menu-open');
+}
+
+document.getElementsByClassName('nav__link burger')
+  .addEventListener('click', openMenu);
+
+document.getElementsByClassName('menu__icon close')
+  .addEventListener('click', closeMenu);
