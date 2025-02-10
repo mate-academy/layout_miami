492f7bc8662d6222786b50f4ffd51aaf1129f261
Jakub Podgrodzki <podgrodzkijakub@gmail.com>
2025-02-10 11:22:45 +0100

    fixed form js and html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.getElementById("contact__form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  document.getElementById("contact__form").reset();
+  window.alert("Form sent!");
+});
