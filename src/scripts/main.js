c2e30ebbc6657b8a6b41656ce86ea1d40d51f1d6
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-08-06 15:07:48 +0100

    Final touch with checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.documentElement.classList.add('html--with-menu');
+  } else {
+    document.documentElement.classList.remove('html--with-menu');
+  }
+});
