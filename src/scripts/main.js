af1dba25177df0da474d67352238f4efd5385d97
filip <filip1folga@gmail.com>
2025-09-21 20:18:18 +0200

    disable scrolling on menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-'use strict';
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu-open');
+  } else {
+    document.body.classList.remove('menu-open');
+  }
+});
