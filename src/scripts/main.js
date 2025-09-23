6bf63db6b8a5ff15e262b4ffead998e51cfa1c71
filip <filip1folga@gmail.com>
2025-09-23 21:25:34 +0200

    changing icons sizes, navigation margins, hover state for menu -fixing issues : "Using a Modifier Instead of an Element"/"Using a Modifier Without the Belonging Class"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
+    const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
     document.body.classList.add('menu-open');
+    document.body.style.paddingRight = scrollBarWidth + 'px';
   } else {
     document.body.classList.remove('menu-open');
+    document.body.style.paddingRight = '0';
   }
+});
+
+
+const form = document.querySelector(".section__form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = form.formName.value;
+  const email = form.formEmail.value;
+  const message = form.querySelector(".form__message").value;
+
+  console.log("Form submitted!", { name, email, message });
+
+  alert("Form submitted without reloading!");
 });
